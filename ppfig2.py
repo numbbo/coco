c7e14eb6458c77b7eec47a9706ecf6efaec9c024
rros <rros@bd143551-dd2f-0410-b9f7-ac39b51fe4c9>
2009-03-26 18:07:46 +0000

    BUG FIX: run.py warns when results of functions from noisy and non-noisy testbeds are provided BUG FIX: readindexfiles.py was buggy: if an index entry is merged with another, some attributes were not updated. Added: first attempt at comparing 2 algorithms (ERT figures). Minor bug detected in fgeneric: extra \n in info files added when... ? MINOR FIX: Unused variable in exampletiming.

---
+++ ppfig2.py
@@ -0,0 +1,272 @@
+#! /usr/bin/env python
+
+# Creates ENFEs and convergence figures for BBOB post-processing.
+
+import os
+import sys
+import warnings
+import matplotlib.pyplot as plt
+import numpy
+from pdb import set_trace
+from bbob_pproc import bootstrap, pproc
+
+
+colors = ('k', 'b', 'c', 'g', 'y', 'm', 'r', 'k', 'k', 'c', 'r', 'm')
+
+#Get benchmark short infos.
+funInfos = {}
+isBenchmarkinfosFound = True
+#infofile = os.path.join(os.path.split(__file__)[0], '..', '..',
+                        #'benchmarkshortinfos.txt')
+infofile = os.path.join(os.path.split(__file__)[0], 'benchmarkshortinfos.txt')
+
+try:
+    f = open(infofile,'r')
+    for line in f:
+        if len(line) == 0 or line.startswith('%') or line.isspace() :
+            continue
+        funcId, funcInfo = line[0:-1].split(None,1)
+        funInfos[int(funcId)] = funcId + ' ' + funcInfo
+    f.close()
+except IOError, (errno, strerror):
+    print "I/O error(%s): %s" % (errno, strerror)
+    isBenchmarkinfosFound = False
+    print 'Could not find file', infofile, \
+          'Titles in scaling figures will not be displayed.'
+
+
+def createFigure(data, figHandle=None):
+    """ Create a plot in a figure (eventually new) from a data array.
+
+        Mandatory input:
+        data - 2d-array where the 1st column contains x-values
+               and the remaining columns contain y-values
+
+        Optional inputs:
+        figHandle - handle to existing figure, if omitted a new figure
+                    will be created
+
+        Output:
+        lines - handles of the Line2D instances.
+    """
+
+    # initialize figure and handles
+    if figHandle is None:
+        figHandle = plt.figure()
+    else:
+        plt.figure(figHandle.number)
+
+    lines = []
+    # Plot data sets
+    for i in range(1,len(data[0,:])):
+        yValues = data[:,i]
+
+        # Plot figure
+        tmp = numpy.where(numpy.isfinite(data[:,i]))[0]
+        if tmp.size > 0:
+            lines.extend(plt.plot(data[tmp,0], yValues[tmp]))
+            #lines.extend(plt.plot([data[tmp[-1],0]], [yValues[tmp[-1]]],
+                                  #marker='+'))
+            #TODO: larger size.
+        #else: ???
+
+    return lines
+
+
+def customizeFigure(figHandle, figureName = None, title='',
+                    fileFormat=('png','eps'), labels=None,
+                    legend=True, locLegend='best', verbose=True):
+    """ Customize a figure by adding a legend, axis label, etc. At the
+        end the figure is saved.
+
+        Inputs:
+        figHandle - handle to existing figure
+        figureName - name of the output figure
+
+        Optional Inputs:
+        fileFormat - list of formats of output files
+        labels - list with xlabel and ylabel
+        legend - if this list is not empty a legend will be generated from
+                 the entries of this list
+        locLegend - location of legend
+
+    """
+
+    # Input checking
+
+    # Get axis handle and set scale for each axis
+    axisHandle = figHandle.gca()
+    axisHandle.set_xscale('log')
+    axisHandle.set_yscale('log')
+
+    xmin, xmax = plt.xlim()
+    plt.xlim(max(xmin, 1e-8), xmax)
+
+    # Annotate figure
+    if not labels is None: #Couldn't it be ''?
+        axisHandle.set_xlabel(labels[0])
+        axisHandle.set_ylabel(labels[1])
+
+    # Grid options
+    axisHandle.grid('True')
+    tmp = axisHandle.get_xticks()
+    tmp2 = []
+    for i in tmp:
+        tmp2.append('%d' % round(numpy.log10(i)))
+    axisHandle.set_xticklabels(tmp2)
+    tmp = axisHandle.get_yticks()
+    tmp2 = []
+    for i in tmp:
+        tmp2.append('%d' % round(numpy.log10(i)))
+    axisHandle.set_yticklabels(tmp2)
+
+    # Legend
+    if legend:
+        plt.legend(loc=locLegend)
+    axisHandle.invert_xaxis()
+    axisHandle.set_title(title)
+
+    # Save figure
+    if not (figureName is None or fileFormat is None):
+        if isinstance(fileFormat, basestring):
+            plt.savefig(figureName + '.' + fileFormat, dpi = 120,
+                        format = entry)
+            if verbose:
+                print 'Wrote figure in %s.' %(figureName + '.' + fileFormat)
+        else:
+            #TODO: that is if fileFormat is iterable.
+            for entry in fileFormat:
+                plt.savefig(figureName + '.' + entry, dpi = 120,
+                            format = entry)
+                if verbose:
+                    print 'Wrote figure in %s.' %(figureName + '.' + entry)
+
+    # Close figure
+    plt.close(figHandle)
+
+    # TODO:    *much more options available (styles, colors, markers ...)
+    #       *output directory - contained in the file name or extra parameter?
+
+
+def sortIndexEntries(indexEntries):
+    """From a list of IndexEntry, returns a sorted dictionary."""
+    sortByFunc = {}
+    dims = set()
+    funcs = set()
+    for elem in indexEntries:
+        sortByFunc.setdefault(elem.funcId,{})
+        sortByFunc[elem.funcId][elem.dim] = elem
+        funcs.add(funcId)
+        dims.add(elem.dim)
+
+    return sortByFunc
+
+
+def generateData(indexEntry):
+    res = []
+    for i in indexEntry.hData:
+        tmp = numpy.array(i[0])
+        tmp2 = []
+        for j in i[indexEntry.nbRuns+1:]:
+            tmp2.append(j <= i[0])
+        tmp = numpy.append(tmp, bootstrap.sp1(i[1:indexEntry.nbRuns + 1], 
+                                              issuccessful=tmp2))
+        tmp = numpy.append(tmp, bootstrap.prctile(i[1:indexEntry.nbRuns + 1], 
+                                                  [0.5])[0])
+        #set_trace()
+        res.append(tmp)
+    return numpy.vstack(res)
+
+def computeERT(hdata):
+    res = []
+    nbRuns = (numpy.shape(hdata)[1]-1)/2
+    for i in hdata:
+        success = i[nbRuns+1:] <= i[0]
+        tmp = [i[0]]
+        tmp.extend(bootstrap.sp(i[1:nbRuns+1], issuccessful=success))
+        res.append(tmp)
+    #set_trace()
+    return numpy.vstack(res)
+
+def main(indexEntriesAlg0, indexEntriesAlg1, dimsOfInterest, outputdir,
+         verbose=True):
+    """From a list of IndexEntry, returns ERT figure."""
+
+    plt.rc("axes", labelsize=20, titlesize=24)
+    plt.rc("xtick", labelsize=20)
+    plt.rc("ytick", labelsize=20)
+    plt.rc("font", size=20)
+    plt.rc("legend", fontsize=20)
+
+    dictFunc0 = indexEntriesAlg0.dictByFunc()
+    dictFunc1 = indexEntriesAlg1.dictByFunc()
+    funcs = set.union(set(dictFunc0), set(dictFunc0))
+
+    for func in funcs:
+        dictFunc0[func] = dictFunc0[func].dictByDim()
+        dictFunc1[func] = dictFunc1[func].dictByDim()
+        if isBenchmarkinfosFound:
+            title = funInfos[func]
+        else:
+            title = ''
+
+        filename = os.path.join(outputdir,'cmpdata_f%d' % (func))
+        fig = plt.figure()
+        for i, dim in enumerate(dimsOfInterest):
+            #compare dictFunc0[func][dim][0] and dictFunc1[func][dim][0]
+            try:
+                if len(dictFunc0[func][dim]) != 1 or len(dictFunc1[func][dim]) != 1:
+                    warnings.warn('gnagnagna')
+                    continue
+            except KeyError:
+                warnings.warn('gnagnagna')
+                continue
+
+            indexEntry0 = dictFunc0[func][dim][0]
+            indexEntry1 = dictFunc1[func][dim][0]
+            # align together and split again (the data are mixed together):
+            res = pproc.alignData(pproc.HArrayMultiReader([indexEntry0.hData,
+                                                           indexEntry1.hData]))
+            idxM = (numpy.shape(res)[1]-1)/2
+            idxCur = 1
+            idxNext = idxCur+indexEntry0.nbRuns()
+            data0 = res[:, numpy.r_[0, idxCur:idxNext]]
+            data0 = numpy.hstack((data0,
+                                  res[:, numpy.r_[idxM+idxCur:idxM+idxNext]]))
+            data0 = computeERT(data0)
+
+            idxCur += indexEntry0.nbRuns()
+            idxNext = idxCur+indexEntry1.nbRuns()
+            data1 = res[:, numpy.r_[0, idxCur:idxNext]]
+            data1 = numpy.hstack((data1,
+                                  res[:, numpy.r_[idxM+idxCur:idxM+idxNext]]))
+            data1 = computeERT(data1)
+            data = numpy.vstack((data0[:,0],
+                                          data1[:, 1]/data0[:, 1])).transpose()
+
+            h = createFigure(data[(data0[:, 2] > 0) * (data1[:, 2] > 0)], fig)
+            plt.setp(h[0], 'color', colors[i], 'marker', 'o',
+                     'label', '%d-D' % dim)
+
+            if (data0[:, 2] == 0).any():
+                #set_trace()
+                h = createFigure(data0[(data0[:, 2] == 0), 0:2], fig)
+                plt.setp(h[0], 'color', colors[i], 'marker', 'x')
+            if (data1[:, 2] == 0).any():
+                #set_trace()
+                h = createFigure(data1[(data1[:, 2] == 0), 0:2], fig)
+                plt.setp(h[0], 'color', colors[i], 'marker', '+')
+
+        legend = func in (1, 24, 101, 130)
+        customizeFigure(fig, filename, title=title,
+                    fileFormat=('eps','png'), labels=['', ''],
+                    legend=legend, locLegend='best', verbose=verbose)
+
+        #for i in h:
+            #plt.setp(i,'color',colors[dim])
+        #h = ppfig.createFigure(entry.arrayFullTab[:,[0,medianindex]], fig)
+        #for i in h:
+            #plt.setp(h,'color',colors[dim],'linestyle','--')
+        #Do all this in createFigure?
+
+    plt.rcdefaults()
---
+++ pprldistr2.py
@@ -0,0 +1,281 @@
+#! /usr/bin/env python
+
+"""Creates run length distribution figures."""
+
+
+from __future__ import absolute_import
+
+import os
+import numpy
+import matplotlib.pyplot as plt
+from pdb import set_trace
+
+#__all__ = []
+
+rldColors = ('k', 'c', 'm', 'r', 'k', 'c', 'm', 'r', 'k', 'c', 'm', 'r')
+rldUnsuccColors = ('k', 'c', 'm', 'k', 'c', 'm', 'k', 'c', 'm', 'k', 'c', 'm')  # should not be too short
+# Used as a global to store the largest xmax and align the FV ECD figures.
+fmax = None
+evalfmax = None
+
+def beautifyRLD(figHandle, figureName, maxEvalsF, fileFormat=('png', 'eps'),
+                text=None, verbose=True):
+    """Format the figure of the run length distribution and save into files."""
+    axisHandle = figHandle.gca()
+    axisHandle.set_xscale('log')
+    plt.xlim(1.0, maxEvalsF)
+    plt.ylim(0.0, 1.0)
+    axisHandle.set_xlabel('log10 of FEvals / DIM')
+    axisHandle.set_ylabel('proportion of trials')
+    # Grid options
+    axisHandle.grid('True')
+    xtic = axisHandle.get_xticks()
+    newxtic = []
+    for j in xtic:
+        newxtic.append('%d' % round(numpy.log10(j)))
+    axisHandle.set_xticklabels(newxtic)
+
+    plt.text(0.5, 0.93, text, horizontalalignment="center",
+             transform=axisHandle.transAxes)
+             #bbox=dict(ec='k', fill=False), 
+
+    #set_trace()
+    plt.legend(loc='best')
+    #if legend:
+        #axisHandle.legend(legend, locLegend)
+
+    # Save figure
+    for entry in fileFormat:
+        plt.savefig(figureName + '.' + entry, dpi = 300,
+                    format = entry)
+        if verbose:
+            print 'Wrote figure in %s.' %(figureName + '.' + entry)
+
+
+
+def plotRLDistr(indexEntries, fvalueToReach, maxEvalsF, verbose=True):
+    """Creates run length distributions from a sequence of indexEntries.
+
+    Keyword arguments:
+    indexEntries
+    fvalueToReach
+    verbose
+
+    Outputs:
+    res -- resulting plot.
+    fsolved -- number of different functions solved.
+    funcs -- number of different function considered.
+    """
+
+    x = []
+    nn = 0
+    fsolved = set()
+    funcs = set()
+    for i in indexEntries:
+        funcs.add(i.funcId)
+        for j in i.hData:
+            if j[0] <= fvalueToReach:
+                #This loop is needed because though some number of function
+                #evaluations might be below maxEvals, the target function value
+                #might not be reached yet. This is because the horizontal data
+                #do not go to maxEvals.
+
+                for k in range(1, i.nbRuns() + 1):
+                    if j[i.nbRuns() + k] <= fvalueToReach:
+                        x.append(j[k] / i.dim)
+                        fsolved.add(i.funcId)
+                break
+        nn += i.nbRuns()
+
+    label = ('%+d:%d/%d' %
+             (numpy.log10(fvalueToReach), len(fsolved), len(funcs)))
+    n = len(x)
+    if n == 0:
+        res = plt.plot([], [], label=label)
+    else:
+        x.sort()
+        x2 = numpy.hstack([numpy.repeat(x, 2), maxEvalsF])
+        #maxEvalsF: used for the limit of the plot.
+        y2 = numpy.hstack([0.0,
+                           numpy.repeat(numpy.arange(1, n+1) / float(nn), 2)])
+        res = plt.plot(x2, y2, label=label)
+
+    return res#, fsolved, funcs
+
+
+def beautifyFVD(figHandle, figureName, fileFormat=('png','eps'),
+                isStoringXMax=False, text=None, verbose=True):
+    """Formats the figure of the run length distribution.
+
+    Keyword arguments:
+    isStoringMaxF -- if set to True, the first call BeautifyVD sets the global
+                     fmax and all subsequent call will have the same maximum
+                     xlim.
+    """
+
+    axisHandle = figHandle.gca()
+    axisHandle.set_xscale('log')
+
+    if isStoringXMax:
+        global fmax
+    else:
+        fmax = None
+
+    if not fmax:
+        xmin, fmax = plt.xlim()
+    plt.xlim(1., fmax)
+
+    #axisHandle.invert_xaxis()
+    plt.ylim(0.0, 1.0)
+    axisHandle.set_xlabel('log10 of Df / Dftarget')
+    # axisHandle.set_ylabel('proportion of successful trials')
+    # Grid options
+    axisHandle.grid('True')
+    xtic = axisHandle.get_xticks()
+    newxtic = []
+    for j in xtic:
+        newxtic.append('%d' % round(numpy.log10(j)))
+    axisHandle.set_xticklabels(newxtic)
+    axisHandle.set_yticklabels(())
+
+    plt.text(0.98, 0.02, text, horizontalalignment="right",
+             transform=axisHandle.transAxes)
+             #bbox=dict(ec='k', fill=False), 
+
+    # Save figure
+    for entry in fileFormat:
+        plt.savefig(figureName + '.' + entry, dpi = 300,
+                    format = entry)
+        if verbose:
+            print 'Wrote figure in %s.' %(figureName + '.' + entry)
+
+def plotFVDistr(indexEntries, fvalueToReach, maxEvalsF, verbose=True):
+    """Creates empirical cumulative distribution functions of final function
+    values plot from a sequence of indexEntries.
+
+    Keyword arguments:
+    indexEntries -- sequence of IndexEntry to process.
+    fvalueToReach -- float used for the lower limit of the plot
+    maxEvalsF -- indicates which vertical data to display.
+    verbose -- controls verbosity.
+
+    Outputs: a plot of a run length distribution.
+    """
+
+    x = []
+    nn = 0
+    for i in indexEntries:
+        for j in i.vData:
+            if j[0] >= maxEvalsF * i.dim:
+                break
+        x.extend(j[i.nbRuns()+1:] / fvalueToReach)
+        nn += i.nbRuns()
+
+    x.sort()
+    x2 = numpy.hstack([numpy.repeat(x, 2)])
+    #not efficient if some vals are repeated a lot
+    #y2 = numpy.hstack([0.0, numpy.repeat(numpy.arange(1, n)/float(nn), 2),
+                       #float(n)/nn, float(n)/nn])
+    y2 = numpy.hstack([0.0, numpy.repeat(numpy.arange(1, nn)/float(nn), 2),
+                       1.0])
+    #set_trace()
+    res = plt.plot(x2, y2)
+
+    return res
+
+
+def main(indexEntries, valuesOfInterest, isStoringXMax=False, outputdir='',
+         info='default', verbose=True):
+    """Generate figures of empirical cumulative distribution functions.
+
+    Keyword arguments:
+    indexEntries -- list of IndexEntry instances to process.
+    valuesOfInterest -- target function values to be displayed.
+    isStoringXMax -- if set to True, the first call BeautifyVD sets the globals
+                     fmax and maxEvals and all subsequent calls will use these
+                     values as rightmost xlim in the generated figures.
+     -- if set to True, the first call BeautifyVD sets the global
+                     fmax and all subsequent call will have the same maximum
+                     xlim.
+    outputdir -- output directory (must exist)
+    info --- string suffix for output file names.
+
+    Outputs:
+    Image files of the empirical cumulative distribution functions.
+    """
+
+    #sortedIndexEntries = sortIndexEntries(indexEntries)
+
+    plt.rc("axes", labelsize=20, titlesize=24)
+    plt.rc("xtick", labelsize=20)
+    plt.rc("ytick", labelsize=20)
+    plt.rc("font", size=20)
+    plt.rc("legend", fontsize=20)
+
+    maxEvalsFactor = max(i.mMaxEvals()/i.dim for i in indexEntries) ** 1.05
+    #maxEvalsFactorCeil = numpy.power(10,
+                                     #numpy.ceil(numpy.log10(maxEvalsFactor)))
+
+    if isStoringXMax:
+        global evalfmax
+    else:
+        evalfmax = None
+
+    if not evalfmax:
+        evalfmax = maxEvalsFactor
+
+    figureName = os.path.join(outputdir,'pprldistr%s' %('_' + info))
+    fig = plt.figure()
+    legend = []
+    for j in range(len(valuesOfInterest)):
+        tmp = plotRLDistr(indexEntries, valuesOfInterest[j], evalfmax,
+                          verbose)
+        #set_trace()
+        if not tmp is None:
+            plt.setp(tmp, 'color', rldColors[j])
+            #set_trace()
+            #legend.append('%+d:%d/%d' %  
+                          #(numpy.log10(valuesOfInterest[j]), len(fsolved), 
+                           #len(f)))
+            if rldColors[j] == 'r':  # 1e-8 in bold
+                plt.setp(tmp, 'linewidth', 3)
+
+    funcs = list(i.funcId for i in indexEntries)
+    if len(funcs) > 1:
+        text = 'f%d-%d' %(min(funcs), max(funcs))
+    else:
+        text = 'f%d' %(funcs[0])
+
+    beautifyRLD(fig, figureName, evalfmax, text=text, verbose=verbose)
+    plt.close(fig)
+
+    figureName = os.path.join(outputdir,'ppfvdistr_%s' %(info))
+    fig = plt.figure()
+    for j in range(len(valuesOfInterest)):
+        #set_trace()
+        tmp = plotFVDistr(indexEntries, valuesOfInterest[j],
+                          maxEvalsFactor, verbose=verbose)
+        #if not tmp is None:
+        plt.setp(tmp, 'color', rldColors[j])
+        if rldColors [j] == 'r':  # 1e-8 in bold
+            plt.setp(tmp, 'linewidth', 3)
+
+    tmp = numpy.floor(numpy.log10(maxEvalsFactor))
+    # coloring left to right:
+    #maxEvalsF = numpy.power(10, numpy.arange(tmp, 0, -1) - 1)
+    # coloring right to left:
+    maxEvalsF = numpy.power(10, numpy.arange(0, tmp))
+
+    #The last index of valuesOfInterest is still used in this loop.
+    #set_trace()
+    for k in range(len(maxEvalsF)):
+        tmp = plotFVDistr(indexEntries, valuesOfInterest[j],
+                          maxEvalsF=maxEvalsF[k], verbose=verbose)
+        plt.setp(tmp, 'color', rldUnsuccColors[k])
+
+    beautifyFVD(fig, figureName, text=text, isStoringXMax=isStoringXMax,
+                verbose=verbose)
+
+    plt.close(fig)
+
+    plt.rcdefaults()
