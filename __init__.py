1c55814718c112e4061960b46d409eade15ca4c5
rros <rros@bd143551-dd2f-0410-b9f7-ac39b51fe4c9>
2011-02-10 15:24:43 +0000

    Merged changes from codecleanup branch: cocoexp, bbobbenchmarks and some corrections to code/matlab

--- __init__.py
+++ __init__.py
@@ -4,12 +4,14 @@
 """COmparing Continuous Optimisers (COCO) post-processing software
 
 This package is meant to generate output figures and tables for the
-benchmarking of continuous optimisers in the case of black-box optimisation.
-The post-processing tool takes as input data from experiments and generates
-output that will be used in the generation of the LateX-formatted article
-summarizing the experiments.
+benchmarking of continuous optimisers in the case of black-box
+optimisation.
+The post-processing tool takes as input data from experiments and
+generates output that will be used in the generation of the LateX-
+formatted article summarizing the experiments.
 
-To obtain more information on the use of this package from the python shell, type:
+To obtain more information on the use of this package from the python
+shell, type:
 
 >> help bbob_pproc.cococommands
 
@@ -21,5 +23,10 @@ import sys
 
 from bbob_pproc.cococommands import *
 
-__version__ = '10.7'
+from bbob_pproc.rungeneric import main as main
 
+__all__  = ['comp2', 'compall', 'main', 'ppfigdim', 'pplogloss', 'pprldistr',
+            'pproc', 'pptable', 'rungeneric', 'rungeneric1', 'rungeneric2',
+            'rungenericmany']
+
+__version__ = '10.7'
