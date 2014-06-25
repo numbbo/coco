a1c0bf6a955d77efa6923d9702b251115975d5b9
atamnaasmaines <atamnaasmaines@gmail.com>
2014-06-25 12:54:52 +0000

    Different powers function

---
+++ src/f_differentPowers.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+#include "numbbo.h"
+
+#include "numbbo_problem.c"
+
+static void f_differentPowers_evaluate(numbbo_problem_t *self, double *x, double *y) {
+    /*
+     TODO: boundary handling
+     TODO: use Xopt
+     TODO: shift x using Xopt
+     TODO: replace "y[0] += 0" with "y[0] += fopt" of the instance
+     */
+    size_t i;
+    assert(self->number_of_objectives == 1);
+    y[0] = 0.0;
+    for (i = 1; i < self->number_of_parameters; ++i) {
+        y[0] += pow(fabs(x[i]), 2 + 4 * (((double)(i - 1))/(self->number_of_parameters - 1)));
+    }
+    y[0] = sqrt(y[0]);
+}
+
+static numbbo_problem_t *differentPowers_problem(const size_t number_of_parameters) {
+    size_t i, problem_id_length;
+    numbbo_problem_t *problem = numbbo_allocate_problem(number_of_parameters, 1, 0);
+    problem->problem_name = numbbo_strdup("different powers function");
+    /* Construct a meaningful problem id */
+    problem_id_length = snprintf(NULL, 0, 
+                                 "%s_%02i", "different powers", (int)number_of_parameters);
+    problem->problem_id = numbbo_allocate_memory(problem_id_length + 1);
+    snprintf(problem->problem_id, problem_id_length + 1, 
+             "%s_%02d", "different powers", (int)number_of_parameters);
+
+    problem->number_of_parameters = number_of_parameters;
+    problem->number_of_objectives = 1;
+    problem->number_of_constraints = 0;
+    problem->evaluate_function = f_differentPowers_evaluate;
+    for (i = 0; i < number_of_parameters; ++i) {
+        problem->lower_bounds[i] = -5.0;
+        problem->upper_bounds[i] = 5.0;
+        problem->best_parameter[i] = 0.0;
+    }
+    /* Calculate best parameter value */
+    f_sharpRidge_evaluate(problem, problem->best_parameter, problem->best_value);
+    return problem;
+}
