11a751e60b97fd8abbc1338ca726a22b4daaac0b
Olaf Mersmann <olafm@p-value.net>
2014-07-08 19:45:45 +0200

    Working f1!

---
+++ build/c/cppdemo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "numbbo.h"
+
+using namespace std;
+
+void my_optimizer(numbbo_problem_t *problem) {
+    static const int budget = 100000;
+    numbbo_random_state_t *rng = numbbo_new_random(0xdeadbeef);
+    double *x = (double *)malloc(problem->number_of_parameters * sizeof(double));
+    double y;
+    for (int i = 0; i < budget; ++i) {
+        for (unsigned int j = 0; j < problem->number_of_parameters; ++j) {
+            const double range = problem->upper_bounds[j] - problem->lower_bounds[j];
+            x[j] = problem->lower_bounds[j] + numbbo_uniform_random(rng) * range;
+        }
+        numbbo_evaluate_function(problem, x, &y);
+    }
+    numbbo_free_random(rng);
+    free(x);
+}
+
+int main(int argc, char **argv) {
+    cout << "Greetings from C++ land." << endl;
+    numbbo_benchmark("toy_suit", "toy_observer", "random_search", my_optimizer);
+    return 0;
+}
