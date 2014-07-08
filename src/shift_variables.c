051b722e140087beaf13a8afc6da2a3b87eb26b8
Olaf Mersmann <olafm@p-value.net>
2014-07-08 18:41:28 +0200

    Allow shifts by an arbitrary vector of values not just a single value.

--- src/shift_variables.c
+++ src/shift_variables.c
@@ -5,8 +5,8 @@
 #include "numbbo_problem.c"
 
 typedef struct {
-    double amount;
-    double *tmp;
+    double *offset;
+    double *shifted_x;
     numbbo_free_function_t old_free_problem;
 } shift_variables_state_t;
 
@@ -17,9 +17,9 @@ static void _sv_evaluate_function(numbbo_problem_t *self, double *x, double *y) 
     shift_variables_state_t *state = (shift_variables_state_t *)problem->state;
 
     for (int i = 0; i < self->number_of_parameters; ++i) {
-        state->tmp[i] = x[i] + state->amount;
+        state->shifted_x[i] = x[i] + state->offset[i];
     }
-    numbbo_evaluate_function(problem->inner_problem, state->tmp, y);
+    numbbo_evaluate_function(problem->inner_problem, state->shifted_x, y);
 }
 
 static void _sv_free_problem(numbbo_problem_t *self) {
@@ -28,24 +28,31 @@ static void _sv_free_problem(numbbo_problem_t *self) {
     assert(problem->state != NULL);
     shift_variables_state_t *state = (shift_variables_state_t *)problem->state;
     
-    numbbo_free_memory(state->tmp);
+    numbbo_free_memory(state->shifted_x);
+    numbbo_free_memory(state->offset);
     state->old_free_problem(self);
 }
 
 /* Shift all variables of ${inner_problem} by ${amount}.
  */
 numbbo_problem_t *shift_variables(numbbo_problem_t *inner_problem,
-                                  const double amount,
+                                  const double *offset,
                                   const bool shift_bounds) {
+    assert(inner_problem != NULL);
+    assert(offset != NULL);
+
+    size_t number_of_parameters = inner_problem->number_of_parameters;
     numbbo_transformed_problem_t *obj =
         numbbo_allocate_transformed_problem(inner_problem);
     numbbo_problem_t *problem = (numbbo_problem_t *)obj;
 
     shift_variables_state_t *state = numbbo_allocate_memory(sizeof(*state));
-    state->amount = amount;
-    state->tmp = (double *)numbbo_allocate_memory(sizeof(double));
+    state->offset = numbbo_duplicate_vector(offset, number_of_parameters);
+    state->shifted_x = numbbo_allocate_vector(number_of_parameters);
     state->old_free_problem = problem->free_problem;
+
     obj->state = state;
+
     problem->evaluate_function = _sv_evaluate_function;
     problem->free_problem = _sv_free_problem;
     return problem;
