9dca1ede34a9443721b68609c646579cd54ade7e
Ouassim AIT ELHARA <ouassimaitelhara@gmail.com>
2014-07-14 20:10:18 +0200

    working on logger...

--- src/bbob2009_observer.c
+++ src/bbob2009_observer.c
@@ -7,14 +7,14 @@ coco_problem_t *bbob2009_observer(coco_problem_t *problem, const char *options) 
     char base_path[NUMBBO_PATH_MAX] = {0};
     char filename[NUMBBO_PATH_MAX] = {0};
     coco_join_path(base_path, sizeof(base_path),
-                     options, "toy_so", coco_get_id(problem), NULL);
+                     options, coco_get_id(problem), NULL);
     if (coco_path_exists(base_path)) {
         coco_error("Result directory exists.");
         return NULL; /* never reached */
     }
     coco_create_path(base_path);
     coco_join_path(filename, sizeof(filename), 
-                     base_path, "first_hitting_times.txt", NULL);
+                     base_path, "bbobexp.dat", NULL);
     problem = bbob2009_logger(problem, filename);
     return problem;
 }
