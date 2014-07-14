4a0b67f6efce71e07004ce96b80f737a918cf990
Ouassim AIT ELHARA <ouassimaitelhara@gmail.com>
2014-07-14 20:10:18 +0200

    working on logger...

--- build/c/demo.cpp
+++ build/c/demo.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-void my_optimizer(numbbo_problem_t *problem) {
+void my_optimizer(coco_problem_t *problem) {
     static const int budget = 100000;
-    const size_t number_of_variables = numbbo_get_number_of_variables(problem);
-    numbbo_random_state_t *rng = numbbo_new_random(0xdeadbeef);
-    double *x = numbbo_allocate_vector(number_of_variables);
-    const double *lower = numbbo_get_smallest_values_of_interest(problem);
-    const double *upper = numbbo_get_largest_values_of_interest(problem);
+    const size_t number_of_variables = coco_get_number_of_variables(problem);
+    coco_random_state_t *rng = coco_new_random(0xdeadbeef);
+    double *x = coco_allocate_vector(number_of_variables);
+    const double *lower = coco_get_smallest_values_of_interest(problem);
+    const double *upper = coco_get_largest_values_of_interest(problem);
     double y;
 
     /* Skip any problems with more than 20 variables */
@@ -18,18 +18,18 @@ void my_optimizer(numbbo_problem_t *problem) {
         return;
 
     for (int i = 0; i < budget; ++i) {
-        for (size_t j = 0; j < numbbo_get_number_of_variables(problem); ++j) {
+        for (size_t j = 0; j < coco_get_number_of_variables(problem); ++j) {
             const double range = upper[j] - lower[j];
-            x[j] = lower[j] + numbbo_uniform_random(rng) * range;
+            x[j] = lower[j] + coco_uniform_random(rng) * range;
         }
-        numbbo_evaluate_function(problem, x, &y);
+        coco_evaluate_function(problem, x, &y);
     }
-    numbbo_free_random(rng);
-    numbbo_free_memory(x);
+    coco_free_random(rng);
+    coco_free_memory(x);
 }
 
 int main(int argc, char **argv) {
     cout << "Greetings from C++ land." << endl;
-    numbbo_benchmark("toy_suit", "toy_observer", "random_search", my_optimizer);
+    coco_benchmark("toy_suit", "toy_observer", "random_search", my_optimizer);
     return 0;
 }
--- src/bbob2009_logger.c
+++ src/bbob2009_logger.c
@@ -14,8 +14,11 @@ static const size_t nbpts_nbevals = 20;
 static const size_t nbpts_fval = 5;
 
 typedef struct {
-    char *path;
-    FILE *logfile;
+    char *path;/*path to the data folder*/
+    FILE *logfile;/*TODO: eventually removed*/
+    FILE *index_file;/*index file*/
+    FILE *fdata_file;/*function value aligned data file*/
+    FILE *tdata_file;/*number of function evaluations aligned data file*/
     long idx_fval_trigger; /* logging target = {10**(i/nbPtsF), i \in Z} */
     double next_target;
     long idx_nbevals_trigger;
@@ -30,9 +33,28 @@ static void bbob2009_logger_update_next_target(bbob2009_logger_t * state){
     return;
 }
 
-static void bbob2009_logger_prepare(bbob2009_logger_t *self){
-    
+static void bbob2009_logger_prepare_files(bbob2009_logger_t *state, double best_value){
+    /*
+        Creates/opens the data files
+        best_value if printed in the header
+     */
+    if(state->fdata_file==NULL){
+        state->fdata_file = fopen(state->path, "w");
+        if (state->fdata_file == NULL) {
+            char *buf;
+            const char *error_format = "bbob2009_logger_prepare() failed to open log file '%s'.";
+            size_t buffer_size = snprintf(NULL, 0, error_format, state->path);
+            buf = (char *)coco_allocate_memory(buffer_size);
+            snprintf(buf, buffer_size, error_format, state->path);
+            coco_error(buf);
+            coco_free_memory(buf); /* Never reached */
+        }
+    }
+    fprintf(state->fdata_file,"%% function evaluation | noise-free fitness - Fopt (%13.12e) | best noise-free fitness - Fopt | measured fitness | best measured fitness | x1 | x2...\n", best_value);
+    return;
 }
+
+
 
 static void bbob2009_logger_evaluate_function(coco_problem_t *self, 
                                               double *x, double *y) {
@@ -42,8 +64,7 @@ static void bbob2009_logger_evaluate_function(coco_problem_t *self,
     coco_evaluate_function(obj->inner_problem, x, y);
     state->number_of_evaluations++;
 
-    /* Open logfile if it is not alread open */
-    if (state->logfile == NULL) {
+    /*if (state->logfile == NULL) {
         state->logfile = fopen(state->path, "w");
         if (state->logfile == NULL) {
             char *buf;
@@ -54,21 +75,21 @@ static void bbob2009_logger_evaluate_function(coco_problem_t *self,
             buf = (char *)coco_allocate_memory(buffer_size);
             snprintf(buf, buffer_size, error_format, state->path);
             coco_error(buf);
-            coco_free_memory(buf); /* Never reached */
+            coco_free_memory(buf);
         }
         fprintf(state->logfile,"%% function evaluation | noise-free fitness - Fopt (%13.12e) | best noise-free fitness - Fopt | measured fitness | best measured fitness | x1 | x2...\n", *(self->best_value));
-    }
+    }*/
     
-    /* Add a line for each hitting level we have reached. */
+    /* Add a line for each hitting level reached. */
     if (y[0] <= state->next_target ){
         size_t i;
-        fprintf(state->logfile, "%ld", state->number_of_evaluations);/*for some reason, it's %.0f in the old code */
-        fprintf(state->logfile, " %+10.9e %+10.9e %+10.9e %+10.9e", y[0], y[0], y[0], y[0]);
+        fprintf(state->fdata_file, "%ld", state->number_of_evaluations);/*for some reason, it's %.0f in the old code */
+        fprintf(state->fdata_file, " %+10.9e %+10.9e %+10.9e %+10.9e", y[0], y[0], y[0], y[0]);
         for (i=0; i < self->number_of_variables; i++) {
-            fprintf(state->logfile, " %+5.4e",x[i]);
+            fprintf(state->fdata_file, " %+5.4e",x[i]);
         }
         
-        fprintf(state->logfile, "\n");
+        fprintf(state->fdata_file, "\n");
         if (state->idx_fval_trigger==INT_MAX)
             /* "jump" directly to the next closest target (to the actual fvalue) from the initial target*/
             state->idx_fval_trigger=ceil(log10(y[0]))*nbpts_fval;
@@ -82,7 +103,7 @@ static void bbob2009_logger_evaluate_function(coco_problem_t *self,
         
     }
     /* Flush output so that impatient users can see progress. */
-    fflush(state->logfile);
+    fflush(state->fdata_file);
 }
 
 static void bbob2009_logger_free_problem(coco_problem_t *self) {
@@ -95,6 +116,18 @@ static void bbob2009_logger_free_problem(coco_problem_t *self) {
     if (state->logfile != NULL) {
         fclose(state->logfile);
         state->logfile = NULL;
+    }
+    if (state->index_file != NULL) {
+        fclose(state->index_file);
+        state->index_file = NULL;
+    }
+    if (state->fdata_file != NULL) {
+        fclose(state->fdata_file);
+        state->fdata_file = NULL;
+    }
+    if (state->tdata_file != NULL) {
+        fclose(state->tdata_file);
+        state->tdata_file = NULL;
     }
     coco_free_memory(obj->state);
     if (obj->inner_problem != NULL) {
@@ -117,14 +150,14 @@ coco_problem_t *bbob2009_logger(coco_problem_t *inner_problem, const char *path)
     problem->free_problem = bbob2009_logger_free_problem;
     state->path = coco_strdup(path);
     state->logfile = NULL; /* Open lazily in logger_evaluate_function(). */
+
+    bbob2009_logger_prepare_files(state, *(problem->best_value));
+    
     
     state->idx_fval_trigger= INT_MAX;
     state->next_target=DBL_MAX;
     state->number_of_evaluations = 0;
     obj->state = state;
-    printf("%s\n", problem->problem_name);
-    printf("%d\n", problem->number_of_variables);
-    
     return problem;
 }
 
