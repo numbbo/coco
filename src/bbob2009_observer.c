c75194cc4d3c36897d18f5b089dd994e1ae20d6e
Ouassim AIT ELHARA <ouassimaitelhara@gmail.com>
2014-07-15 15:17:49 +0200

    bbob_2009_logger.c added bbob2009_logger_error_io

--- src/bbob2009_observer.c
+++ src/bbob2009_observer.c
@@ -4,17 +4,16 @@
 
 coco_problem_t *bbob2009_observer(coco_problem_t *problem, const char *options) {
 
-    char base_path[NUMBBO_PATH_MAX] = {0};
-    char filename[NUMBBO_PATH_MAX] = {0};
-    coco_join_path(base_path, sizeof(base_path),
-                     options, coco_get_id(problem), NULL);
-    if (coco_path_exists(base_path)) {
-        coco_error("Result directory exists.");
-        return NULL; /* never reached */
-    }
-    coco_create_path(base_path);
-    coco_join_path(filename, sizeof(filename), 
-                     base_path, "bbobexp", NULL);/* TODO: pass the correct path as in the old code*/
-    problem = bbob2009_logger(problem, filename);
-    return problem;
+	char base_path[NUMBBO_PATH_MAX] = { 0 };
+	char filename[NUMBBO_PATH_MAX] = { 0 };
+	coco_join_path(base_path, sizeof(base_path), options, coco_get_id(problem),
+			NULL);
+	if (coco_path_exists(base_path)) {
+		coco_error("Result directory exists.");
+		return NULL; /* never reached */
+	}
+	coco_create_path(base_path);
+	coco_join_path(filename, sizeof(filename), base_path, "bbobexp", NULL);/* TODO: pass the correct path as in the old code*/
+	problem = bbob2009_logger(problem, filename);
+	return problem;
 }
