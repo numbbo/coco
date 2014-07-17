6110327116a23721bab4b46ec39de311aad4ca29
Olaf Mersmann <olafm@p-value.net>
2014-07-17 12:45:34 +0200

    Replace Makefile with custom python script.

---
+++ do.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python2.7
+## -*- mode: python -*-
+
+import sys
+import re
+import os
+
+## Change to the root directory of repository and add our tools/
+## subdirectory to system wide search path for modules.
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.abspath('tools'))
+
+from amalgamate import amalgamate
+from platform import make, run
+from platform import copy_file, expand_file, write_file
+from platform import hg_version, hg_revision
+
+core_files = ['src/coco_benchmark.c', 'src/coco_random.c', 
+              'src/coco_generics.c', 'src/coco_c_runtime.c']
+
+################################################################################
+## C
+def build_c():
+    amalgamate(core_files + ['src/coco_c_runtime.c'],  'build/c/coco.c')
+    copy_file('src/coco.h', 'build/c/coco.h')
+    write_file(hg_revision(), "build/c/REVISION")
+    write_file(hg_version(), "build/c/VERSION")
+
+def test_c():
+    build_c()
+    make("build/c", "clean")
+    make("build/c", "all")
+    run('build/c', ['./test_bbob2009'])
+
+################################################################################
+## Python
+def build_python():
+    amalgamate(core_files + ['src/coco_c_runtime.c'],  'build/python/coco/coco.c')
+    copy_file('src/coco.h', 'build/python/coco/coco.h')
+    expand_file('build/python/README.in', 'build/python/README',
+                {'COCO_VERSION': hg_version()})
+    expand_file('build/python/setup.py.in', 'build/python/setup.py',
+                {'COCO_VERSION': hg_version()})
+
+def test_python():
+    build_python()
+    pass
+
+################################################################################
+## R
+def build_r():
+    amalgamate(core_files + ['src/coco_r_runtime.c'],  'build/r/skel/src/coco.c')
+    copy_file('src/coco.h', 'build/r/skel/src/coco.h')
+    expand_file('build/r/skel/DESCRIPTION.in', 'build/r/skel/DESCRIPTION', 
+                {'COCO_VERSION': hg_version()})
+
+def test_r():
+    build_r()
+    pass
+
+def release_c():
+    build_c()
+
+################################################################################
+## Global
+def build():
+    build_c()
+    build_python()
+    build_r()
+
+def test():
+    test_c()
+    test_python()
+    test_r()
+    
+def help():
+    print """COCO framework bootstrap tool.
+
+Usage: do.py <command>
+
+Available commands:
+
+  build_c - Build C framework
+  test_c  - Run minimal test of C components
+  build   - Build C, Python and R modules
+"""
+
+def main(args):
+    if len(args) != 1: 
+        help()
+        sys.exit(0)
+    cmd = args[0]
+    if cmd == 'build_c': build_c()
+    elif cmd == 'test_c': test_c()
+    elif cmd == 'build_python': build_python()
+    elif cmd == 'test_python': test_python()
+    elif cmd == 'build_r': build_r()
+    elif cmd == 'test_r': test_r()
+    elif cmd == 'build': build()
+    elif cmd == 'test': test()
+    else: help()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ tools/platform.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2.7
+## -*- mode: python -*-
+
+## Lots of utility functions to abstract away platform differences.
+
+import os
+from shutil import copyfile
+from subprocess import CalledProcessError, check_output, call, STDOUT
+
+def hg(args):
+    """Run a Mercurial command and return its output. 
+
+    All errors are deemed fatal and the system will quit."""
+    full_command = ['hg']
+    full_command.extend(args)
+    try:
+        output = check_output(full_command).rstrip()
+    except CalledProcessError as e:
+        print 'Failed to execute hg.'
+        sys.exit(-1)
+    return output
+
+def is_dirty():
+    """Return True if the current working copy has uncommited changes."""
+    return hg(['hg', 'id', '-i'])[-1] == '+'
+
+def hg_version():
+    """ Derive the current version number from the latest tag and the
+    number of (local) commits since the tagged revision. """
+    return hg(['log', '-r', '.', '--template', '{latesttag}.{latesttagdistance}'])
+
+def hg_revision():
+    return hg(['id', '-i'])
+
+def run(directory, args):
+    print "RUN\t%s in %s" % (args[0], directory)
+    oldwd = os.getcwd()
+    try:
+        os.chdir(directory)
+        output = check_output(args, stderr=STDOUT)
+    except CalledProcessError as e:
+        print "ERROR:"
+        print e.output
+        sys.exit(-1)
+    finally:
+        os.chdir(oldwd)
+
+def copy_file(source, destination):
+    print "COPY\t%s -> %s" % (source, destination)
+    copyfile(source, destination)
+
+def write_file(string, destination):
+    print "WRITE\t%s" % destination
+    with open(destination, "w") as fd:
+        fd.write(string)
+
+def make(directory, target):
+    """Run make to build a target"""
+    print "MAKE\t%s in %s" % (target, directory)
+    oldwd = os.getcwd()
+    try:
+        os.chdir(directory)
+        output = check_output(['make', target], stderr=STDOUT)
+    except CalledProcessError as e:
+        print "ERROR:"
+        print e.output
+        sys.exit(-1)
+    finally:
+        os.chdir(oldwd)
+
+def expand_file(source, destination, dictionary):
+    print "EXPAND\t%s to %s" % (source, destination)
+    from string import Template
+    with open(source, "r") as fd:
+        content = Template(fd.read())        
+        with open(destination, "w") as outfd:
+            outfd.write(content.safe_substitute(dictionary))
+
