e111d49634735b4b9990015cc3509014bd4be4ad
Olaf Mersmann <olafm@p-value.net>
2014-07-20 01:12:46 +0200

    Implement BBOB f11 and f12.

--- src/f_discus.c
+++ src/f_discus.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
 #include <assert.h>
 
 #include "coco.h"
-
 #include "coco_problem.c"
 
-static void f_discus_evaluate(coco_problem_t *self, double *x, double *y) {
+static void _discus_evaluate(coco_problem_t *self, double *x, double *y) {
     size_t i;
     static const double condition = 1.0e6;
     assert(self->number_of_objectives == 1);
-    y[0] = 0.0;
+
+    y[0] = condition * x[0] * x[0];
     for (i = 1; i < self->number_of_variables; ++i) {
         y[0] += x[i] * x[i];
     }
-    
-    y[0] += condition * x[0] * x[0];
 }
 
 static coco_problem_t *discus_problem(const size_t number_of_variables) {
@@ -23,24 +20,23 @@ static coco_problem_t *discus_problem(const size_t number_of_variables) {
                                                         1, 0);
     problem->problem_name = coco_strdup("discus function");
     /* Construct a meaningful problem id */
-    problem_id_length = snprintf(NULL, 0, 
+    problem_id_length = snprintf(NULL, 0,
                                  "%s_%02i", "discus",
                                  (int)number_of_variables);
     problem->problem_id = coco_allocate_memory(problem_id_length + 1);
-    snprintf(problem->problem_id, problem_id_length + 1, 
+    snprintf(problem->problem_id, problem_id_length + 1,
              "%s_%02d", "discus", (int)number_of_variables);
 
     problem->number_of_variables = number_of_variables;
     problem->number_of_objectives = 1;
     problem->number_of_constraints = 0;
-    problem->evaluate_function = f_discus_evaluate;
+    problem->evaluate_function = _discus_evaluate;
     for (i = 0; i < number_of_variables; ++i) {
         problem->smallest_values_of_interest[i] = -5.0;
         problem->largest_values_of_interest[i] = 5.0;
         problem->best_parameter[i] = 0.0;
     }
     /* Calculate best parameter value */
-    f_discus_evaluate(problem, problem->best_parameter, problem->best_value);
+    _discus_evaluate(problem, problem->best_parameter, problem->best_value);
     return problem;
 }
-
