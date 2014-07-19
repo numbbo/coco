325e22df7a65a9f65377eba78b4e145ec37849e6
Olaf Mersmann <olafm@p-value.net>
2014-07-19 09:27:14 +0200

    Implement BBOB f8.

--- src/bbob2009_suit.c
+++ src/bbob2009_suit.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 
 #include "coco_generics.c"
 
@@ -17,6 +18,7 @@
 #include "brs_transform.c"
 #include "condition_variables.c"
 #include "oscillate_variables.c"
+#include "scale_variables.c"
 #include "shift_variables.c"
 
 /**
@@ -145,6 +147,27 @@ coco_problem_t *bbob2009_suit(const int function_index) {
         fopt = bbob2009_compute_fopt(function_id, instance_id);
         problem = linear_slope_problem(dimension, xopt);
         problem = shift_objective(problem, fopt);
+    } else if (function_id == 8) {
+        double xopt[40], minus_one[40], fopt, factor;
+        bbob2009_compute_xopt(xopt, rseed, dimension);
+        for (i = 0; i < dimension; ++i) {
+            minus_one[i] = -1.0;
+            xopt[i] *= 0.75;
+        }
+        fopt = bbob2009_compute_fopt(function_id, instance_id);
+        /* C89 version of 
+         *   fmax(1.0, sqrt(dimension) / 8.0); 
+         * follows
+         */
+        factor = sqrt(dimension) / 8.0;
+        if (factor < 1.0)
+            factor = 1.0;
+
+        problem = rosenbrock_problem(dimension);
+        problem = shift_variables(problem, minus_one, 0);
+        problem = scale_variables(problem, factor);
+        problem = shift_variables(problem, xopt, 0);
+        problem = shift_objective(problem, fopt);        
     } else {
         return NULL;
     }
---
+++ src/scale_variables.c
@@ -0,0 +1,50 @@
+/*
+ * Scale variables by a given factor.
+ */
+#include <assert.h>
+
+#include "coco.h"
+#include "coco_problem.c"
+
+typedef struct {
+    double factor;
+    double *x;
+} _scv_data_t;
+
+static void _scv_evaluate_function(coco_problem_t *self, double *x, double *y) {
+    size_t i;
+    _scv_data_t *data;
+    coco_problem_t *inner_problem;
+    data = coco_get_transform_data(self);
+    inner_problem = coco_get_transform_inner_problem(self);
+    do {
+        const double factor = data->factor;
+
+        for (i = 0; i < self->number_of_variables; ++i) {
+            data->x[i] = factor * x[i];
+        }
+        coco_evaluate_function(inner_problem, data->x, y);
+    } while(0);
+}
+
+static void _scv_free_data(void *thing) {
+    _scv_data_t *data = thing;
+    coco_free_memory(data->x);
+}
+
+/**
+ * Scale all variables by factor before evaluation.
+ */
+coco_problem_t *scale_variables(coco_problem_t *inner_problem,
+                                const double factor) {
+    _scv_data_t *data;
+    coco_problem_t *self;
+
+    data = coco_allocate_memory(sizeof(*data));
+    data->factor = factor;
+    data->x = coco_allocate_vector(inner_problem->number_of_variables);
+
+    self = coco_allocate_transformed_problem(inner_problem, data, _scv_free_data);
+    self->evaluate_function = _scv_evaluate_function;
+    return self;
+}
