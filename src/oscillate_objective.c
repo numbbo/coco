8a20cf1630f9716ea6c7813b281aa95022575d99
Olaf Mersmann <olafm@p-value.net>
2014-07-29 21:01:00 +0200

    Slight cleanup of objective oscillation.

--- src/oscillate_objective.c
+++ src/oscillate_objective.c
@@ -5,12 +5,17 @@
 #include "coco_problem.c"
 
 static void _oo_evaluate_function(coco_problem_t *self, double *x, double *y) {
+    static const double factor = 0.1;
     coco_evaluate_function(coco_get_transform_inner_problem(self), x, y);
-    if (y[0] > 0){
-    	y[0] = pow(exp(log(y[0])/0.1 + 0.49*(sin(log(y[0])/0.1) + sin(0.79*log(y[0])/0.1))), 0.1);
-    }
-    else if (y[0] < 0){
-    	y[0] = -pow(exp(log(-y[0])/0.1 + 0.49*(sin(0.55 * log(-y[0])/0.1) + sin(0.31*log(-y[0])/0.1))), 0.1);
+    if (y[0] != 0) {
+        double log_y;
+        log_y = log(fabs(y[0])) / factor;
+        if (y[0] > 0) {
+            y[0] = pow(exp(log_y + 0.49*(sin(log_y) + sin(0.79*log_y))), factor);
+        } else  {
+            y[0] = -pow(exp(log_y + 0.49 * (sin(0.55 * log_y) + sin(0.31 * log_y))),
+                        factor);
+        }
     }
 }
 
