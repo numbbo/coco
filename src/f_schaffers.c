dd5b8ee90770f7c2a56aefd5ce063728c97ecf0a
Olaf Mersmann <olafm@p-value.net>
2014-08-16 10:14:14 +0200

    Fix f17.

--- src/f_schaffers.c
+++ src/f_schaffers.c
@@ -8,7 +8,7 @@
 
 /* Schaffers F7 function, transformations not implemented for the moment  */
 
-static void f_schaffers_evaluate(coco_problem_t *self, double *x, double *y) {
+static void _schaffers_evaluate(coco_problem_t *self, double *x, double *y) {
     size_t i;
     assert(self->number_of_variables > 1);
     assert(self->number_of_objectives == 1);
@@ -17,10 +17,9 @@ static void f_schaffers_evaluate(coco_problem_t *self, double *x, double *y) {
     y[0] = 0.0;
     for (i = 0; i < self->number_of_variables - 1; ++i) {
     	const double tmp = x[i] * x[i] + x[i + 1] * x[i + 1];
-    	y[0] += pow(tmp, 0.25) * (1 + pow(sin(50. * pow(tmp, 0.1)), 2.));
-
+    	y[0] += pow(tmp, 0.25) * (1.0 + pow(sin(50.0 * pow(tmp, 0.1)), 2.0));
     }
-    y[0] = pow((1./(self->number_of_variables - 1)) * y[0], 2.);
+    y[0] = pow(y[0] / (self->number_of_variables - 1.0), 2.0);
 }
 
 static coco_problem_t *schaffers_problem(const size_t number_of_variables) {
@@ -39,14 +38,14 @@ static coco_problem_t *schaffers_problem(const size_t number_of_variables) {
     problem->number_of_variables = number_of_variables;
     problem->number_of_objectives = 1;
     problem->number_of_constraints = 0;
-    problem->evaluate_function = f_schaffers_evaluate;
+    problem->evaluate_function = _schaffers_evaluate;
     for (i = 0; i < number_of_variables; ++i) {
         problem->smallest_values_of_interest[i] = -5.0;
         problem->largest_values_of_interest[i] = 5.0;
         problem->best_parameter[i] = 0.0;
     }
     /* Calculate best parameter value */
-    f_schaffers_evaluate(problem, problem->best_parameter, problem->best_value);
+    _schaffers_evaluate(problem, problem->best_parameter, problem->best_value);
     return problem;
 }
 
