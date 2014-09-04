1a3ce4a5f34fbd51b2b40a8fa0f5e198c1460d71
Ouassim AIT ELHARA <ouassimaitelhara@gmail.com>
2014-09-04 15:37:28 +0200

    bbob2009_logger.c now fills the .tdat files. Also added some functions

--- src/bbob2009_logger.c
+++ src/bbob2009_logger.c
@@ -10,7 +10,7 @@
 #include "coco_problem.c"
 #include "coco_strdup.c"
 
-/* static const size_t nbpts_nbevals = 20; */
+static const size_t nbpts_nbevals = 20;
 static const size_t nbpts_fval = 5;
 
 /*TODO: add possibility of adding a prefix to the index files*/
@@ -21,21 +21,59 @@ typedef struct {
     FILE *fdata_file;/*function value aligned data file*/
     FILE *tdata_file;/*number of function evaluations aligned data file*/
     FILE *rdata_file;/*restart info data file*/
-    long idx_fval_trigger; /* logging target = {10**(i/nbPtsF), i \in Z} */
-    double next_target;
-    long idx_nbevals_trigger;
-    long idx_dim_nbevals_trigger;
-    double fTrigger;
-    double evalsTrigger;
+    double f_trigger; /* next upper bound on the fvalue to trigger a log in the .dat file*/
+    long t_trigger; /* next lower bound on nb fun evals to trigger a log in the .tdat file*/
+    long idx_f_trigger; /* allows to track the index i in logging target = {10**(i/nbpts_fval), i \in Z} */
+    long idx_t_trigger; /* allows to track the index i in logging nbevals  = {int(10**(i/nbpts_nbevals)), i \in Z} */
+    long idx_tdim_trigger; /* allows to track the index i in logging nbevals  = {dim * 10**i, i \in Z} */
     long number_of_evaluations;
+    double best_value;
 } _bbob2009_logger_t;
 
 char* _file_header_str="%% function evaluation | noise-free fitness - Fopt (%13.12e) | best noise-free fitness - Fopt | measured fitness | best measured fitness | x1 | x2...\n";
 
-static void _bbob2009_logger_update_next_target(_bbob2009_logger_t *data) {
-    data->next_target = pow(10, data->idx_fval_trigger * 1.0 / nbpts_fval);
+static void _bbob2009_logger_update_f_trigger(_bbob2009_logger_t *data, double fvalue) {
+    
+    /* "jump" directly to the next closest (but larger) target to the current fvalue from the initial target*/
+    if (data->idx_f_trigger == INT_MAX)
+        data->idx_f_trigger = ceil(log10(fvalue)) * nbpts_fval;
+    else
+        data->idx_f_trigger--;
+    
+    data->f_trigger = pow(10, data->idx_f_trigger * 1.0 / nbpts_fval);
+    while (fvalue <= data->f_trigger) {
+        data->idx_f_trigger--;
+        data->f_trigger = pow(10, data->idx_f_trigger * 1.0 / nbpts_fval);
+    }
 }
 
+
+static void _bbob2009_logger_update_t_trigger(_bbob2009_logger_t *data, long number_of_variables) {
+    while(data->number_of_evaluations >= floor(pow(10, (double)data->idx_t_trigger / (double) nbpts_nbevals)))
+        data->idx_t_trigger++;
+    
+    while(data->number_of_evaluations >= number_of_variables * pow(10, (double) data->idx_tdim_trigger))
+        data->idx_tdim_trigger++;
+
+    data->t_trigger = fmin(floor(pow(10, (double)data->idx_t_trigger / (double)nbpts_nbevals)), number_of_variables * pow(10, (double) data->idx_tdim_trigger));
+
+}
+
+
+/**
+ * adds a line to a data file
+ */
+static void _bbob2009_logger_write_data(FILE * target_file, long number_of_evaluations, double nf_delta_fitness, double best_nf_delta_fitness, double measured_fitness, double best_measured_fitness, double * x, long number_of_variables){
+    /* for some reason, it's %.0f in the old code instead of the 10.9e in the documentation*/
+    fprintf(target_file, "%ld %+10.9e %+10.9e %+10.9e %+10.9e", number_of_evaluations, nf_delta_fitness, best_nf_delta_fitness, measured_fitness, best_measured_fitness);
+    if (number_of_variables<22) {
+        size_t i;
+        for (i = 0; i < number_of_variables; i++) {
+            fprintf(target_file, " %+5.4e", x[i]);
+        }
+    }
+    fprintf(target_file, "\n");
+}
 
 /**
  * Error when trying to create the file "path"
@@ -113,7 +151,9 @@ static void _bbob2009_logger_initialize(_bbob2009_logger_t *data,
     fprintf(data->rdata_file,_file_header_str,best_value);
 }
 
-
+/**
+ * Layer added to the transformed-problem evaluate_function by the logger
+ */
 static void _bbob2009_logger_evaluate_function(coco_problem_t *self,
                                                double *x, double *y) {
     _bbob2009_logger_t *data;
@@ -121,34 +161,23 @@ static void _bbob2009_logger_evaluate_function(coco_problem_t *self,
     coco_evaluate_function(coco_get_transform_inner_problem(self), x, y);
     data->number_of_evaluations++;
 
-    /* Add a line for each hitting level reached. */
-    if (y[0] <= data->next_target) {
-        size_t i;
-        /* for some reason, it's %.0f in the old code instead of the 10.9e in the documentation*/
-        fprintf(data->fdata_file, "%ld", data->number_of_evaluations);
-        fprintf(data->fdata_file, " %+10.9e %+10.9e %+10.9e %+10.9e", y[0],
-                y[0], y[0], y[0]);
-        if (self->number_of_variables<22) {
-            for (i = 0; i < self->number_of_variables; i++) {
-                fprintf(data->fdata_file, " %+5.4e", x[i]);
-            }
-        }
-
-        fprintf(data->fdata_file, "\n");
-        if (data->idx_fval_trigger == INT_MAX)
-            /* "jump" directly to the next closest target (to the
-             * actual fvalue) from the initial target
-             */
-            data->idx_fval_trigger = ceil(log10(y[0])) * nbpts_fval;
-        else
-            data->idx_fval_trigger--;
-        _bbob2009_logger_update_next_target(data);
-        while (y[0] <= data->next_target) {
-            data->idx_fval_trigger--;
-            _bbob2009_logger_update_next_target(data);
-        }
-
+    /* Add a line in the .dat file for each hitting level reached. */
+    if (y[0] <= data->f_trigger) {
+        
+        
+        _bbob2009_logger_write_data(data->fdata_file, data->number_of_evaluations, y[0], y[0], y[0], y[0], x, self->number_of_variables);
+        
+        _bbob2009_logger_update_f_trigger(data, y[0]);
     }
+    /* Add a line in the .tdat file each time an fevals trigger is reached. */
+    if (data->number_of_evaluations >= data->t_trigger){
+        _bbob2009_logger_write_data(data->tdata_file, data->number_of_evaluations, y[0], y[0], y[0], y[0], x, self->number_of_variables);
+        _bbob2009_logger_update_t_trigger(data, self->number_of_variables);
+        
+        
+    }
+    
+
     /* Flush output so that impatient users can see progress. */
     fflush(data->fdata_file);
 }
@@ -169,6 +198,9 @@ static void _bbob2009_logger_free_data(void *stuff) {
         fclose(data->tdata_file);
         data->tdata_file = NULL;
     }
+    /*  TODO: write the finalize code for a single run here.  
+     It should write data of the best-ever fitness value (should first be added to the _bbob2009_logger_t struct) and of the final function evaluation and close the data files.
+     */
 }
 
 coco_problem_t *bbob2009_logger(coco_problem_t *inner_problem, const char *path) {
@@ -183,9 +215,13 @@ coco_problem_t *bbob2009_logger(coco_problem_t *inner_problem, const char *path)
     _bbob2009_logger_initialize(data,
                                    *(inner_problem->best_value),
                                    inner_problem->problem_id);
-    data->idx_fval_trigger = INT_MAX;
-    data->next_target = DBL_MAX;
+    data->idx_f_trigger = INT_MAX;
+    data->idx_t_trigger = 0;
+    data->idx_tdim_trigger = 0;
+    data->f_trigger = DBL_MAX;
+    data->t_trigger = 0;
     data->number_of_evaluations = 0;
+    
 
     self = coco_allocate_transformed_problem(inner_problem, data,
                                              _bbob2009_logger_free_data);
