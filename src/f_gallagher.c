88650cd0852bb9cd4f9d2cb95fa6c326fbd999af
Asma Atamna <atamnaasmaines@gmail.com>
2014-11-13 12:05:54 +0100

    modified f_gallagher.c according to the old code.New f22 and old f22 compute different values. Debugging in progress...

--- src/f_gallagher.c
+++ src/f_gallagher.c
@@ -5,9 +5,10 @@
 #include "coco_problem.c"
 #include "bbob2009_legacy_code.c"
 
-const int MAX_NB_PEAKS = 101;
+const int NB_PEAKS_21 = 101;
+const int NB_PEAKS_22 = 21;
 const int MAX_DIM = 40;
-double peaks[MAX_NB_PEAKS * MAX_DIM];
+double peaks[NB_PEAKS_21 * MAX_DIM];
 int compare_doubles(const void *, const void *);
 
 typedef struct {
@@ -19,7 +20,7 @@ typedef struct {
 	coco_free_function_t old_free_problem;
 } _bbob_gallagher_t;
 
-static void _bbob_gallagher_101_evaluate(coco_problem_t *self, double *x, double *y) {
+static void _bbob_gallagher_evaluate(coco_problem_t *self, double *x, double *y) {
 	size_t i, j, k; /*Loop over dim*/
 	/*int var;*//* TODO: If I try to declare a variable here, one test fails...which is weird  */
 	double *tmx;
@@ -98,6 +99,7 @@ static coco_problem_t *bbob_gallagher_problem(const size_t number_of_variables,
 	coco_problem_t *problem;
 	_bbob_gallagher_t *data;
 	double maxcondition = 1000., *arrCondition, fitvalues[2] = {1.1, 9.1};
+	double b, c; /* Parameters for generating local optima. In the old code, they are different in f21 and f22 */
 
 	if (number_of_peaks == 101) {
 		rseed = 21 + 10000 * instance_id;
@@ -117,23 +119,25 @@ static coco_problem_t *bbob_gallagher_problem(const size_t number_of_variables,
 	bbob2009_compute_rotation(data->rotation, rseed, number_of_variables);
 	problem = coco_allocate_problem(number_of_variables, 1, 0);
 	/* Construct a meaningful problem id */
-	if (number_of_peaks == 101) {
+	if (number_of_peaks == NB_PEAKS_21) {
 		problem->problem_name = coco_strdup("BBOB f21");
 		problem_id_length = snprintf(NULL, 0, "%s_%02i", "bbob2009_f21", (int)number_of_variables);
 		problem->problem_id = coco_allocate_memory(problem_id_length + 1);
 		snprintf(problem->problem_id, problem_id_length + 1, "%s_%02d", "bbob2009_f21", (int)number_of_variables);
-	} else {
+		b = 10.; c = 5.;
+	} else if (number_of_peaks == NB_PEAKS_22) {
 		problem->problem_name = coco_strdup("BBOB f22");
 		problem_id_length = snprintf(NULL, 0, "%s_%02i", "bbob2009_f22", (int)number_of_variables);
 		problem->problem_id = coco_allocate_memory(problem_id_length + 1);
 		snprintf(problem->problem_id, problem_id_length + 1, "%s_%02d", "bbob2009_f22", (int)number_of_variables);
-	}
+		b = 9.8; c = 4.9;
+	} /* TODO: else */
 
 	problem->number_of_variables = number_of_variables;
 	problem->number_of_objectives = 1;
 	problem->number_of_constraints = 0;
 	problem->data = data;
-	problem->evaluate_function = _bbob_gallagher_101_evaluate;
+	problem->evaluate_function = _bbob_gallagher_evaluate;
 	for (i = 0; i < number_of_variables; ++i) {
 		problem->smallest_values_of_interest[i] = -5.0;
 		problem->largest_values_of_interest[i] = 5.0;
@@ -171,11 +175,11 @@ static coco_problem_t *bbob_gallagher_problem(const size_t number_of_variables,
 
 	bbob2009_unif(peaks, number_of_variables * number_of_peaks, data->rseed);
 	for (i = 0; i < number_of_variables; ++i) {
-		data->xopt[i] = 0.8 * (10. * peaks[i] - 5.);
+		data->xopt[i] = 0.8 * (b * peaks[i] - c);
 		for (j = 0; j < number_of_peaks; ++j) {
 			data->Xlocal[i][j] = 0.;
 			for (k = 0; k < number_of_variables; ++k) {
-				data->Xlocal[i][j] += data->rotation[i][k] * (10. * peaks[j * number_of_variables + k] - 5.);
+				data->Xlocal[i][j] += data->rotation[i][k] * (b * peaks[j * number_of_variables + k] - c);
 			}
 			if (j == 0){
 				data->Xlocal[i][j] *= 0.8;
@@ -191,7 +195,7 @@ static coco_problem_t *bbob_gallagher_problem(const size_t number_of_variables,
 }
 
 int compare_doubles(const void *a, const void *b) {
-	double temp = peaks[*(const int*)a] - peaks[*(const int*)b]; /* TODO: replace */
+	double temp = peaks[*(const int*)a] - peaks[*(const int*)b]; /* TODO: replace int by size_t? */
 	if (temp > 0)
 		return 1;
 	else if (temp < 0)
