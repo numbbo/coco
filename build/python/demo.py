6b414a312c587600556ff812c4336a82fb01e281
Olaf Mersmann <olafm@p-value.net>
2014-12-05 23:39:53 +0100

    Cleanup Python demo

--- build/python/demo.py
+++ build/python/demo.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import coco
 import numpy as np
 
 def my_optimizer(f, lower_bounds, upper_bounds, budget):
+    print("In optimizer...")
     n = len(lower_bounds)
     delta = upper_bounds - lower_bounds
     x = lower_bounds + np.random.rand(n) * delta
     for i in range(budget):
         y = f(x)
-        
+
 for problem in coco.Benchmark("bbob2009", "bbob2009_observer", "random_search"):
-    print "Optimizing '%s' ... " % str(problem)
+    print("Optimizing '%s' ... " % str(problem))
     my_optimizer(problem,
                  problem.lower_bounds,
                  problem.upper_bounds,
-                 100000)
+                 10000)
     problem.free()
