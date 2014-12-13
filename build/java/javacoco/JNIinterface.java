8b9e47e66d39353873b73ee9446441d7a711f8fa
Asma Atamna <atamnaasmaines@gmail.com>
2014-12-13 17:05:34 +0100

    create separate Problem and Benchmark classes (java)

--- build/java/javacoco/JNIinterface.java
+++ build/java/javacoco/JNIinterface.java
@@ -1,71 +1,10 @@
 package javacoco;
 
-public abstract class JNIinterface {
+public class JNIinterface {
 	
 	/* Load the library */
 	static {
 		System.loadLibrary("JNIinterface");
-	}
-	
-	/* Inner classes */
-	public class Problem {
-		int number_of_variables; // update after coco_get_problem(...) call
-		int number_of_objectives; // update after coco_get_problem(...) call
-		int number_of_constraints; // update after coco_get_problem(...) call
-		double[] smallest_values_of_interst; // update after coco_get_problem(...) call
-		double[] largest_values_of_interst; // update after coco_get_problem(...) call
-		double[] best_value; // update after coco_get_problem(...) call
-		double[] best_parameter; // update after coco_get_problem(...) call
-		String problem_name; // update after coco_get_problem(...) call
-		String problem_id; // update after coco_get_problem(...) call
-		String problem_suit;
-		int function_index;
-		String observer_name;
-		String options; // information to generate data directories
-		
-		/* Constructor(s) */
-		public Problem(String problem_suit, int function_index) {
-			super();
-			this.problem_suit = problem_suit;
-			this.function_index = function_index;
-			this.observer_name = new String("");
-			this.options = new String("");
-		}
-		
-		public Problem(String problem_suit, int function_index, String observer_name, String options) {
-			super();
-			this.problem_suit = new String(problem_suit);
-			this.function_index = function_index;
-			this.observer_name = new String(observer_name);
-			this.options = new String(options);
-		}
-		
-		/* TODO: Getters and setters */
-		
-		/* toString method */
-		@Override
-		public String toString() {
-			if (this.problem_id != null)
-				return this.problem_id;
-			else
-				return "finalized/invalid problem";
-		}
-	}
-	
-	public class Benchmark {
-		String problem_suit;
-		String observer;
-		String options;
-		int function_index;
-		
-		/* Constructor */
-		public Benchmark(String problme_suit, String observer, String options) {
-			super();
-			this.problem_suit = problme_suit;
-			this.observer = observer;
-			this.options = options;
-			this.function_index = 0;
-		}
 	}
 	
 	/* Class method */
@@ -75,7 +14,6 @@ public abstract class JNIinterface {
 		return problem;
 	}
 	
-
 	/* Native method */
-	public native double[] coco_evaluate_function(Problem p, double[] x);
+	public static native double[] coco_evaluate_function(Problem p, double[] x);
 }
