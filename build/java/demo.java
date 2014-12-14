55de9ed512322994e3422e10a6242dc80495249e
Asma Atamna <atamnaasmaines@gmail.com>
2014-12-14 14:41:05 +0100

    remove files in build/java

--- build/java/demo.java
+++ build/java/demo.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import javacoco.*;
 
 /* Draft: determine native methods to declare */
 
@@ -15,8 +14,7 @@ public class demo {
             for (j = 0; j < n; j++){
                 x[j] = lower_bounds[j] + (upper_bounds[j] - lower_bounds[j]) * r.nextDouble();
             }
-            JNIinterface my_interface = new JNIinterface();
-            y = my_interface.coco_evaluate_function(problem, x);
+            y = JNIinterface.cocoEvaluateFunction(problem, x);
         }
         
         
@@ -26,14 +24,11 @@ public class demo {
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-    	JNIinterface my_interface = new JNIinterface();
-        Benchmark my_benchmark = new Benchmark("bbob2009", "bbob_observer", "random_search"); // parameters to be defined
-        int i;
-        for (i = 0; i < 500; i++) {
-            Problem problem = my_interface.next_problem(my_benchmark);
-            JNIinterface.coco_evaluate_function(problem, {1., 2.});
+        Benchmark my_benchmark = new Benchmark("bbob2009", "bbob2009_observer", "random_search"); // parameters to be defined
+        while (true) {
+            Problem problem = JNIinterface.nextProblem(my_benchmark);
             System.out.println("Optimizing " + problem.toString());
-            my_optimizer(problem, JNIinterface.coco_get_smallest_values_of_interest(problem), JNIinterface.coco_get_largest_values_of_interest(problem), 10000);
+            my_optimizer(problem, JNIinterface.cocoGetSmallestValuesOfInterest(problem), JNIinterface.cocoGetLargestValuesOfInterest(problem), 10000);
         }
         
         
