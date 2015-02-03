f931b5d8458dd28963e4d4f4228605cfb9fbf886
Asma Atamna <atamnaasmaines@gmail.com>
2015-02-03 18:17:07 +0100

    add cocoGetSmallestValuesOfInterest.c to build/matlab/

--- build/matlab/nextProblem.m
+++ build/matlab/nextProblem.m
@@ -1,4 +1,5 @@
 function Pr = nextProblem(benchmark)
+    % Handle exceptions
     Pr = Problem(benchmark.problem_suit, benchmark.function_index, benchmark.observer, benchmark.options);
     benchmark.function_index = benchmark.function_index + 1;
 end
