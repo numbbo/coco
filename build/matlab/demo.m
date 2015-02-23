4ef50b31da97210c6e416dfb1e1a1da25a13f939
Nikolaus Hansen <nikolaus.hansen@gmail.com>
2015-02-23 02:20:29 +0100

    MAX_EVALUATIONS in demo.m

--- build/matlab/demo.m
+++ build/matlab/demo.m
@@ -1,9 +1,10 @@
+MAX_EVALUATIONS = 1000;
 my_benchmark = Benchmark('bbob2009', 'bbob2009_observer', 'random_search');
 while true
     try
         problem = nextProblem(my_benchmark);
         disp(['Optimizing ', problem.toString()]);
-        my_optimizer(problem, cocoGetSmallestValuesOfInterest(problem), cocoGetLargestValuesOfInterest(problem), 1000);
+        my_optimizer(problem, cocoGetSmallestValuesOfInterest(problem), cocoGetLargestValuesOfInterest(problem), MAX_EVALUATIONS);
     catch e
         disp(e.message);
         return
