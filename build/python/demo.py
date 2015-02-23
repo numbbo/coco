988649cbc33e14374ce153948691b508ee411264
Nikolaus Hansen <nikolaus.hansen@gmail.com>
2015-02-23 14:21:26 +0100

    demo.py: coco -> cocoex, various changes towards a better interface (running, but still in the works)

--- build/python/demo.py
+++ build/python/demo.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from coco import Benchmark
+from cocoex import Benchmark
 import numpy as np
 try: range = xrange  # let range always be an iterator
 except NameError: pass
@@ -22,19 +22,55 @@ def my_optimizer(fun, lower_bounds, upper_bounds, budget):
     return x_min
 
 if 11 < 3:
-    # simplest use case 
-    for problem in Benchmark("bbob2009", "bbob2009_observer", "random_search"):
+    # generic usecase, possible if my_optimizer can be cast into a coco_optimizer_t *
+    # which might often not be a straight forward type conversion, because the
+    # optimizer takes a function (pointer) as input
+    print('Smallest usecase')
+    Benchmark(my_optimizer,
+              "bbob2009", "instances:1-5",  # of 15 instances (not instance nb)
+              "bbob20009_observer", "folder:random_search, verbosity:1")
+
+if 1 < 3:
+    # generic usecase possible in all languages
+    # TODO: implement method next_problem_index
+    print('Generic usecase')
+    bm = Benchmark("bbob2009", "", # "instances:1-5, dimensions:2-20", 
+                   "bbob2009_observer", "random_search") #"folder:random_search, verbosity:1")
+    problem_index = -1
+    while True:
+        problem_index = bm.next_problem_index(problem_index)
+        if problem_index < 0:
+            break
+        problem = bm.get_problem(problem_index)  # this should give a console message by the observer
+        print("on '%s' ... " % (str(problem)), end='')
         my_optimizer(problem,
                      problem.lower_bounds,
                      problem.upper_bounds,
                      MAXEVALS)
-        print("done with '%s' ... " % str(problem))
+        print("done")
+        problem.free()  # this should give a console message by the observer
+        # preferably free would not be necessary, but this might not be possible
+
+if 11 < 3:
+    # simple use case, somewhat Python specific, doesn't add much to the above
+    # TODO: add suit options to this interface
+    print('Pythonic usecase')
+    for problem in Benchmark("bbob2009", "", # here go the suit options
+                             "bbob2009_observer", "random_search"):
+        print("on '%s' ... " % (str(problem)), end='')
+        my_optimizer(problem,
+                     problem.lower_bounds,
+                     problem.upper_bounds,
+                     MAXEVALS)
+        print("done")
         problem.free()  # not strictly necessary (depends on the observer) 
     print("done.")
 
-elif 1 < 3:
+if 11 < 3:
+    # depreciated
     # use case using problem_index which allows to pick and choose (e.g. to parallelize experiments) 
-    bm = Benchmark("bbob2009", "bbob2009_observer", "random_search")
+    print('Depreciated usecase')
+    bm = Benchmark("bbob2009", "bbob2009_observer", "", "random_search")
     problem_index = 0
     while True:
         if 11 < 3 and ((problem_index + 0) % 5):
@@ -52,6 +88,7 @@ elif 1 < 3:
     print("done.")
 
 if 11 < 3:
+    # depreciated, the selection process should be implemented on the benchmark side
     # use case with "random access" via dimension, function, instance, method coco.problem_index is not implemented yet
     raise NotImplementedError
     bm = Benchmark("bbob2009", "bbob2009_observer", "random_search")
