d109707ace12622ea7b939326bca28e4e427360f
Nikolaus Hansen <nikolaus.hansen@gmail.com>
2015-02-23 14:00:13 +0100

    newline removed

--- src/bbob2009_observer.c
+++ src/bbob2009_observer.c
@@ -4,7 +4,6 @@
 
 coco_problem_t *bbob2009_observer(coco_problem_t *problem,
                                   const char *options) {
-
   if (problem == NULL)
     return problem;
   coco_create_path(options);
