6059edb4a311cd766db34baaa6e63738c85aa493
Nikolaus Hansen <nikolaus.hansen@gmail.com>
2015-02-25 18:26:30 +0100

    comment added

--- src/bbob2009_observer.c
+++ src/bbob2009_observer.c
@@ -6,6 +6,7 @@ coco_problem_t *bbob2009_observer(coco_problem_t *problem,
                                   const char *options) {
   if (problem == NULL)
     return problem;
+  /* TODO: parse options that look like "folder:foo; verbosity:bar" */
   coco_create_path(options);
   problem = bbob2009_logger(problem, options);
   return problem;
