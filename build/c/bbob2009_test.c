11c1fbb2833e3a8478e9e70840fecfd319ed9c8e
Nikolaus Hansen <nikolaus.hansen@gmail.com>
2015-03-01 19:28:12 +0100

    improved bbob2009_test.c, otherwise remarks and minor cosmetics

--- build/c/bbob2009_test.c
+++ build/c/bbob2009_test.c
@@ -9,13 +9,17 @@
 #include "coco.h"
 
 int main(int argc, char **argv) {
-  int function_id = 0;
+  int problem_idx = -1;
+  const char *suite = "bbob2009";
   coco_problem_t *problem = NULL;
 
-  while (NULL != (problem = coco_get_problem("bbob2009", function_id++))) {
-    printf("%4i: %s: f=%02i i=%02i\n", function_id,
-           coco_get_problem_id(problem), bbob2009_get_function_id(problem),
-           bbob2009_get_instance_id(problem));
+  while ((problem_idx = coco_next_problem_index(suite, problem_idx, "")) >= 0) {
+    problem = coco_get_problem(suite, problem_idx);
+    if (problem != NULL) {
+      printf("%4i: %s\n", problem_idx, coco_get_problem_id(problem));
+      coco_free_problem(problem);
+    } else
+      printf("problem %4i not found\n", problem_idx);
   }
   return 0;
 }
