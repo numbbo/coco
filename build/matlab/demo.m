c06c2e4e33648ad4f9e439898254fac8f130590a
Asma Atamna <atamnaasmaines@gmail.com>
2015-03-10 18:06:55 +0100

    new demo.m now works, still doesn't interface to matlab optimizers

--- build/matlab/demo.m
+++ build/matlab/demo.m
@@ -1,12 +1,22 @@
-MAX_EVALUATIONS = 100;
+MAX_EVALS = 100;
+number_of_batches = 1;
+current_batch = 1;
 my_benchmark = Benchmark('bbob2009', '', 'bbob2009_observer', 'random_search');
-problem_index = -1
+problem_index = -1;
 while true
     try
         problem_index = nextProblemIndex(my_benchmark, problem_index);
+        if (problem_index < 0)
+            break;
+        end
+        if (mod(problem_index + current_batch - 1, number_of_batches)~= 0)
+            continue;
+        end
         problem = getProblem(my_benchmark, problem_index);
-        disp(['Optimizing ', problem.toString()]);
-        my_optimizer(problem, problem.lower_bounds, problem.upper_bounds, MAX_EVALUATIONS);
+        %disp(['Optimizing ', problem.toString()]);
+        my_optimizer(problem, problem.lower_bounds, problem.upper_bounds, MAX_EVALS);
+        disp(['Done with problem ', problem.toString(), '...']);
+        free(problem);
     catch e
         disp(e.message);
         return
