319b2904aa1860fc3202a131a1a0f962c0ca2a8d
Nikolaus Hansen <nikolaus.hansen@gmail.com>
2015-03-26 21:03:14 +0100

    code cleaning in coco_strdup.c

--- src/coco_strdup.c
+++ src/coco_strdup.c
@@ -27,6 +27,10 @@ char *coco_strdup(const char *string) {
   memcpy(duplicate, string, len + 1);
   return duplicate;
 }
+/**
+ * Formatted string duplication. Optional arguments are
+ * used like in sprintf. 
+ */ 
 char * coco_strdupf(const char *str, ...) {
   va_list args;
   char *s;
@@ -36,21 +40,40 @@ char * coco_strdupf(const char *str, ...) {
   va_end(args);
   return s;
 }
+
+#define coco_vstrdupf_buflen 444
+/**
+ * va_list version of formatted string duplication coco_strdupf()
+ */
 char * coco_vstrdupf(const char *str, va_list args) {
-  static char buf[444];
-#if 1 /* this might not be defined on very old systems */
-  vsnprintf(buf, 442, str, args); /* apparently args cannot be used another time */
-#else /* less save alternative */
-  assert(strlen(str) < 222);
-  vsprintf(buf, str, args); 
+  static char buf[coco_vstrdupf_buflen];
+  long written; 
+  /* apparently args can only be used once, therefore
+   * len = vsnprintf(NULL, 0, str, args) to find out the
+   * length does not work. Therefore we use a buffer
+   * which limits the max length. Longer strings should
+   * never appear anyway, so this is rather a non-issue. */
+  
+#if 1 
+  written = vsnprintf(buf, coco_vstrdupf_buflen - 2, str, args); 
+  if (written < 0)
+    coco_error("coco_vstrdupf(): vsnprintf failed on '%s'", str);
+#else /* less save alternative, if vsnprintf is not available */
+  assert(strlen(str) < coco_vstrdupf_buflen / 2 - 2);
+  written = vsprintf(buf, str, args); 
+  if (written < 0)
+    coco_error("coco_vstrdupf(): vsprintf failed on '%s'", str);
 #endif
+  if (written > coco_vstrdupf_buflen - 3)
+    coco_error("coco_vstrdupf(): A suspiciously long string is tried to being duplicated '%s'", buf);
   return coco_strdup(buf);
 }
+#undef coco_vstrdupf_buflen
 
 /**
  * coco_strconcat(string1, string2):
  *
- * Return a concatenate copy of ${string1} + ${string1}. 
+ * Return a concatenate copy of ${string1} + ${string2}. 
  * The caller is responsible for free()ing the memory allocated
  * using coco_free_memory().
  */
@@ -65,9 +88,9 @@ char *coco_strconcat(const char *s1, const char *s2) {
 }
 
 /**
- * return index where ${seq} occurs in ${base}, -1 if it doesn't.
+ * return first index where ${seq} occurs in ${base}, -1 if it doesn't.
  *
- * If there is an equivalent standard C function, this should be removed. 
+ * If there is an equivalent standard C function, this can/should be removed. 
  */
 long coco_strfind(const char *base, const char *seq) {
   const size_t strlen_seq = strlen(seq);
@@ -82,7 +105,8 @@ long coco_strfind(const char *base, const char *seq) {
       }
       if (j == strlen_seq) {
         if (i > 1e9)
-          coco_error("coco_strfind(): strange values observed i=%lu, j=%lu, strlen(base)=%lu", i, j, strlen(base));
+          coco_error("coco_strfind(): strange values observed i=%lu, j=%lu, strlen(base)=%lu",
+                     i, j, strlen(base));
         return (long)i;
       }
     }
