ce3872f6c909090b3fb7d334971c41ae4eaac7ef
Phillipe Sampaio <phillipece@gmail.com>
2015-10-17 08:25:28 +0200

    Update demo.c
    Demo for constraint extension

--- build/c/demo.c
+++ build/c/demo.c
@@ -4,15 +4,6 @@
 
 #include "coco.h"
 
-/* Timing of random_search on a MacBook Pro (2014) [s]: 
-    evals=       [ 1e2, 1e3, 1e4, 2e4, 4e4  ],  # on about 2e3 problems
-    verbose=     [  2,  10,   80, 152, 299, ],
-    quiet=       [  2,   9,   78,           ],
-    no_observer= [  1,   7,   73, 146, 291  ],
-    old_code =   [ -1, -1,    65, ]
-    
-    ==> 7.5e-3s/eval ~ 1e-2s / eval ~ 10s / 1e3evals ~ 2h / 1e6evals ~ 90d / 1e9evals
- */ 
 static coco_problem_t *CURRENT_COCO_PROBLEM; /* used in objective_function */
 
 /**************************************************
@@ -25,15 +16,25 @@ double objective_function_example(const double *x) {
     coco_evaluate_function(CURRENT_COCO_PROBLEM, x, &y);
     return y;
 }
+
+typedef double (*constraint_function_example_t) (const double *);
+double constraint_function_example(const double *x) {
+    double y;
+    coco_evaluate_constraint(CURRENT_COCO_PROBLEM, x, &y);
+    return y;
+}
 void random_search(size_t dimension,
                    objective_function_example_t fun,
+                   constraint_function_example_t cons,
                    const double *lower,
                    const double *upper,
                    long budget,
-                   double final_target) {
+                   double final_target,
+		   double feasibility_threshold) {
   coco_random_state_t *rng = coco_new_random(0xdeadbeef); /* use coco fcts for convenience */
   double *x = coco_allocate_vector(dimension);
-  double y;
+  double objective_function_value;
+  double constraint_function_value;
   long i;
 
   for (i = 0; i < budget; ++i) {
@@ -41,12 +42,22 @@ void random_search(size_t dimension,
     for (j = 0; j < dimension; ++j) {
       x[j] = lower[j] + coco_uniform_random(rng) * (upper[j] - lower[j]);
     }
-    y = fun(x);
+    objective_function_value = fun(x);
+    constraint_function_value = cons(x);
+    
+    printf("\n\nIteration i = %d\n\n", i);
+    for (j = 0; j < dimension; ++j) {
+      printf("x[%d] = %f\n", j, x[j]);
+    
+    }
+    printf("f(x) = %f\n", objective_function_value);
+    printf("c(x) = %f\n", constraint_function_value);
+    
      /* To be of any real use, we would need to retain the best x-value here.
       * For benchmarking purpose the implementation suffices, as the
       * observer takes care of bookkeeping. 
       */
-    if (y <= final_target)
+    if (objective_function_value <= final_target && constraint_function_value <= feasibility_threshold)
       break;
   }
   coco_free_random(rng);
@@ -57,9 +68,9 @@ void random_search(size_t dimension,
  *   Set up the experiment                    
  **************************************************/
 static const long MAX_BUDGET = 1e2;  /* work on small budgets first */
-static const char * SUITE_NAME       = "bbob2009";
+static const char * SUITE_NAME       = "consprob2015";
 static const char * SUITE_OPTIONS    = ""; /* e.g.: "instances:1-5; dimensions:-20" */
-static const char * OBSERVER_NAME    = "bbob2009_observer"; /* writes data */
+static const char * OBSERVER_NAME    = "consprob2015_observer"; /* writes data */
 /* static const char * OBSERVER_NAME = "no_observer"; / * writes no data */
 static const char * OBSERVER_OPTIONS = "random_search_on_bbob2009"; /* future: "folder:random_search; verbosity:1" */
 static const char * SOLVER_NAME      = "random_search"; /* for the choice in coco_optimize below */
@@ -83,10 +94,9 @@ void objective_function(const double *x, double *y) {
 }
 /* Minimal solver definition only to avoid compile/link errors/warnings below for my_solver */
 void my_solver(objective_function_t func, const double *initial_x, size_t dim, long budget) {
-  double y;
-  (void)dim; (void)budget;
-  func(initial_x, &y);
+  double y; func(initial_x, &y); 
 }
+
 /**
  * Finally, coco_optimize calls, depending on SOLVER_NAME, one
  * of the defined optimizers (e.g. random_search, my_solver, ...),
@@ -99,6 +109,7 @@ void coco_optimize(coco_problem_t *problem) { /* should at the least take budget
   const double * ubounds = coco_get_largest_values_of_interest(problem);
   double * initial_x = coco_allocate_vector(coco_get_number_of_variables(problem));
   const double final_target = coco_get_final_target_fvalue1(problem);
+  const double feasibility_threshold = 1e-8;
   long remaining_budget; 
   
   coco_get_initial_solution(problem, initial_x);
@@ -107,8 +118,8 @@ void coco_optimize(coco_problem_t *problem) { /* should at the least take budget
   while ((remaining_budget = MAX_BUDGET - coco_get_evaluations(problem)) > 0) {
     /* call the solver */
     if (strcmp("random_search", SOLVER_NAME) == 0) { /* example case, no need to modify */
-      random_search(dimension, objective_function_example,
-                    lbounds, ubounds, remaining_budget, final_target);
+      random_search(dimension, objective_function_example, constraint_function_example,
+                    lbounds, ubounds, remaining_budget, final_target, feasibility_threshold);
     } else if (strcmp("my_solver", SOLVER_NAME) == 0) {
       /* MODIFY this call according to objective_function_t and solver's needs */
       my_solver(objective_function, /* MODIFY my_solver to your_solver ... */
@@ -121,19 +132,16 @@ void coco_optimize(coco_problem_t *problem) { /* should at the least take budget
 /**************************************************
  *   run the experiment                       
  **************************************************/
-#if 0
 int main() {
-  coco_benchmark(SUITE_NAME, OBSERVER_NAME, OBSERVER_OPTIONS,
-                 coco_optimize);
-  return 0;
-}
-#elif 0
-int main() {  /* short example, also nice to read */
+  
   coco_problem_t *problem;
   int problem_index;
-  
-  for (problem_index = 0; problem_index >= 0;
-       problem_index = coco_next_problem_index(SUITE_NAME, problem_index, SUITE_OPTIONS)) {
+    
+  for (problem_index = -1; ; ) {
+    problem_index = coco_next_problem_index(SUITE_NAME, problem_index, SUITE_OPTIONS); 
+    if (problem_index < 0)
+      break;
+    
     problem = coco_get_problem(SUITE_NAME, problem_index);
     problem = coco_observe_problem(OBSERVER_NAME, problem, OBSERVER_OPTIONS);
     coco_optimize(problem);
@@ -143,62 +151,28 @@ int main() {  /* short example, also nice to read */
   if (NUMBER_OF_BATCHES > 1) printf(" batch %d/%d.\n", CURRENT_BATCH, NUMBER_OF_BATCHES);
   else printf(".\n");
   return 0;
-}
-
-#elif 1
-int main(void) { /* longer example supporting several batches */
-  coco_problem_t * problem;
-  long problem_index = coco_next_problem_index(
-                        SUITE_NAME, -1, SUITE_OPTIONS); /* next(-1) == first */
-  if (NUMBER_OF_BATCHES > 1)
-    printf("Running only batch %d out of %d batches for suite %s\n",
-           CURRENT_BATCH, NUMBER_OF_BATCHES, SUITE_NAME);
-  for ( ; problem_index >= 0;
-       problem_index = coco_next_problem_index(SUITE_NAME, problem_index, SUITE_OPTIONS)
-      ) {
-    /* here we reject indices from other batches */
-    if (((problem_index - CURRENT_BATCH + 1) % NUMBER_OF_BATCHES) != 0)
-      continue;
-    problem = coco_get_problem(SUITE_NAME, problem_index);
-    problem = coco_observe_problem(OBSERVER_NAME, problem, OBSERVER_OPTIONS);
-    if (problem == NULL) {
-      printf("problem with index %ld not found, skipped", problem_index);
-      continue;
-    }
-    coco_optimize(problem); /* depending on verbosity, this gives a message from the observer */
-    coco_free_problem(problem);  
-  }
-  printf("Done with suite '%s' (options '%s')", SUITE_NAME, SUITE_OPTIONS);
-  if (NUMBER_OF_BATCHES > 1) printf(" batch %d/%d.\n", CURRENT_BATCH, NUMBER_OF_BATCHES);
-  else printf(".\n");
-  return 0;
-}
-
-#elif 1
-/* Interface via dimension, function-ID and instance-ID. This does not translate
-   directly to different languages or benchmark suites. */
-int main(void) {
-  long problem_index;
-  int function_id, instance_id, dimension_idx;
-  coco_problem_t * problem;
   
-  /*int *functions;
-  int dimensions[] = {2,3,5,10,20,40};
-  int *instances;*/
-  for (dimension_idx = 0; dimension_idx < 6; dimension_idx++) {/*TODO: find way of using the constants in bbob200_suite*/
-      for (function_id = 0; function_id < 24; function_id++) {
-          for (instance_id = 0; instance_id < 15; instance_id++) { /* this is specific to 2009 */
-              problem_index = bbob2009_encode_problem_index(function_id, instance_id, dimension_idx);
-              problem = coco_get_problem(SUITE_NAME, problem_index);
-              problem = coco_observe_problem(OBSERVER_NAME, problem, OBSERVER_OPTIONS);
-              if (problem == NULL)
-                break;
-              coco_optimize(problem);
-              coco_free_problem(problem);
-          }
-      }
-    /*bbob2009_get_problem_index(functions[ifun], dimensions[idim], instances[iinst]); */
-  }
+  /*
+  
+  size_t i;
+  coco_problem_t *problem = NULL;
+  int problem_index; // Is this useful when we are not testing with a benchmark suite?
+  const size_t dimension = 3;
+  double xopt[dimension], fopt;
+  
+  fopt = 10;
+  for (i = 0; i < dimension; i++) { xopt[i] = 5.0; }
+
+  problem = sphere_problem(dimension);
+  problem = oscillate_variables(problem);
+  problem = shift_variables(problem, xopt, 0);
+  problem = shift_objective(problem, fopt);
+  problem = coco_observe_problem(OBSERVER_NAME, problem, OBSERVER_OPTIONS);
+  coco_optimize(problem);
+  coco_free_problem(problem);
+  
   return 0;
+  
+  */
 }
-#endif
+
