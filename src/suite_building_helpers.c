677a187617ec1efa9c8e878ebb2a8977740da8b8
nikohansen <nikolaus.hansen@gmail.com>
2015-10-26 21:30:03 +0100

    revised the problem building helper function in src/suite_building_helpers.c

--- src/suite_building_helpers.c
+++ src/suite_building_helpers.c
@@ -55,6 +55,122 @@ bbob2009_problem_id(const char *name, size_t number_of_variables) {
 }
 
 /**
+ * coco_problem_t *coco_create_incomplete_named_bounded_problem(
+ *                            const char * problem_id,  // "unique" ID, only benign characters
+ *                            const char * problem_name,
+ *                            coco_evaluate_function_t fct,
+ *                            size_t number_of_variables,
+ *                            double smallest_value_of_interest,
+ *                            double largest_value_of_interest,
+ *                            double best_parameter):
+ *
+ * Return a coco problem, which needs to be completed by setting
+ * ->evaluate_function and/or ->evaluate_constraints and ->best_value
+ * e.g. via coco_evaluate(this, problem->best_parameter, problem->best_value).
+ * 
+ * Ideally, the best_parameter of a "raw" problem is all-zeros. 
+ *
+ * Example:
+ *
+ *    coco_problem_t *problem = coco_create_incomplete_named_bounded_problem(
+ *                          "sphere_20", "sphere", 20, 1, 0, -5, 5, 0);
+ *
+ * Details: this is yet a tentative name/interface.
+ * FIXME: find a better interface for best_parameter?
+ * FIXME: the relation between values of interest and best_parameter_value
+ */
+coco_problem_t *coco_create_incomplete_named_bounded_problem(
+                            const char * problem_id,
+                            const char * problem_name,
+                            size_t dimension,
+                            size_t number_of_objectives,
+                            size_t number_of_constraints,
+                            double smallest_value_of_interest,
+                            double largest_value_of_interest,
+                            double best_parameter) {
+  size_t i;
+  coco_problem_t *problem = coco_allocate_problem(dimension, number_of_objectives, number_of_constraints);
+  assert(problem->number_of_objectives == number_of_objectives);
+  assert(problem->number_of_constraints == number_of_constraints);
+  
+  problem->problem_id = coco_strdup(problem_id);
+  problem->problem_name = coco_strdup(problem_name);
+
+  for (i = 0; i < number_of_variables; ++i) {
+    problem->smallest_values_of_interest[i] = smallest_value_of_interest;
+    problem->largest_values_of_interest[i] = largest_value_of_interest;
+    problem->best_parameter[i] = best_parameter;
+  }
+  return problem;
+}
+
+/***************** EXAMPLE: BENT CIGAR PROBLEM ****************/
+/*** generic definition without any dependencies on COCO ***/
+static double b3bob2009_raw_bent_cigar(size_t dimension, const double *x) {
+  static const double condition = 1.0e6;
+  size_t i;
+  double res;
+
+  res = x[0] * x[0];
+  for (i = 1; i < dimension; ++i) {
+    res += condition * x[i] * x[i];
+  }
+  return res;
+}
+
+/*** transform into coco_evaluate_function_t type ***/
+static void b3bob2009_raw_bent_cigar_evaluate(coco_problem_t *self, const double *x, double *y) {
+  assert(coco_get_number_of_objectives(self) == 1);
+  y[0] = b2bob2009_raw_bent_cigar(coco_get_number_of_variables(self), x);
+}
+
+/*** define as coco_problem_t ***/
+static coco_problem_t *b3bob2009_raw_bent_cigar_problem(const size_t number_of_variables) {
+  char *problem_id = bbob2009_problem_id("bent_cigar", number_of_variables);
+  coco_problem_t *problem = coco_create_incomplete_named_bounded_problem(
+                                problem_id, "bent cigar",
+                                number_of_variables, 1, 0, -5, 5, 0);
+  coco_free_memory(problem_id);
+  problem->evaluate_function = b3bob2009_raw_bent_cigar_evaluate;
+  coco_evaluate(problem, problem->best_parameter, problem->best_value); /* Calculate best parameter value */
+  return problem;
+}
+
+/*** transform into final bbob2009 problem in coco format ***/
+static coco_problem_t *
+b3bob2009_bent_cigar_problem(long dimension_, long instance_id) {
+    const int function_id = 12;
+    const size_t dimension = (size_t)dimension_; /* prevent subtle warnings */
+    double *M = coco_allocate_vector(dimension * dimension);
+    double *b = coco_allocate_vector(dimension);
+    double *xopt = coco_allocate_vector(dimension);
+    double fopt;
+    double **rot1;
+    long rseed = function_id + 10000 * instance_id;
+
+    coco_problem_t *problem; 
+    
+    fopt = bbob2009_compute_fopt(function_id, instance_id);
+    bbob2009_compute_xopt(xopt, rseed + 1000000, dimension_);
+
+    rot1 = bbob2009_allocate_matrix(dimension, dimension);
+    bbob2009_compute_rotation(rot1, rseed + 1000000, dimension_);
+    bbob2009_copy_rotation_matrix(rot1, M, b, dimension);
+    bbob2009_free_matrix(rot1, dimension);
+
+    problem = b3bob2009_raw_bent_cigar_problem(dimension);
+    problem = shift_objective(problem, fopt); /* these have been renamed */
+    problem = affine_transform_variables(problem, M, b, dimension);
+    problem = asymmetric_variable_transform(problem, 0.5);
+    problem = affine_transform_variables(problem, M, b, dimension);
+    problem = shift_variables(problem, xopt, 0);
+    coco_free_memory(M);
+    coco_free_memory(b);
+    coco_free_memory(xopt);
+    return problem;
+}
+
+/**
  * coco_problem_t *coco_allocate_so_problem_sss(
  *                            const char * problem_id,  // "unique" ID, only benign characters
  *                            const char * problem_name,
@@ -64,6 +180,7 @@ bbob2009_problem_id(const char *name, size_t number_of_variables) {
  *                            double largest_value_of_interest,
  *                            double best_parameter):
  *
+ * Obsolete, to be removed. 
  * Return a single-objective coco problem constructed from a function and
  * scalar values for bounds and best parameter.
  *
