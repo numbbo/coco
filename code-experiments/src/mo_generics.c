aa3cfab12962c387dbbfaee2de3c5dbadb640c82
Tea Tušar <tea.tusar@inria.fr>
2015-11-19 10:02:49 +0100

    Preparing input/output for MO indicators.

--- code-experiments/src/mo_generics.c
+++ code-experiments/src/mo_generics.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include "coco.h"
 
-#if 0 /* To silence the compiler (the code is not yet completed) */
 /**
- * Allocates memory for a matrix with ${number_of_rows} rows and ${number_of_columns} columns of strings of
- * maximal size ${max_length_of_string}.
+ * Allocates memory for a matrix with number_of_rows rows and number_of_columns columns of strings of
+ * maximal size max_length_of_string.
  */
 static char ***mo_allocate_matrix_of_strings(const size_t number_of_rows, const size_t number_of_columns,
     const size_t max_length_of_string) {
@@ -28,8 +27,8 @@ static char ***mo_allocate_matrix_of_strings(const size_t number_of_rows, const 
 }
 
 /**
- * Frees the memory occupied by the ${matrix_of_strings} matrix of string with ${number_of_rows} rows and
- * ${cnumber_of_columns} columns.
+ * Frees the memory occupied by the matrix_of_strings matrix of string with number_of_rows rows and
+ * cnumber_of_columns columns.
  */
 static void mo_free_matrix_of_strings(char ***matrix_of_strings, const size_t number_of_rows,
     const size_t number_of_columns) {
@@ -46,16 +45,16 @@ static void mo_free_matrix_of_strings(char ***matrix_of_strings, const size_t nu
 }
 
 /**
- * Counts and returns the number of lines in the already opened file pointed to by ${fp}.
+ * Counts and returns the number of lines in the already opened file.
  */
-static size_t mo_get_file_line_number(FILE *fp) {
+static size_t mo_get_number_of_lines_in_file(FILE *file) {
 
   int ch;
   size_t number_of_lines = 0;
 
   /* Count the number of lines */
   do {
-    ch = fgetc(fp);
+    ch = fgetc(file);
     if (ch == '\n')
       number_of_lines++;
   } while (ch != EOF);
@@ -64,63 +63,40 @@ static size_t mo_get_file_line_number(FILE *fp) {
       number_of_lines++;
 
   /* Return to the beginning of the file */
-  rewind(fp);
+  rewind(file);
 
   return number_of_lines;
 }
 
 /**
- * Assumes the input file contains lines with single double values. Reads and returns those values.
- */
-static double *mo_get_doubles_from_file(const char *filename) {
-
-  size_t i, number_of_lines;
-  double *result;
-
-  FILE *fp = fopen(filename, "r");
-  if (fp == 0) {
-    coco_error("failed to open input file %s", filename);
-  }
-  number_of_lines = mo_get_file_line_number(fp);
-
-  /* Prepare the vector */
-  result = coco_allocate_vector(number_of_lines);
-  for (i = 0; i < number_of_lines; i++) {
-    fscanf(fp, "%lf", &result[i]);
-  }
-  fclose(fp);
-
-  return result;
-}
-
-/**
  * Assumes the input file contains pairs of strings separated by tabs and that each line is of maximal length
- * ${max_length_of_string}. Returns the strings in the form of a N x 2 matrix, where N is the number of lines
+ * max_length_of_string. Returns the strings in the form of a N x 2 matrix, where N is the number of lines
  * in the file.
  */
 static char ***mo_get_string_pairs_from_file(const char *filename, const size_t max_length_of_string) {
 
   size_t i, number_of_lines;
-  FILE *fp = fopen(filename, "r");
+  FILE *file = fopen(filename, "r");
   char ***matrix_of_strings;
 
-  if (fp == 0) {
-    coco_error("failed to open input file %s", filename);
+  if (file == 0) {
+    coco_error("mo_get_string_pairs_from_file() failed to open file %s", filename);
   }
-  number_of_lines = mo_get_file_line_number(fp);
+  number_of_lines = mo_get_file_number_of_lnes(file);
 
   /* Prepare the matrix */
   matrix_of_strings = mo_allocate_matrix_of_strings(number_of_lines, 2, max_length_of_string);
   for (i = 0; i < number_of_lines; i++) {
-    fscanf(fp, "%s\t%[^\n]", matrix_of_strings[i][0], matrix_of_strings[i][1]);
+    fscanf(file, "%s\t%[^\n]", matrix_of_strings[i][0], matrix_of_strings[i][1]);
   }
-  fclose(fp);
+  fclose(file);
 
   return matrix_of_strings;
 }
 
 /**
- * Converts string (char *) to double. Does not check for underflow or overflow, ignores any trailing characters.
+ * Converts string (char *) to double. Does not check for underflow or overflow, ignores any trailing
+ * characters.
  */
 static double mo_string_to_double(const char *string) {
   double result;
@@ -135,8 +111,9 @@ static double mo_string_to_double(const char *string) {
 }
 
 /**
- * Scans the input matrix to find the first value that matches the given ${key} (looks for the key in ${key_column} column and
- * for the value in ${value_column} column). If the key is not found, it returns ${default_value}.
+ * Scans the input matrix to find the first value that matches the given key (looks for the key in
+ * key_column column and for the value in value_column column). If the key is not found, it returns
+ * default_value.
  */
 static double mo_get_matching_double_value(const char ***matrix_of_strings, const char *key, const size_t number_of_rows,
     const size_t key_column, const size_t value_column, double default_value) {
@@ -155,25 +132,15 @@ static double mo_get_matching_double_value(const char ***matrix_of_strings, cons
   return default_value;
 }
 
-/**
- * Appends ${string} to the given file.
- */
-static void mo_append_to_file(const char *filename, const char *string) {
-
-  FILE *fp = fopen(filename, "a");
-  fprintf(fp, string);
-  fprintf(fp, "\n");
-  fclose(fp);
-}
-#endif
+/*#endif*/
 
 /**
  * Checks the dominance relation in the unconstrained minimization case between
- * ${objectives1} and ${objectives2} and returns:
- *  1 if ${objectives1} dominates ${objectives2}
- *  0 if ${objectives1} and ${objectives2} are non-dominated
- * -1 if ${objectives2} dominates ${objectives1}
- * -2 if ${objectives1} is identical to ${objectives2}
+ * objectives1 and objectives2 and returns:
+ *  1 if objectives1 dominates objectives2
+ *  0 if objectives1 and objectives2 are non-dominated
+ * -1 if objectives2 dominates objectives1
+ * -2 if objectives1 is identical to objectives2
  */
 static int mo_get_dominance(const double *objectives1, const double *objectives2, const size_t num_obj) {
   /* TODO: Should we care about comparison precision? */
--- code-experiments/src/suite_biobj_300.c
+++ code-experiments/src/suite_biobj_300.c
@@ -10,7 +10,7 @@
 
 /**
  * The biobjective suite of 300 function combinations generated using the suite_bbob2009. For each
- * function combination, there are 11 preset instances in each of the five dimensions {2, 3, 5, 10, 20}.
+ * function, there are 11 preset instances in each of the five dimensions {2, 3, 5, 10, 20}.
  *
  * Ref: Benchmarking Numerical Multiobjective Optimizers Revisited @ GECCO'15
  */
@@ -21,8 +21,8 @@
 
 /* Mapping to the suite_bbob2009 function instances */
 static const size_t SUITE_BIOBJ_300_INSTANCE_LIST[5][2] = { { 2, 4 }, { 3, 5 }, { 7, 8 }, { 9, 10 }, { 11, 12 } };
-/* List of 300 problems (300 is the total number of bi-objective combinations) */
-static int SUITE_BIOBJ_300_PROBLEM_LIST[300][2];
+/* List of 300 functions (300 is the total number of bi-objective function combinations) */
+static int SUITE_BIOBJ_300_FUNCTION_LIST[300][2];
 /* Whether the suite is defined */
 static int SUITE_BIOBJ_300_DEFINED = 0;
 /* A list of problem groups (TODO: to be replaced by a problem_info field) */
@@ -34,51 +34,58 @@ static const char SUITE_BIOBJ_300_PROBLEM_GROUP[24][30] = {
     "weakly-structured", "weakly-structured", "weakly-structured", "weakly-structured", "weakly-structured" };
 
 /**
- * How: instance varies faster than combination which is still faster than dimension
+ * How: instance varies faster than function which is still faster than dimension
  * 
- *  problem_index | instance | combination | dimension
- * ---------------+----------+-------------+-----------
- *              0 |        1 |           1 |         2
- *              1 |        2 |           1 |         2
- *              2 |        3 |           1 |         2
- *              3 |        4 |           1 |         2
- *              4 |        5 |           1 |         2
- *              5 |        1 |           2 |         2
- *              6 |        2 |           2 |         2
- *             ...        ...           ...        ...
- *           1499 |        5 |         300 |         2
- *           1500 |        1 |           1 |         3
- *           1501 |        2 |           1 |         3
- *             ...        ...           ...        ...
- *           7497 |        3 |         300 |        20
- *           7498 |        4 |         300 |        20
- *           7499 |        5 |         300 |        20
+ *  problem_index | instance | function | dimension
+ * ---------------+----------+----------+-----------
+ *              0 |        1 |        1 |         2
+ *              1 |        2 |        1 |         2
+ *              2 |        3 |        1 |         2
+ *              3 |        4 |        1 |         2
+ *              4 |        5 |        1 |         2
+ *              5 |        1 |        2 |         2
+ *              6 |        2 |        2 |         2
+ *             ...        ...        ...        ...
+ *           1499 |        5 |      300 |         2
+ *           1500 |        1 |        1 |         3
+ *           1501 |        2 |        1 |         3
+ *             ...        ...        ...        ...
+ *           7497 |        3 |      300 |        20
+ *           7498 |        4 |      300 |        20
+ *           7499 |        5 |      300 |        20
  */
 
-/* Commented to silence the compiler.
+/* Commented to silence the compiler.*/
 
-static long suite_biobj_300_encode_problem_index(int combination_idx, long instance_idx, int dimension_idx) {
+static long suite_biobj_300_encode_problem_index(int function_id, long instance_id, int dimension_idx) {
   long problem_index;
-  problem_index = instance_idx + combination_idx * SUITE_BIOBJ_NUMBER_OF_INSTANCES
+  problem_index = instance_id + function_id * SUITE_BIOBJ_NUMBER_OF_INSTANCES
       + dimension_idx * (SUITE_BIOBJ_NUMBER_OF_INSTANCES * SUITE_BIOBJ_NUMBER_OF_FUNCTIONS);
   return problem_index;
 }
-*/
+/**/
 
 static void suite_biobj_300_decode_problem_index(const long problem_index,
-                                                 int *combination_idx,
-                                                 long *instance_idx,
+                                                 int *function_id,
+                                                 long *instance_id,
                                                  long *dimension_idx) {
   long rest;
   *dimension_idx = problem_index / (SUITE_BIOBJ_NUMBER_OF_INSTANCES * SUITE_BIOBJ_NUMBER_OF_FUNCTIONS);
   rest = problem_index % (SUITE_BIOBJ_NUMBER_OF_INSTANCES * SUITE_BIOBJ_NUMBER_OF_FUNCTIONS);
-  *combination_idx = (int) (rest / SUITE_BIOBJ_NUMBER_OF_INSTANCES);
-  *instance_idx = rest % SUITE_BIOBJ_NUMBER_OF_INSTANCES;
+  *function_id = (int) (rest / SUITE_BIOBJ_NUMBER_OF_INSTANCES);
+  *instance_id = rest % SUITE_BIOBJ_NUMBER_OF_INSTANCES;
 }
 
+static long suite_biobj_300_get_instance_id(const long problem_index) {
+   long rest;
+   rest = problem_index % (SUITE_BIOBJ_NUMBER_OF_INSTANCES * SUITE_BIOBJ_NUMBER_OF_FUNCTIONS);
+   return (rest % SUITE_BIOBJ_NUMBER_OF_INSTANCES);
+ }
+
+
 static coco_problem_t *suite_biobj_300(const long problem_index) {
-  int function_idx, problem1_idx, problem2_idx;
-  long instance_idx, dimension_idx;
+  int function_id, function1_id, function2_id;
+  long instance_id, dimension_idx;
   coco_problem_t *problem1, *problem2, *problem;
 
   if (problem_index < 0)
@@ -89,40 +96,37 @@ static coco_problem_t *suite_biobj_300(const long problem_index) {
     int i, j;
     for (i = 1; i <= 24; ++i) {
       for (j = i; j <= 24; ++j) {
-        SUITE_BIOBJ_300_PROBLEM_LIST[k][0] = i;
-        SUITE_BIOBJ_300_PROBLEM_LIST[k][1] = j;
+        SUITE_BIOBJ_300_FUNCTION_LIST[k][0] = i;
+        SUITE_BIOBJ_300_FUNCTION_LIST[k][1] = j;
         k++;
       }
     }
     SUITE_BIOBJ_300_DEFINED = 1;
   }
 
-  suite_biobj_300_decode_problem_index(problem_index, &function_idx, &instance_idx, &dimension_idx);
+  suite_biobj_300_decode_problem_index(problem_index, &function_id, &instance_id, &dimension_idx);
 
-  problem1_idx = SUITE_BIOBJ_300_PROBLEM_LIST[function_idx][0];
-  problem2_idx = SUITE_BIOBJ_300_PROBLEM_LIST[function_idx][1];
+  function1_id = SUITE_BIOBJ_300_FUNCTION_LIST[function_id][0];
+  function2_id = SUITE_BIOBJ_300_FUNCTION_LIST[function_id][1];
 
-  problem1 = suite_bbob2009_problem(problem1_idx, BBOB2009_DIMS[dimension_idx],
-      (long) SUITE_BIOBJ_300_INSTANCE_LIST[instance_idx][0]);
-  problem2 = suite_bbob2009_problem(problem2_idx, BBOB2009_DIMS[dimension_idx],
-      (long) SUITE_BIOBJ_300_INSTANCE_LIST[instance_idx][1]);
+  problem1 = suite_bbob2009_problem(function1_id, BBOB2009_DIMS[dimension_idx],
+      (long) SUITE_BIOBJ_300_INSTANCE_LIST[instance_id][0]);
+  problem2 = suite_bbob2009_problem(function2_id, BBOB2009_DIMS[dimension_idx],
+      (long) SUITE_BIOBJ_300_INSTANCE_LIST[instance_id][1]);
   problem = coco_stacked_problem_allocate(problem1, problem2);
   problem->index = problem_index;
 
   /* Construct the id for the suite_biobj_300 in the form "biobj_300_fxxx_DIMy" */
   coco_free_memory(problem->problem_id);
-  problem->problem_id = coco_strdupf("biobj_300_f%03d_DIM%d", function_idx + 1, problem->number_of_variables);
+  problem->problem_id = coco_strdupf("biobj_300_f%03d_DIM%d", function_id + 1, problem->number_of_variables);
 
   /* Construct the information about the problem - its "type" */
   /* TODO: Use a new field (for example problem_info) instead of problem_name to store this information */
   coco_free_memory(problem->problem_name);
-  if (problem1_idx < problem2_idx)
-    problem->problem_name = coco_strdupf("%s_%s", SUITE_BIOBJ_300_PROBLEM_GROUP[problem1_idx], SUITE_BIOBJ_300_PROBLEM_GROUP[problem2_idx]);
+  if (function1_id < function2_id)
+    problem->problem_name = coco_strdupf("%s_%s", SUITE_BIOBJ_300_PROBLEM_GROUP[function1_id], SUITE_BIOBJ_300_PROBLEM_GROUP[function2_id]);
   else
-    problem->problem_name = coco_strdupf("%s_%s", SUITE_BIOBJ_300_PROBLEM_GROUP[problem2_idx], SUITE_BIOBJ_300_PROBLEM_GROUP[problem1_idx]);
-
-  printf("%s, %ld\n", problem->problem_name, problem->index);
-  fflush(stdout);
+    problem->problem_name = coco_strdupf("%s_%s", SUITE_BIOBJ_300_PROBLEM_GROUP[function2_id], SUITE_BIOBJ_300_PROBLEM_GROUP[function1_id]);
 
   return problem;
 }
