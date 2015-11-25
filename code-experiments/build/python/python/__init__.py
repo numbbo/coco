218ee79fc43d6a02ac3a951bb76a5bc5df95c075
nikohansen <nikolaus.hansen@gmail.com>
2015-11-25 20:39:06 +0100

    __init__.py can now deal with both names, interface and _interface

--- code-experiments/build/python/python/__init__.py
+++ code-experiments/build/python/python/__init__.py
@@ -1,4 +1,8 @@
 import utilities
-from ._interface import Problem, Benchmark, benchmarks
+try:
+    from ._interface import Problem, Benchmark, benchmarks
+except:
+    from .interface import Problem, Benchmark, benchmarks
+  
 # from .utilities import about_equal
 # from .exceptions import NoSuchProblemException, InvalidProblemException
