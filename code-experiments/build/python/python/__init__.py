389c426017219e1c8f6f4a1b58d3b5858d1b3781
nikohansen <nikolaus.hansen@gmail.com>
2015-11-25 16:39:26 +0100

    _interface -> interface in cocoex module __init__

--- code-experiments/build/python/python/__init__.py
+++ code-experiments/build/python/python/__init__.py
@@ -1,4 +1,4 @@
 import utilities
-from ._interface import Problem, Benchmark, benchmarks
+from .interface import Problem, Benchmark, benchmarks
 # from .utilities import about_equal
 # from .exceptions import NoSuchProblemException, InvalidProblemException
