1b7b2dd2c8cd7605ba64a85882d00fd70aead3d7
nikohansen <nikolaus.hansen@gmail.com>
2015-11-25 03:04:27 +0100

    call cython from do.py only on OSX

--- do.py
+++ do.py
@@ -101,7 +101,8 @@ def build_python():
     _prep_python()
     ## Force distutils to use Cython
     os.environ['USE_CYTHON'] = 'true'
-    run('code-experiments/build/python/cython', ['cython', 'interface.pyx'])
+    if ('darwin' in sys.platform):  # not clear why this is needed
+        run('code-experiments/build/python/cython', ['cython', 'interface.pyx'])
     # python('code-experiments/build/python', ['setup.py', 'sdist'])
     python('code-experiments/build/python', ['setup.py', 'install', '--user'])
     os.environ.pop('USE_CYTHON')
@@ -143,7 +144,8 @@ def test_python():
         os.makedirs(python_temp_lib)
         os.environ['PYTHONPATH'] = python_temp_lib
         os.environ['USE_CYTHON'] = 'true'
-        run('code-experiments/build/python/cython', ['cython', 'interface.pyx'])
+        if ('darwin' in sys.platform):
+            run('code-experiments/build/python/cython', ['cython', 'interface.pyx'])
         python('code-experiments/build/python', ['setup.py', 'install', '--home', python_temp_home])
         python('code-experiments/build/python', ['coco_test.py', 'bbob2009_testcases.txt'])
         os.environ.pop('USE_CYTHON')
