b5664e62404a1b5a2b145a0ee19d74505687c155
oaelhara <ouassimaitelhara@yahoo.fr>
2015-12-11 15:25:33 +0100

    updating bbob2009_logger initialization

--- code-experiments/src/observer_bbob2009.c
+++ code-experiments/src/observer_bbob2009.c
@@ -11,6 +11,10 @@ static coco_problem_t *deprecated__observer_bbob2009(coco_problem_t *problem, co
   return problem;
 }
 
+/**
+ * Initializes the bbob2009 observer. Possible options:
+ * - folder_path : relative path to the output folder
+ */
 static void observer_bbob2009(coco_observer_t *self, const char *options) {
   /* TODO: To implement this method instead of the deprecated one. */
   return;
