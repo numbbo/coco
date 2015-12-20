ef2312b166d52405ce2dea7a42242062678027af
asmaatamna <asma.atamna@inria.fr>
2015-12-20 18:03:47 +0100

    updated Matlab interface

--- code-experiments/build/matlab/demo.m
+++ code-experiments/build/matlab/demo.m
@@ -1,24 +1,23 @@
 MAX_EVALS = 100;
-number_of_batches = 1;
-current_batch = 1;
-suite_name = 'suite_biobj_300';
+suite_name = 'suite_biobj';
+suite_instance = '';
+suite_options = 'dimensions: 2,10 instance_idx: 1';
 observer_name = 'observer_biobj';
-observer_options = 'result_folder: RS_on_suite_biobj_300 include_decision_variables: 0 log_nondominated: final';
+observer_options = ['result_folder: RS_on_suite_biobj \',...
+                    'algorithm_name: RS \',...
+                    'algorithm_info: "A simple random search algorithm" \',...
+                    'log_decision_variables: low_dim \',...
+                    'compute_indicators: log_nondominated: all'];
+suite = cocoSuite(suite_name, suite_instance, suite_options);
 observer = cocoObserver(observer_name, observer_options);
-problem_index = -1;
 while true
-    problem_index = cocoSuiteGetNextProblemIndex(suite_name, problem_index, '');
-    if (problem_index < 0)
+    problem = cocoSuiteGetNextProblem(suite, observer);
+    if (~cocoProblemIsValid(problem))
         break;
     end
-    if (mod(problem_index + current_batch - 1, number_of_batches)~= 0)
-        continue;
-    end
-    problem = cocoSuiteGetProblem(suite_name, problem_index);
-    problem = cocoProblemAddObserver(problem, observer);
     disp(['Optimizing ', cocoProblemGetId(problem)]);
     my_optimizer(problem, cocoProblemGetSmallestValuesOfInterest(problem), cocoProblemGetLargestValuesOfInterest(problem), MAX_EVALS);
     disp(['Done with problem ', cocoProblemGetId(problem), '...']);
-    cocoProblemFree(problem);
 end
 cocoObserverFree(observer);
+cocoSuiteFree(suite);
--- code-experiments/build/matlab/setup.m
+++ code-experiments/build/matlab/setup.m
@@ -26,10 +26,11 @@
 %%%%%%%%%%%%%%%%%
 
 mex -Dchar16_t=uint16_t cocoEvaluateFunction.c
-mex -Dchar16_t=uint16_t cocoProblemAddObserver.c
-mex -Dchar16_t=uint16_t cocoProblemFree.c
+mex -Dchar16_t=uint16_t cocoSuite.c
+mex -Dchar16_t=uint16_t cocoSuiteFree.c
 mex -Dchar16_t=uint16_t cocoObserver.c
 mex -Dchar16_t=uint16_t cocoObserverFree.c
+mex -Dchar16_t=uint16_t cocoSuiteGetNextProblem.c
 mex -Dchar16_t=uint16_t cocoProblemGetDimension.c
 mex -Dchar16_t=uint16_t cocoProblemGetEvaluations.c
 mex -Dchar16_t=uint16_t cocoProblemGetId.c
@@ -38,5 +39,3 @@ mex -Dchar16_t=uint16_t cocoProblemGetName.c
 mex -Dchar16_t=uint16_t cocoProblemGetNumberOfObjectives.c
 mex -Dchar16_t=uint16_t cocoProblemGetSmallestValuesOfInterest.c
 mex -Dchar16_t=uint16_t cocoProblemIsValid.c
-mex -Dchar16_t=uint16_t cocoSuiteGetProblem.c
-mex -Dchar16_t=uint16_t cocoSuiteGetNextProblemIndex.c
