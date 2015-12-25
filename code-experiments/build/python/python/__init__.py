1051e15dd83cb0258210b215ac267bc932fa4d8a
nikohansen <nikolaus.hansen@gmail.com>
2015-12-25 14:28:12 +0100

    Refinements to Python interface.pyx, bug-fix when iterating over suite
    	modified:   code-experiments/build/python/cython/interface.pyx
    	modified:   code-experiments/build/python/python/__init__.py

--- code-experiments/build/python/python/__init__.py
+++ code-experiments/build/python/python/__init__.py
@@ -1,9 +1,18 @@
+"""Experimentation module of the COCO - COmparing Continuous Optimizers - 
+framework. 
+
+The module defines the benchmark test beds in the `Suite` class
+and the output data facilities in the `Observer` class. 
+
+Besides some documentation of the `Suite` class, a more complete
+example use case can be found in the `example_experiment.py` file. 
+"""
 import utilities
 try:
-    from ._interface import Suite, Observer
-except Exception as e:
-    print("numbbo/code-experiments/build/python/python/__init__.py: could not import '_interface', trying 'interface'", e)
-    from .interface import Suite, Observer
+    from ._interface import Suite, Observer, known_suite_names
+except Exception as _e:
+    # print("numbbo/code-experiments/build/python/python/__init__.py: could not import '_interface', trying 'interface'", _e)
+    from .interface import Suite, Observer, known_suite_names
   
 # from .utilities import about_equal
 # from .exceptions import NoSuchProblemException, InvalidProblemException
