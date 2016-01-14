e9fda75b78deb162e5ae11452817036946dc4c78
oaelhara <ouassimaitelhara@yahoo.fr>
2016-01-14 15:19:06 +0100

    now linked the meant-for-large-scale transformation with the rest of the project

--- code-experiments/src/large_scale_transform_vars_permblockdiag.c
+++ code-experiments/src/large_scale_transform_vars_permblockdiag.c
@@ -2,37 +2,64 @@
 
 #include "coco.h"
 #include "coco_problem.c"
+#include "large_scale_transformations.c"
 
 typedef struct {
-  double *B, *x;
-  size_t *P1, *P2, *block_sizes, nb_blocks;/*permutation matrices, P1 for the columns of M and P2 for its rows*/
+  double **B;
+  double *x;
+  size_t *P1; /*permutation matrices, P1 for the columns of B and P2 for its rows*/
+  size_t *P2;
+  size_t *block_sizes;
+  size_t nb_blocks;
+  size_t *block_map; /* maps rows to blocksizes, keep until better way is found */
 } ls_transform_vars_permblockdiag_t;
 
-static void transform_vars_affine_evaluate(coco_problem_t *self, const double *x, double *y) {
+static void ls_transform_vars_permblockdiag_evaluate(coco_problem_t *self, const double *x, double *y) {
   size_t i, j;
   ls_transform_vars_permblockdiag_t *data;
   coco_problem_t *inner_problem;
   
   data = coco_transformed_get_data(self);
   inner_problem = coco_transformed_get_inner_problem(self);
-  
+
+  printf("eval started\n");
   for (i = 0; i < inner_problem->number_of_variables; ++i) {
-    const double *current_row = data->B + data->P2[i] * self->number_of_variables;
-    data->x[i] = 0;/*data->b[i];*/
+    /*
+    printf("in ls_transform\n");
+    printf("P1: ");
     for (j = 0; j < self->number_of_variables; ++j) {
-      data->x[i] += x[j] * current_row[data->P1[j]];
+      printf("%d ", data->P1[i]);
     }
+    printf("\n");
+    
+    printf("P2: ");
+    for (j = 0; j < self->number_of_variables; ++j) {
+      printf("%d ", data->P2[i]);
+    }
+    printf("\n");
+    */
+    /*const double *current_row = data->B;*/
+    data->x[i] = 0;/*data->b[i];*/
+    /*compute data->x[i] = < B[P2[i]] , x[P1[i]] >  */
+    for (j = 0; j < data->block_map[i]; ++j) {/*blocksize[P2[i]]*/
+      
+      data->x[i] += data->B[data->P2[i]][j] * x[data->P1[j]];
+    }
+    /*printf("x_%d=%f\n", i, x[0]);*/
+
   }
+  printf("eval finished\n\n");
   coco_evaluate_function(inner_problem, data->x, y);
 }
 
-static void transform_vars_affine_free(void *thing) {
-  transform_vars_affine_data_t *data = thing;
-  coco_free_memory(data->M);
+static void ls_transform_vars_permblockdiag_free(void *thing) {
+  ls_transform_vars_permblockdiag_t *data = thing;
+  coco_free_memory(data->B);
   coco_free_memory(data->P1);
-  coco_free_memory(data->P1);
+  coco_free_memory(data->P2);
   coco_free_memory(data->block_sizes);
   coco_free_memory(data->x);
+  coco_free_memory(data->block_map);
 }
 
 /*
@@ -42,22 +69,41 @@ static void transform_vars_affine_free(void *thing) {
  * The matrix M is stored in row-major format.
  */
 static coco_problem_t *f_ls_transform_vars_permblockdiag(coco_problem_t *inner_problem,
-                                               const double *B,
-                                               const double *P1,
-                                               const double *P2,
-                                               const size_t number_of_variables) {
+                                               const double **B,
+                                               const size_t *P1,
+                                               const size_t *P2,
+                                               const size_t number_of_variables,
+                                               const size_t *block_sizes,
+                                               const size_t nb_blocks) {
   coco_problem_t *self;
   ls_transform_vars_permblockdiag_t *data;
-  size_t entries_in_B;
-  
-  /*entries_in_B = inner_problem->number_of_variables * number_of_variables;*/
+  size_t entries_in_M, idx_blocksize, next_bs_change, current_blocksize;
+  int i;
+  entries_in_M = 0;
+  assert(number_of_variables > 0);/*tmp*/
+  for (i = 0; i < nb_blocks; i++) {
+    entries_in_M += block_sizes[i] * block_sizes[i];
+  }
   data = coco_allocate_memory(sizeof(*data));
-  data->B = coco_duplicate_vector(M, entries_in_M);
-  data->P1 = coco_duplicate_vector(P1, inner_problem->number_of_variables);
-  data->P2 = coco_duplicate_vector(P2, inner_problem->number_of_variables);
+  data->B = ls_copy_block_matrix(B, number_of_variables, block_sizes, nb_blocks);
   data->x = coco_allocate_vector(inner_problem->number_of_variables);
+  data->P1 = coco_duplicate_size_t_vector(P1, inner_problem->number_of_variables);
+  data->P2 = coco_duplicate_size_t_vector(P2, inner_problem->number_of_variables);
+  data->block_sizes = coco_duplicate_size_t_vector(block_sizes, nb_blocks);
+  data->nb_blocks = nb_blocks;
+  data->block_map = (size_t *)coco_allocate_memory(number_of_variables * sizeof(size_t));
+  idx_blocksize = 0;
+  next_bs_change = block_sizes[idx_blocksize];
+  for (i = 0; i < number_of_variables; i++) {
+    if (i >= next_bs_change) {
+      idx_blocksize++;
+      next_bs_change += block_sizes[idx_blocksize];
+    }
+    current_blocksize=block_sizes[idx_blocksize];
+    data->block_map[i] = current_blocksize;
+  }
   
-  self = coco_transformed_allocate(inner_problem, data, transform_vars_affine_free);
-  self->evaluate_function = transform_vars_affine_evaluate;
+  self = coco_transformed_allocate(inner_problem, data, ls_transform_vars_permblockdiag_free);
+  self->evaluate_function = ls_transform_vars_permblockdiag_evaluate;
   return self;
 }
--- code-experiments/src/large_scale_transformations.c
+++ code-experiments/src/large_scale_transformations.c
@@ -2,51 +2,56 @@
 #include <assert.h>
 #include "coco.h"
 
-#include "coco_runtime_c.c" //
-#include "suite_bbob_legacy_code.c"//
-#include <time.h>//
+#include "coco_runtime_c.c" /*tmp*/
+#include "coco_random.c" /*tmp*/
+#include "suite_bbob_legacy_code.c" /*tmp*/
+
+#include <time.h> /*tmp*/
 
 static double *random_data;/* global variable used to generate the random permutations */
 
 /**
  * ls_allocate_blockmatrix(n, m, bs):
  *
- * Allocate a ${n} by ${m} block matrix of nb_blocks block sizes blocksizes structured as an array of pointers
+ * Allocate a ${n} by ${m} block matrix of nb_blocks block sizes block_sizes structured as an array of pointers
  * to double arrays.
- * each row constains only the blocksizes[i] possibly non-zero elements
+ * each row constains only the block_sizes[i] possibly non-zero elements
  */
-static double **ls_allocate_blockmatrix(const size_t n, const size_t m, const size_t* blocksizes, const size_t nb_blocks) {
+static double **ls_allocate_blockmatrix(const size_t n, const size_t* block_sizes, const size_t nb_blocks) {
   double **matrix = NULL;
   size_t current_blocksize;
   size_t next_bs_change;
   size_t idx_blocksize;
   size_t i;
-  size_t sum_blocksizes;
-  assert(n == m);/* we only treat square matrices for now*/
+  size_t sum_block_sizes;
   
-  sum_blocksizes = 0;
+  sum_block_sizes = 0;
   for (i = 0; i < nb_blocks; i++){
-    sum_blocksizes += blocksizes[i];
+    sum_block_sizes += block_sizes[i];
   }
-  assert(sum_blocksizes == n);
+  assert(sum_block_sizes == n);
   
   matrix = (double **) coco_allocate_memory(sizeof(double *) * n);
   idx_blocksize = 0;
-  next_bs_change = blocksizes[idx_blocksize];
+  next_bs_change = block_sizes[idx_blocksize];
   
   for (i = 0; i < n; ++i) {
     if (i >= next_bs_change) {
       idx_blocksize++;
-      next_bs_change += blocksizes[idx_blocksize];
+      next_bs_change += block_sizes[idx_blocksize];
     }
-    current_blocksize=blocksizes[idx_blocksize];
+    current_blocksize=block_sizes[idx_blocksize];
     matrix[i] = coco_allocate_vector(current_blocksize);
     
   }
   return matrix;
 }
 
-static void ls_free_blockmatrix(double **matrix, const size_t n) {
+
+/*
+ * frees a block diagonal matrix (same as a matrix but in case of change, easier to update separatly from free_matrix)
+ */
+static void ls_free_block_matrix(double **matrix, const size_t n) {
   size_t i;
   for (i = 0; i < n; ++i) {
     if (matrix[i] != NULL) {
@@ -62,23 +67,34 @@ static void ls_free_blockmatrix(double **matrix, const size_t n) {
 /**
  * ls_compute_blockrotation(B, seed, DIM):
  *
- * Compute a ${DIM}x${DIM} block-diagonal matrix based on ${seed} and blocksizes and stores it in ${B}.
+ * Compute a ${DIM}x${DIM} block-diagonal matrix based on ${seed} and block_sizes and stores it in ${B}.
  * B is a 2D vector with DIM lines and each line has blocksize(line) elements (the zeros are not stored)
  */
-static void ls_compute_blockrotation(double **B, long seed, size_t n, size_t *blocksizes, size_t nb_blocks) {
+static void ls_compute_blockrotation(double **B, long seed, size_t n, size_t *block_sizes, size_t nb_blocks) {
   double prod;
-  double gvect[2000];
+  double *gvect;
   double **current_block;
-  long i, j, k; /* Loop over pairs of column vectors. */
-  size_t idx_block, current_blocksize,cumsum_prev_blocksizes;
+  size_t i, j, k; /* Loop over pairs of column vectors. */
+  size_t idx_block, current_blocksize,cumsum_prev_block_sizes, sum_block_sizes;
+  size_t nb_entries, current_gvect_pos;
   
-  cumsum_prev_blocksizes = 0;/* shift in rows to account for the previous blocks */
+  nb_entries = 0;
+  sum_block_sizes = 0;
+  for (i = 0; i < nb_blocks; i++){
+    sum_block_sizes += block_sizes[i];
+    nb_entries += block_sizes[i] * block_sizes[i];
+  }
+  assert(sum_block_sizes == n);
+  gvect = coco_allocate_vector(nb_entries);
+  
+  bbob2009_gauss(gvect, nb_entries, seed);
+  current_gvect_pos = 0;
+  cumsum_prev_block_sizes = 0;/* shift in rows to account for the previous blocks */
   for (idx_block = 0; idx_block < nb_blocks; idx_block++) {
-    current_blocksize = blocksizes[idx_block];
+    current_blocksize = block_sizes[idx_block];
     current_block = bbob2009_allocate_matrix(current_blocksize, current_blocksize);
-    bbob2009_gauss(gvect, current_blocksize * current_blocksize, seed);
-    bbob2009_reshape(current_block, gvect, current_blocksize, current_blocksize);
-
+    bbob2009_reshape(current_block, &gvect[current_gvect_pos], current_blocksize, current_blocksize);
+    
     for (i = 0; i < current_blocksize; i++) {
       for (j = 0; j < i; j++) {
         prod = 0;
@@ -101,38 +117,40 @@ static void ls_compute_blockrotation(double **B, long seed, size_t n, size_t *bl
     /* now fill the block matrix*/
     for (i = 0 ; i < current_blocksize; i++) {
       for (j = 0; j < current_blocksize; j++) {
-        B[i+cumsum_prev_blocksizes][j]=current_block[i][j];
+        B[i + cumsum_prev_block_sizes][j]=current_block[i][j];
       }
     }
     
-    cumsum_prev_blocksizes+=current_blocksize;
-    ls_free_blockmatrix(current_block, current_blocksize);
+    cumsum_prev_block_sizes+=current_blocksize;
+    current_gvect_pos += current_blocksize * current_blocksize;
+    ls_free_block_matrix(current_block, current_blocksize);
   }
-    
+  coco_free_memory(gvect);
 }
 
 /*
- * copy the 2-D matrix into a 1-D vector
+ * makes a copy of a block_matrix
  */
-static void ls_copy_block_matrix(double **rot, double *M, const size_t dimension, const size_t *blocksizes, const size_t nb_blocks) {
-  size_t row, column, idx_blocksize, current_blocksize, current_idx, next_bs_change;
-  double *current_row;
+static double **ls_copy_block_matrix(double **B, const size_t dimension, const size_t *block_sizes, const size_t nb_blocks) {
+  double **dest;
+  size_t i, j, idx_blocksize, current_blocksize, next_bs_change;
   
-  current_idx = 0;
+  dest = ls_allocate_blockmatrix(dimension, block_sizes, nb_blocks);
   idx_blocksize = 0;
-  current_blocksize = blocksizes[idx_blocksize];
-  next_bs_change = blocksizes[idx_blocksize];
-
-  for (row = 0; row < dimension; ++row) {
-    if (row >= next_bs_change) {
+  current_blocksize = block_sizes[idx_blocksize];
+  next_bs_change = block_sizes[idx_blocksize];
+  assert(nb_blocks != 0); /*tmp*//*to silence warning*/
+  for (i = 0; i < dimension; i++) {
+    if (i >= next_bs_change) {
       idx_blocksize++;
-      next_bs_change += blocksizes[idx_blocksize];
+      next_bs_change += block_sizes[idx_blocksize];
     }
-    current_blocksize=blocksizes[idx_blocksize];
-    for (column = 0; column < current_blocksize; column++, current_idx++) {
-      M[current_idx] = rot[row][column];
+    current_blocksize=block_sizes[idx_blocksize];
+    for (j = 0; j < current_blocksize; j++) {
+      dest[i][j] = B[i][j];
     }
   }
+  return dest;
 }
 
 /**
@@ -153,12 +171,11 @@ static int f_compare_doubles_for_random_permutation(const void *a, const void *b
  * generates a random, uniformly sampled, permutation and puts it in P
  */
 static void ls_compute_random_permutation(size_t *P, long seed, size_t n) {
-  //size_t *rperm;
   long i;
   random_data = coco_allocate_vector(n);
   bbob2009_unif(random_data, n, seed);
   for (i = 0; i < n; i++){
-    P[i] = i;
+    P[i] = (size_t) i;
   }
   qsort(P, n, sizeof(size_t), f_compare_doubles_for_random_permutation);
   
@@ -171,12 +188,20 @@ static void ls_compute_random_permutation(size_t *P, long seed, size_t n) {
  * use size_t as return type and force positive values instead?
  */
 long ls_rand_int(long lower_bound, long upper_bound, long seed){
-  /*double rand_01[1];*/ /* bbob2009_unif seems to produce non uniform values! */
+  double rand_01[1]; /* bbob2009_unif seems to produce non uniform values! */
   long range;
   range = upper_bound - lower_bound + 1;
-  /*bbob2009_unif(rand_01, 1, seed);*/
-  return (rand() % range) + lower_bound;
-  /*return ((long) (range - rand_01[0] * range)) + lower_bound;*/ /*TODO: find out why this is biased towards smallest value */
+  coco_random_state_t *rng = coco_random_new(seed);
+
+  bbob2009_unif(rand_01, 1, seed);
+  
+  if (seed>0) {
+    srand((unsigned int)seed);/*tmp*//*to be removed when alternative is fixed*/
+    /*only reset seed when separate experiments, when seed=0, srand must have been called elsewhere */
+  }
+  /*return (rand() % range) + lower_bound;*/
+  return (long)((((double) rand()) / RAND_MAX) * range + lower_bound);
+  /*return ((long) (rand_01[0] * range)) + lower_bound; *//*TODO: find out why this is biased towards smallest value*/
 }
 
 
@@ -187,240 +212,63 @@ long ls_rand_int(long lower_bound, long upper_bound, long seed){
  * for now so dynamic is implemented (simple since no need for tracking indices
  */
 static void ls_compute_truncated_uniform_swap_permutation(size_t *P, long seed, size_t n, size_t nb_swaps, size_t swap_range) {
-  long i, first_swap_var, second_swap_var, tmp, idx_swap;
-  long lower_bound, upper_bound;
+  long i, idx_swap;
+  size_t lower_bound, upper_bound, first_swap_var, second_swap_var, tmp;
   size_t *idx_order;
   random_data = coco_allocate_vector(n);
   bbob2009_unif(random_data, n, seed);
   idx_order = (size_t *) coco_allocate_memory(n * sizeof(size_t));;
   for (i = 0; i < n; i++){
-    P[i] = i;
-    idx_order[i] = i;
+    P[i] = (size_t) i;
+    idx_order[i] = (size_t) i;
   }
-  //qsort(idx_order, n, sizeof(size_t), f_compare_doubles_for_random_permutation);
-
+  qsort(idx_order, n, sizeof(size_t), f_compare_doubles_for_random_permutation);
   for (idx_swap = 0; idx_swap < nb_swaps; idx_swap++) {
     first_swap_var = idx_order[idx_swap];
-    lower_bound = first_swap_var - swap_range;
-    if (lower_bound < 0) {
+    if (first_swap_var < swap_range) {
       lower_bound = 0;
     }
-    upper_bound = first_swap_var + swap_range;
-    if (upper_bound > n - 1) {
+    else{
+      lower_bound = first_swap_var - swap_range;
+    }
+    if (first_swap_var + swap_range > n - 1) {
       upper_bound = n - 1;
     }
+    else{
+      upper_bound = first_swap_var + swap_range;
+    }
 
-    second_swap_var = ls_rand_int(lower_bound, upper_bound, seed);
+    second_swap_var = (size_t) ls_rand_int((long) lower_bound, (long) upper_bound, 0);
     while (first_swap_var == second_swap_var) {
-      second_swap_var = ls_rand_int(lower_bound, upper_bound, seed);
-
+      second_swap_var = (size_t) ls_rand_int((long) lower_bound, (long) upper_bound, 0);
     }
-
-    if (n <= 19) {
-      for (i = 0; i < n; i++) {
-        printf("%zu ",P[i]);
-      }
-      printf("\n");
-    }
-
     /* swap*/
     tmp = P[first_swap_var];
     P[first_swap_var] = P[second_swap_var];
     P[second_swap_var] = tmp;
-    printf("%ld <-> %ld, dist:%d \n", first_swap_var, second_swap_var, abs(first_swap_var - second_swap_var));
-    if (n <= 19) {
-      for (i = 0; i < n; i++) {
-        printf("%zu ",P[i]);
-      }
-      printf("\n");
-    }
-    printf("\n");
-    
   }
   
-  qsort(idx_order, n, sizeof(size_t), f_compare_doubles_for_random_permutation);/* first, shuffle to elimiate extrema bias*/
   
 }
 
 
-int main(){
-  int testBlockMatrix = 0;
-  int testPermutations = 1;
 
-  size_t nb_blocks = 3;
-  size_t blocksizes[3] = {2,3,1};
-  size_t n, nb_entries, idx_blocksize, next_bs_change, current_blocksize, idx_block, cumsum_prev_blocksizes;
-  long seed;
-  srand(time(NULL));
-  seed=rand();
+/*
+ * duplicates a size_t vector
+ */
+size_t *coco_duplicate_size_t_vector(const size_t *src, const size_t number_of_elements) {
+  size_t i;
+  size_t *dst;
   
-  if (testPermutations) {
-    size_t *P;
-    
-    int i, j, nbRuns;
-    nbRuns = 0;
-    if (nbRuns) {
-      n = 10;
-      P = (size_t *) coco_allocate_memory(n * sizeof(size_t));
-      for (j = 0; j < nbRuns; j++) {
-        seed = rand();
-        ls_compute_random_permutation(P, seed, n);
-        for (i = 0; i < n; i++) {
-          printf("%zu ",P[i]);
-        }
-        printf("\n");
-        }
-    }
-    
-    nbRuns = 1;
-    if (nbRuns) {
-      size_t nb_swaps, swap_range;
-      n = 100;
-      P = (size_t *) coco_allocate_memory(n * sizeof(size_t));
-      nb_swaps = n;
-      swap_range = n / 3;
-      printf("nbSwaps:%zu    swapRange:%zu\n", nb_swaps, swap_range);
-      for (j = 0; j < nbRuns; j++) {
-        seed = rand();
-        ls_compute_truncated_uniform_swap_permutation(P, seed, n, nb_swaps, swap_range);
-        for (i = 0; i < n; i++) {
-          printf("%zu ",P[i]);
-        }
-        printf("\n");
-      }
-      
-    }
-    
-      
-      
-    double *rand_01;
-    long lb, ub, range;
-    int *freq;
-    
-    nbRuns = 0;
-    if (nbRuns) {
-      rand_01 = coco_allocate_vector(1);
-      lb = -13;
-      ub = 1;
-      range = ub - lb + 1;
-      freq = (int*) coco_allocate_memory(range * sizeof(int));
-      for (i = 0; i < range; i++) {
-        freq[i] = 0;
-      }
-      long res;
-      for (j = 0; j < nbRuns; j++) {
-          seed = rand();
-          res = ls_rand_int(lb, ub, seed);
-          freq[res - lb]++;
-          //printf("%d: %d\n", j, res);
-      }
-
-      for ( i = 0; i < range; i++) {
-            printf("f[%ld]: %d\n", i + lb, freq[i]);
-      }
-      for ( i = 0; i < range - 1; i++) {
-        printf("f[%ld]/f[%ld]: %f\n", i + lb, i + 1 + lb, ((float) freq[i]) / ((float) freq[i+1]));
-      }
-      coco_free_memory(rand_01);
-      coco_free_memory(freq);
-    }
-    
-    nbRuns = 0;
-    if (nbRuns) {
-      int nb_vals = 10;
-      rand_01 = coco_allocate_vector(nbRuns);
-      seed = rand();
-      bbob2009_unif(rand_01, nbRuns, seed);
-
-      freq = (int*) coco_allocate_memory(nb_vals * sizeof(int));
-      for (i = 0; i < range; i++) {
-        freq[i] = 0;
-      }
-      for (j = 0; j < nbRuns; j++) {
-        freq[(int) (rand_01[j] * nb_vals)]++;
-        //printf("j: %f %d \n", rand_01[j], (int) (rand_01[j] * nb_vals));
-      }
-      
-      if (nbRuns) {
-        for ( i = 0; i < nb_vals; i++) {
-          printf("f[%d]: %f\n", i, freq[i] * 1. / nbRuns);
-        }
-      }
-      coco_free_memory(rand_01);
-      coco_free_memory(freq);
-    }
-    
+  assert(src != NULL);
+  assert(number_of_elements > 0);
+  
+  dst = (size_t *)coco_allocate_memory(number_of_elements * sizeof(size_t));
+  for (i = 0; i < number_of_elements; ++i) {
+    dst[i] = src[i];
   }
-  
-  
-  if (testBlockMatrix) {
-    double **B, *R, prod, sum;
-    int i, j, j1, j2;
-    n=0;
-    nb_entries = 0;
-    for (i = 0; i < nb_blocks; i++) {
-      n += blocksizes[i];
-      nb_entries += blocksizes[i] * blocksizes[i];
-    }
-    printf("nb_entries:%zu\n",nb_entries);
-    B = ls_allocate_blockmatrix(n,n,blocksizes,nb_blocks);
-    ls_compute_blockrotation(B, seed, n, blocksizes, nb_blocks);
-    R = coco_allocate_vector(nb_entries);
-    ls_copy_block_matrix(B, R, n, blocksizes, nb_blocks);
-
-    printf("1-D version: \n");
-    for (i = 0; i < nb_entries; i++) {
-      printf("%f ", R[i]);
-    }
-    printf("\n");
-    
-    // show matrix
-    idx_blocksize = 0;
-    next_bs_change = blocksizes[idx_blocksize];
-    for (i = 0; i < n; ++i) {
-      if (i >= next_bs_change) {
-        idx_blocksize++;
-        next_bs_change += blocksizes[idx_blocksize];
-      }
-      current_blocksize=blocksizes[idx_blocksize];
-      for (j = 0; j < current_blocksize; j++) {
-        printf("%f ",B[i][j]);
-      }
-      printf("\n");
-    }
-    
-    // test orthonormality
-    printf("\ntest orthonormality\n");
-    cumsum_prev_blocksizes = 0;
-    for (idx_block = 0; idx_block < nb_blocks; idx_block++) {
-      current_blocksize = blocksizes[idx_block];
-      for (j1 = 0; j1 < current_blocksize; j1++) {
-        for (j2 = 0; j2 < j1; j2++) {
-          prod = 0;
-          for (i = 0; i < current_blocksize; i++){
-            prod += B[i + cumsum_prev_blocksizes][j1] * B[i + cumsum_prev_blocksizes][j2];
-          }
-          printf("block%lu <%d,%d>: %f\n", idx_block + 1, j2 + 1, j1 + 1, prod);
-        }
-        
-        sum = 0;
-        for (i = 0; i < current_blocksize; i++) {
-          sum += B[i + cumsum_prev_blocksizes][j1] * B[i + cumsum_prev_blocksizes][j1];
-        }
-        printf("block%lu |%d|: %f\n", idx_block + 1, j1 + 1, sum);
-      }
-
-      cumsum_prev_blocksizes+=current_blocksize;
-    }
-  }
-  
-  
-  
-  
-  //ls_free_blockmatrix(B,n);
-  
-  
-  return 0;
-  
+  return dst;
 }
+
+
 
