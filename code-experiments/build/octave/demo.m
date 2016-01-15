69ed8352cef124353c3b814b4318277f22e4f7d7
brockhof <dimo.brockhoff@inria.fr>
2016-01-15 11:13:18 +0100

    deleted outdated Octave code to have a clean(er) release, rename of Matlab README to README.txt and corresponding change in README.md, additional improvements/corrections in README.md

--- code-experiments/build/octave/demo.m
+++
@@ -1,6 +0,0 @@
-my_benchmark = benchmark("bbob", "bbob", "random_search")
-while (true)
-    problem = next_problem(my_benchmark)
-    my_optimizer(problem, problem.lower_bounds, problem.upper_bounds, 1000)
-    free(problem)
-endwhile
