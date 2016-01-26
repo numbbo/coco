7735fa465399f381250edcecddb2f3ca117f672a
brockhof <dimo.brockhoff@inria.fr>
2016-01-26 23:44:32 +0100

    updated Sphinx configuration for bbob-biobj experimental setup documentation

--- docs/bbob-biobj-experiments/make.bat
+++ docs/bbob-biobj-experiments/make.bat
@@ -6,6 +6,8 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set BUILDDIR=build
+set HTMLBUILDDIR=../../../bbob-biobj-experiments-doc
+set PDF=bbob-biobj-experiments.pdf
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
 set I18NSPHINXOPTS=%SPHINXOPTS% source
 if NOT "%PAPER%" == "" (
@@ -18,26 +20,28 @@ if "%1" == "" goto help
 if "%1" == "help" (
 	:help
 	echo.Please use `make ^<target^>` where ^<target^> is one of
-	echo.  html       to make standalone HTML files
-	echo.  dirhtml    to make HTML files named index.html in directories
-	echo.  singlehtml to make a single large HTML file
-	echo.  pickle     to make pickle files
-	echo.  json       to make JSON files
-	echo.  htmlhelp   to make HTML files and a HTML help project
-	echo.  qthelp     to make HTML files and a qthelp project
-	echo.  devhelp    to make HTML files and a Devhelp project
-	echo.  epub       to make an epub
-	echo.  latex      to make LaTeX files, you can set PAPER=a4 or PAPER=letter
-	echo.  text       to make text files
-	echo.  man        to make manual pages
-	echo.  texinfo    to make Texinfo files
-	echo.  gettext    to make PO message catalogs
-	echo.  changes    to make an overview over all changed/added/deprecated items
-	echo.  xml        to make Docutils-native XML files
-	echo.  pseudoxml  to make pseudoxml-XML files for display purposes
-	echo.  linkcheck  to check all external links for integrity
-	echo.  doctest    to run all doctests embedded in the documentation if enabled
-	echo.  coverage   to run coverage check of the documentation if enabled
+	echo.  html           to make standalone HTML files
+	echo.  html-topublish to make HTML files in the repository bbob-biobj-experiments-doc"
+	echo.  latexpdf       to make LaTeX files and pdf in ../
+	echo.  dirhtml        to make HTML files named index.html in directories
+	echo.  singlehtml     to make a single large HTML file
+	echo.  pickle         to make pickle files
+	echo.  json           to make JSON files
+	echo.  htmlhelp       to make HTML files and a HTML help project
+	echo.  qthelp         to make HTML files and a qthelp project
+	echo.  devhelp        to make HTML files and a Devhelp project
+	echo.  epub           to make an epub
+	echo.  latex          to make LaTeX files, you can set PAPER=a4 or PAPER=letter
+	echo.  text           to make text files
+	echo.  man            to make manual pages
+	echo.  texinfo        to make Texinfo files
+	echo.  gettext        to make PO message catalogs
+	echo.  changes        to make an overview over all changed/added/deprecated items
+	echo.  xml            to make Docutils-native XML files
+	echo.  pseudoxml      to make pseudoxml-XML files for display purposes
+	echo.  linkcheck      to check all external links for integrity
+	echo.  doctest        to run all doctests embedded in the documentation if enabled
+	echo.  coverage       to run coverage check of the documentation if enabled
 	goto end
 )
 
@@ -77,6 +81,14 @@ if "%1" == "html" (
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
+	goto end
+)
+
+if "%1" == "html-topublish" (
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %HTMLBUILDDIR%
+	if errorlevel 1 exit /b 1
+	echo.
+	echo.Build finished. The HTML pages are in %HTMLBUILDDIR%.
 	goto end
 )
 
@@ -162,8 +174,9 @@ if "%1" == "latexpdf" (
 	cd %BUILDDIR%/latex
 	make all-pdf
 	cd %~dp0
+	move %BUILDDIR%\latex\%PDF% ..\
 	echo.
-	echo.Build finished; the PDF files are in %BUILDDIR%/latex.
+	echo.Build finished; see ../%PDF%.
 	goto end
 )
 
