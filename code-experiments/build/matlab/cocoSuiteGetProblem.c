dca2b123e7ce5db1164e52281498b076c36d0abd
brockhof <dimo.brockhoff@inria.fr>
2016-01-29 15:10:54 +0100

    added cocoSetLogLevel.c started to document the Matlab wrapper

--- code-experiments/build/matlab/cocoSuiteGetProblem.c
+++ code-experiments/build/matlab/cocoSuiteGetProblem.c
@@ -19,7 +19,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
     /* check for proper number of arguments */
     if(nrhs!=2) {
-        mexErrMsgIdAndTxt("cocoSuiteGetProblem:nrhs","Two inputs required.");
+        mexErrMsgIdAndTxt("cocoSuiteGetProblem:nrhs","Two inputs required.\n Try \'help cocoSuiteGetProblem.m\'.");
     }
     /* get problem_suite */   
     ref = (size_t *)mxGetData(prhs[0]);
