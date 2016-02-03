2f4685a9fbc06d7c24c73685c1b81a5bd9422f89
ttusar <tea.tusar@ijs.si>
2016-02-03 13:10:06 +0100

    Minor changes to the evaluation trigger. Created a unit test for the evaluation trigger. Marked coco_observer_evaluation_to_log as deprecated.

--- code-experiments/test/unit-test/test_coco_observer.c
+++ code-experiments/test/unit-test/test_coco_observer.c
@@ -16,7 +16,7 @@ static void test_coco_observer_evaluation_to_log(void **state) {
   for (i = 0; i < 6; i++) {
     dim = dimensions[i];
     for (evals = 1; evals < 1500; evals++) {
-      result = coco_observer_evaluation_to_log(evals, dim);
+      result = deprecated__coco_observer_evaluation_to_log(evals, dim);
       if ((evals == 1) || (evals == dim) || (evals == 2 * dim) || (evals == 5 * dim))
         assert(result);
       else if ((evals == 10 * dim) || (evals == 20 * dim) || (evals == 50 * dim))
@@ -31,6 +31,9 @@ static void test_coco_observer_evaluation_to_log(void **state) {
   (void)state; /* unused */
 }
 
+/**
+ * Tests the function coco_observer_targets_trigger.
+ */
 static void test_coco_observer_targets_trigger(void **state) {
 
   coco_observer_targets_t *targets = coco_observer_targets(10, 1e-8);
@@ -110,11 +113,42 @@ static void test_coco_observer_targets_trigger(void **state) {
   (void)state; /* unused */
 }
 
+/**
+ * Tests the function coco_observer_evaluations_trigger.
+ */
+static void test_coco_observer_evaluations_trigger(void **state) {
+
+  size_t dimensions[6] = { 2, 3, 5, 10, 20, 40 };
+  coco_observer_evaluations_t *evaluations;
+  size_t evals, i, dim;
+  int update;
+
+  for (i = 0; i < 6; i++) {
+    dim = dimensions[i];
+    evaluations = coco_observer_evaluations("1,2,5", dim);
+    for (evals = 1; evals < 1500; evals++) {
+      update = coco_observer_evaluations_trigger(evaluations, evals);
+      if ((evals == 1) || (evals == dim) || (evals == 2 * dim) || (evals == 5 * dim))
+        assert(update);
+      else if ((evals == 10 * dim) || (evals == 20 * dim) || (evals == 50 * dim))
+        assert(update);
+      else if ((evals == 100 * dim) || (evals == 200 * dim) || (evals == 500 * dim))
+        assert(update);
+      else
+        assert(!update);
+    }
+    coco_observer_evaluations_free(evaluations);
+  }
+
+  (void)state; /* unused */
+}
+
 static int test_all_coco_observer(void) {
 
   const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_coco_observer_evaluation_to_log),
-      cmocka_unit_test(test_coco_observer_targets_trigger)
+      cmocka_unit_test(test_coco_observer_targets_trigger),
+      cmocka_unit_test(test_coco_observer_evaluations_trigger)
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
