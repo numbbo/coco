929ca7d2b1772faebf11f6aca01b43c342c2ffa6
brockhof <dimo.brockhoff@inria.fr>
2016-02-09 17:16:10 +0100

    continued to document Matlab/Octave wrapper and delete unnecessary files

--- code-experiments/build/matlab/cocoSuiteGetNextProblem.m
+++ code-experiments/build/matlab/cocoSuiteGetNextProblem.m
@@ -19,6 +19,6 @@
 %   >> observer = cocoCall('cocoObserver', 'bbob-biobj', 'result_folder: test');
 %   COCO INFO: Results will be output to folder exdata\test
 %   >> problem = cocoCall('cocoSuiteGetNextProblem', suite, observer);
-%   COCO INFO: 09.02.16 16:31:16, d=2, running: f01.>> cocoCall('cocoObserverFree', suite);
+%   COCO INFO: 09.02.16 16:31:16, d=2, running: f01.>> cocoCall('cocoObserverFree', observer);
 %   >> cocoCall('cocoSuiteFree', suite);
 %
