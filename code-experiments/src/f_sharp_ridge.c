cc54d7b8875a1e26df65c154eedddf6c9c3957c9
NDManh <nguyenducmanh1982@yahoo.com>
2016-02-18 17:01:48 +0100

    Large scale for sharp ridge problem

--- code-experiments/src/f_sharp_ridge.c
+++ code-experiments/src/f_sharp_ridge.c
@@ -107,3 +107,77 @@ static coco_problem_t *f_sharp_ridge_bbob_problem_allocate(const size_t function
   coco_free_memory(xopt);
   return problem;
 }
+
+/**
+ * @brief Creates the BBOB sharp ridge problem in large dimension.
+ */
+static coco_problem_t *f_sharp_ridge_permblockdiag_bbob_problem_allocate(const size_t function,
+                                                                       const size_t dimension,
+                                                                       const size_t instance,
+                                                                       const long rseed,
+                                                                       const char *problem_id_template,
+                                                                       const char *problem_name_template) {
+    double *xopt, fopt;
+    coco_problem_t *problem = NULL;
+    double **B1;
+    double **B2;
+    const double *const *B1_copy;
+    const double *const *B2_copy;
+    size_t *P11 = coco_allocate_vector_size_t(dimension);
+    size_t *P21 = coco_allocate_vector_size_t(dimension);
+    size_t *P12 = coco_allocate_vector_size_t(dimension);
+    size_t *P22 = coco_allocate_vector_size_t(dimension);
+    size_t *block_sizes1;
+    size_t *block_sizes2;
+    size_t nb_blocks1;
+    size_t nb_blocks2;
+    size_t swap_range;
+    size_t nb_swaps;
+    
+    block_sizes1 = ls_get_block_sizes(&nb_blocks1, dimension);
+    block_sizes2 = ls_get_block_sizes(&nb_blocks2, dimension);
+    swap_range = ls_get_swap_range(dimension);
+    nb_swaps = ls_get_nb_swaps(dimension);
+    
+    xopt = coco_allocate_vector(dimension);
+    fopt = bbob2009_compute_fopt(function, instance);
+    bbob2009_compute_xopt(xopt, rseed, dimension);
+    
+    B1 = ls_allocate_blockmatrix(dimension, block_sizes1, nb_blocks1);
+    B1_copy = (const double *const *)B1;/*TODO: silences the warning, not sure if it prevents the modification of B at all levels*/
+    B2 = ls_allocate_blockmatrix(dimension, block_sizes2, nb_blocks2);
+    B2_copy = (const double *const *)B2;/*TODO: silences the warning, not sure if it prevents the modification of B at all levels*/
+    
+    ls_compute_blockrotation(B1, rseed + 1000000, dimension, block_sizes1, nb_blocks1);
+    ls_compute_blockrotation(B2, rseed + 2000000, dimension, block_sizes2, nb_blocks2);
+    ls_compute_truncated_uniform_swap_permutation(P11, rseed + 3000000, dimension, nb_swaps, swap_range);
+    ls_compute_truncated_uniform_swap_permutation(P21, rseed + 4000000, dimension, nb_swaps, swap_range);
+    ls_compute_truncated_uniform_swap_permutation(P12, rseed + 4000000, dimension, nb_swaps, swap_range);
+    ls_compute_truncated_uniform_swap_permutation(P22, rseed + 5000000, dimension, nb_swaps, swap_range);
+    
+    
+    problem = f_sharp_ridge_allocate(dimension);
+    problem = transform_vars_permblockdiag(problem, B1_copy, P11, P21, dimension, block_sizes1, nb_blocks1);
+    problem = transform_vars_conditioning(problem, 10.0);
+    problem = transform_vars_permblockdiag(problem, B2_copy, P12, P22, dimension, block_sizes2, nb_blocks2);
+    problem = transform_vars_shift(problem, xopt, 0);
+    problem = transform_obj_shift(problem, fopt);
+    
+    coco_problem_set_id(problem, problem_id_template, function, instance, dimension);
+    coco_problem_set_name(problem, problem_name_template, function, instance, dimension);
+    coco_problem_set_type(problem, "large_scale_block"); /*TODO: no large scale prefix*/
+    
+    ls_free_block_matrix(B1, dimension);
+    ls_free_block_matrix(B2, dimension);
+    coco_free_memory(P11);
+    coco_free_memory(P21);
+    coco_free_memory(P12);
+    coco_free_memory(P22);
+    coco_free_memory(block_sizes1);
+    coco_free_memory(block_sizes2);
+    coco_free_memory(xopt);
+    
+    return problem;
+}
+
+
