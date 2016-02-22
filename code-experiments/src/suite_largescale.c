ecb60008a586d71b2285b2d133214c128ac6464e
oaelhara <ouassimaitelhara@yahoo.fr>
2016-02-22 16:25:35 +0100

    large-scale suite: added uncommented use of f8 and f9 in the suite

--- code-experiments/src/suite_largescale.c
+++ code-experiments/src/suite_largescale.c
@@ -49,7 +49,7 @@ static coco_problem_t *coco_get_largescale_problem(const size_t function,
   /*const long rseed_17 = (long) (17 + 10000 * instance);*/
 
   /*TODO: finish implementing the large scale test-suite functions.
-          current list: 1-6,10-12,14*/
+          current list: 1-6,8-14*/
   if (function == 1) {
     problem = f_sphere_bbob_problem_allocate(function, dimension, instance, rseed,
                                              problem_id_template, problem_name_template);
@@ -72,11 +72,11 @@ static coco_problem_t *coco_get_largescale_problem(const size_t function,
     problem = NULL; /*f_step_ellipsoid_bbob_problem_allocate(function, dimension, instance, rseed,
                                                      problem_id_template, problem_name_template);*/
   } else if (function == 8) {
-    problem = NULL; /*f_rosenbrock_bbob_problem_allocate(function, dimension, instance, rseed,
-                                                 problem_id_template, problem_name_template);*/
+    problem = f_rosenbrock_bbob_problem_allocate(function, dimension, instance, rseed,
+                                                 problem_id_template, problem_name_template);
   } else if (function == 9) {
-    problem = NULL; /*f_rosenbrock_rotated_bbob_problem_allocate(function, dimension, instance, rseed,
-                                                         problem_id_template, problem_name_template);*/
+    problem = f_rosenbrock_permblockdiag_bbob_problem_allocate(function, dimension, instance, rseed,
+                                                         problem_id_template, problem_name_template);
   } else if (function == 10) {
     problem = f_ellipsoid_permblockdiag_bbob_problem_allocate(function, dimension, instance, rseed,
                                             problem_id_template, problem_name_template);
