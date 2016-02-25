751929d0069134d11cb50044b2b7731b5d595897
NDManh <nguyenducmanh1982@yahoo.com>
2016-02-25 18:05:26 +0100

    Put ''transform_obj_norm_by_dim'' before ''transform_obj_penalize'' in large scale

--- code-experiments/src/f_step_ellipsoid.c
+++ code-experiments/src/f_step_ellipsoid.c
@@ -229,64 +229,74 @@ static coco_problem_t *f_step_ellipsoid_permblockdiag_bbob_problem_allocate(cons
                                                               const long rseed,
                                                               const char *problem_id_template,
                                                               const char *problem_name_template) {
-  double alpha = 10.; /*parameter of rounding*/
-  double *xopt, fopt;
-  coco_problem_t *problem = NULL;
-  double **B1, **B2;
-  const double *const *B1_copy;
-  const double *const *B2_copy;
-  size_t *P11, *P12, *P21, *P22;
-  size_t *block_sizes1, *block_sizes2, nb_blocks1, nb_blocks2, swap_range1, swap_range2, nb_swaps1, nb_swaps2;
-  double penalty_factor = 1.;
-
-  xopt = coco_allocate_vector(dimension);
-  fopt = bbob2009_compute_fopt(function, instance);
-  bbob2009_compute_xopt(xopt, rseed, dimension);
-  
-  block_sizes1 = coco_get_block_sizes(&nb_blocks1, dimension, "bbob-largescale");
-  block_sizes2 = coco_get_block_sizes(&nb_blocks2, dimension, "bbob-largescale");
-  swap_range1 = coco_get_swap_range(dimension, "bbob-largescale");
-  swap_range2 = coco_get_swap_range(dimension, "bbob-largescale");
-  nb_swaps1 = coco_get_nb_swaps(dimension, "bbob-largescale");
-  nb_swaps2 = coco_get_nb_swaps(dimension, "bbob-largescale");
-
-  B1 = coco_allocate_blockmatrix(dimension, block_sizes1, nb_blocks1);
-  B2 = coco_allocate_blockmatrix(dimension, block_sizes2, nb_blocks2);
-  B1_copy = (const double *const *)B1;
-  B2_copy = (const double *const *)B2;
-  coco_compute_blockrotation(B1, rseed + 1000000, dimension, block_sizes1, nb_blocks1);
-  coco_compute_blockrotation(B2, rseed, dimension, block_sizes2, nb_blocks2);
-  
-  P11 = coco_allocate_vector_size_t(dimension);
-  P12 = coco_allocate_vector_size_t(dimension);
-  P21 = coco_allocate_vector_size_t(dimension);
-  P22 = coco_allocate_vector_size_t(dimension);
-  coco_compute_truncated_uniform_swap_permutation(P11, rseed + 2000000, dimension, nb_swaps1, swap_range1);
-  coco_compute_truncated_uniform_swap_permutation(P12, rseed + 3000000, dimension, nb_swaps1, swap_range1);
-  coco_compute_truncated_uniform_swap_permutation(P21, rseed + 4000000, dimension, nb_swaps2, swap_range2);
-  coco_compute_truncated_uniform_swap_permutation(P22, rseed + 5000000, dimension, nb_swaps2, swap_range2);
-
-  problem = f_step_ellipsoid_allocate(dimension);
-  
-  problem = transform_vars_permutation(problem, P22, dimension);
-  problem = transform_vars_blockrotation(problem, B2_copy, dimension, block_sizes2, nb_blocks2);
-  problem = transform_vars_permutation(problem, P21, dimension);
-  problem = transform_vars_round_step(problem, alpha);/* also puts \hat{z}_1 in x[dimension]*/
-  
-  problem = transform_vars_conditioning(problem, 10.0);
-  problem = transform_vars_permutation(problem, P12, dimension);
-  problem = transform_vars_blockrotation(problem, B1_copy, dimension, block_sizes1, nb_blocks1);
-  problem = transform_vars_permutation(problem, P11, dimension);
-  problem = transform_vars_shift(problem, xopt, 0);
-  
-  problem = transform_obj_norm_by_dim(problem);
-  problem = transform_obj_penalize(problem, penalty_factor);
-  problem = transform_obj_shift(problem, fopt);
-
-  coco_problem_set_id(problem, problem_id_template, function, instance, dimension);
-  coco_problem_set_name(problem, problem_name_template, function, instance, dimension);
-  coco_problem_set_type(problem, "large_scale_block_rotated");
-  
-  return problem;
+    double alpha = 10.; /*parameter of rounding*/
+    double *xopt, fopt;
+    coco_problem_t *problem = NULL;
+    double **B1, **B2;
+    const double *const *B1_copy;
+    const double *const *B2_copy;
+    size_t *P11, *P12, *P21, *P22;
+    size_t *block_sizes1, *block_sizes2, nb_blocks1, nb_blocks2, swap_range1, swap_range2, nb_swaps1, nb_swaps2;
+    double penalty_factor = 1.;
+    
+    xopt = coco_allocate_vector(dimension);
+    fopt = bbob2009_compute_fopt(function, instance);
+    bbob2009_compute_xopt(xopt, rseed, dimension);
+    
+    block_sizes1 = coco_get_block_sizes(&nb_blocks1, dimension, "bbob-largescale");
+    block_sizes2 = coco_get_block_sizes(&nb_blocks2, dimension, "bbob-largescale");
+    swap_range1 = coco_get_swap_range(dimension, "bbob-largescale");
+    swap_range2 = coco_get_swap_range(dimension, "bbob-largescale");
+    nb_swaps1 = coco_get_nb_swaps(dimension, "bbob-largescale");
+    nb_swaps2 = coco_get_nb_swaps(dimension, "bbob-largescale");
+    
+    B1 = coco_allocate_blockmatrix(dimension, block_sizes1, nb_blocks1);
+    B2 = coco_allocate_blockmatrix(dimension, block_sizes2, nb_blocks2);
+    B1_copy = (const double *const *)B1;
+    B2_copy = (const double *const *)B2;
+    coco_compute_blockrotation(B1, rseed + 1000000, dimension, block_sizes1, nb_blocks1);
+    coco_compute_blockrotation(B2, rseed, dimension, block_sizes2, nb_blocks2);
+    
+    P11 = coco_allocate_vector_size_t(dimension);
+    P12 = coco_allocate_vector_size_t(dimension);
+    P21 = coco_allocate_vector_size_t(dimension);
+    P22 = coco_allocate_vector_size_t(dimension);
+    coco_compute_truncated_uniform_swap_permutation(P11, rseed + 2000000, dimension, nb_swaps1, swap_range1);
+    coco_compute_truncated_uniform_swap_permutation(P12, rseed + 3000000, dimension, nb_swaps1, swap_range1);
+    coco_compute_truncated_uniform_swap_permutation(P21, rseed + 4000000, dimension, nb_swaps2, swap_range2);
+    coco_compute_truncated_uniform_swap_permutation(P22, rseed + 5000000, dimension, nb_swaps2, swap_range2);
+    
+    problem = f_step_ellipsoid_allocate(dimension);
+    
+    problem = transform_vars_permutation(problem, P22, dimension);
+    problem = transform_vars_blockrotation(problem, B2_copy, dimension, block_sizes2, nb_blocks2);
+    problem = transform_vars_permutation(problem, P21, dimension);
+    problem = transform_vars_round_step(problem, alpha);/* also puts \hat{z}_1 in x[dimension]*/
+    
+    problem = transform_vars_conditioning(problem, 10.0);
+    problem = transform_vars_permutation(problem, P12, dimension);
+    problem = transform_vars_blockrotation(problem, B1_copy, dimension, block_sizes1, nb_blocks1);
+    problem = transform_vars_permutation(problem, P11, dimension);
+    problem = transform_vars_shift(problem, xopt, 0);
+    
+    problem = transform_obj_norm_by_dim(problem);
+    problem = transform_obj_penalize(problem, penalty_factor);
+    problem = transform_obj_shift(problem, fopt);
+    
+    coco_problem_set_id(problem, problem_id_template, function, instance, dimension);
+    coco_problem_set_name(problem, problem_name_template, function, instance, dimension);
+    coco_problem_set_type(problem, "large_scale_block_rotated");
+    
+    coco_free_block_matrix(B1, dimension);
+    coco_free_block_matrix(B2, dimension);
+    coco_free_memory(P11);
+    coco_free_memory(P21);
+    coco_free_memory(P12);
+    coco_free_memory(P22);
+    coco_free_memory(block_sizes1);
+    coco_free_memory(block_sizes2);
+    coco_free_memory(xopt);
+    
+    return problem;
 }
 
