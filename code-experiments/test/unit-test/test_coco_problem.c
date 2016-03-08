34a945c006e090adf81de58710a9b887abb21df7
ttusar <tea.tusar@ijs.si>
2016-03-08 17:14:59 +0100

    Debugging NAN unit test.

--- code-experiments/test/unit-test/test_coco_problem.c
+++ code-experiments/test/unit-test/test_coco_problem.c
@@ -10,28 +10,44 @@
  */
 static void test_coco_evaluate_function(void **state) {
 
-	coco_suite_t *suite = coco_suite("bbob", NULL, NULL);
+	coco_suite_t *suite;
 	coco_problem_t *problem;
-	double *x = coco_allocate_vector(2);
-	double *y = coco_allocate_vector(2);
+	double *x;
+	double *y;
 
+	suite = coco_suite("bbob", NULL, NULL);
+	x = coco_allocate_vector(2);
+	y = coco_allocate_vector(1);
   while ((problem = coco_suite_get_next_problem(suite, NULL)) != NULL) {
-
   	if (coco_problem_get_dimension(problem) > 2)
   		continue;
-
   	x[0] = 0;
   	x[1] = NAN;
     coco_evaluate_function(problem, x, y);
-
     if (!coco_vector_contains_nan(y, coco_problem_get_number_of_objectives(problem))) {
-    	coco_warning("nan = %f, y0 = %f, y1 = %f", NAN, y[0], y[1]);
+    	coco_warning("true nan = %d, nan = %f, y0 = %f, (nan == y0) = %d", TRUE_NAN, NAN, y[0], y[0] == NAN);
     }
-
     assert(coco_vector_contains_nan(y, coco_problem_get_number_of_objectives(problem)));
-
   }
+  coco_suite_free(suite);
+  coco_free_memory(x);
+  coco_free_memory(y);
 
+	suite = coco_suite("bbob-biobj", NULL, NULL);
+	x = coco_allocate_vector(2);
+	y = coco_allocate_vector(2);
+  while ((problem = coco_suite_get_next_problem(suite, NULL)) != NULL) {
+  	if (coco_problem_get_dimension(problem) > 2)
+  		continue;
+  	x[0] = 0;
+  	x[1] = NAN;
+    coco_evaluate_function(problem, x, y);
+    if (!coco_vector_contains_nan(y, coco_problem_get_number_of_objectives(problem))) {
+    	coco_warning("true nan = %d, nan = %f, y0 = %f, y1 = %f, (nan == y0) = %d, (nan == y1) = %d", TRUE_NAN,
+    			NAN, y[0], y[1], y[0] == NAN, y[1] == NAN);
+    }
+    assert(coco_vector_contains_nan(y, coco_problem_get_number_of_objectives(problem)));
+  }
   coco_suite_free(suite);
   coco_free_memory(x);
   coco_free_memory(y);
