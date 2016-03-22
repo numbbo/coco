67dfb6e300c85837e20c420d9787d2a5c87c0c74
oaelhara <ouassimaitelhara@yahoo.fr>
2016-03-22 11:51:28 +0100

    f_rastrigin: corrected use of P11, P12,... to remain consistent with other functions

--- code-experiments/src/f_rastrigin.c
+++ code-experiments/src/f_rastrigin.c
@@ -181,62 +181,57 @@ static coco_problem_t *f_rastrigin_permblockdiag_bbob_problem_allocate(const siz
   size_t swap_range1, swap_range2;
   size_t nb_swaps1, nb_swaps2;
 
-  
   block_sizes1 = coco_get_block_sizes(&nb_blocks1, dimension, "bbob-largescale");
   block_sizes2 = coco_get_block_sizes(&nb_blocks2, dimension, "bbob-largescale");
   swap_range1 = coco_get_swap_range(dimension, "bbob-largescale");
   swap_range2 = coco_get_swap_range(dimension, "bbob-largescale");
   nb_swaps1 = coco_get_nb_swaps(dimension, "bbob-largescale");
   nb_swaps2 = coco_get_nb_swaps(dimension, "bbob-largescale");
-  
+
   xopt = coco_allocate_vector(dimension);
   fopt = bbob2009_compute_fopt(function, instance);
   bbob2009_compute_xopt(xopt, rseed, dimension);
-  
+
   B1 = coco_allocate_blockmatrix(dimension, block_sizes1, nb_blocks1);
   B2 = coco_allocate_blockmatrix(dimension, block_sizes2, nb_blocks2);
   B1_copy = (const double *const *)B1;
   B2_copy = (const double *const *)B2;
-  
+
   coco_compute_blockrotation(B1, rseed + 1000000, dimension, block_sizes1, nb_blocks1);
   coco_compute_blockrotation(B2, rseed + 2000000, dimension, block_sizes2, nb_blocks2);
-  
+
   coco_compute_truncated_uniform_swap_permutation(P11, rseed + 3000000, dimension, nb_swaps1, swap_range1);
   coco_compute_truncated_uniform_swap_permutation(P12, rseed + 4000000, dimension, nb_swaps1, swap_range1);
   coco_compute_truncated_uniform_swap_permutation(P21, rseed + 5000000, dimension, nb_swaps2, swap_range2);
   coco_compute_truncated_uniform_swap_permutation(P22, rseed + 6000000, dimension, nb_swaps2, swap_range2);
   
   problem = f_rastrigin_allocate(dimension);
-  problem = transform_vars_permutation(problem, P22, dimension);
-  problem = transform_vars_blockrotation(problem, B2_copy, dimension, block_sizes2, nb_blocks2);
   problem = transform_vars_permutation(problem, P12, dimension);
-  
-  problem = transform_vars_conditioning(problem, 10.0);
-  
-  problem = transform_vars_permutation(problem, P21, dimension);
   problem = transform_vars_blockrotation(problem, B1_copy, dimension, block_sizes1, nb_blocks1);
   problem = transform_vars_permutation(problem, P11, dimension);
-
-  problem = transform_vars_asymmetric(problem, 0.2);
-
-  problem = transform_vars_oscillate(problem);
+  problem = transform_vars_conditioning(problem, 10.0);
   problem = transform_vars_permutation(problem, P22, dimension);
   problem = transform_vars_blockrotation(problem, B2_copy, dimension, block_sizes2, nb_blocks2);
+  problem = transform_vars_permutation(problem, P21, dimension);
+  problem = transform_vars_asymmetric(problem, 0.2);
+  problem = transform_vars_oscillate(problem);
   problem = transform_vars_permutation(problem, P12, dimension);
+  problem = transform_vars_blockrotation(problem, B1_copy, dimension, block_sizes1, nb_blocks1);
+  problem = transform_vars_permutation(problem, P11, dimension);
   problem = transform_vars_shift(problem, xopt, 0);
 
   problem = transform_obj_scale(problem, 1.0 / (double) dimension);
   problem = transform_obj_shift(problem, fopt);
-  
+
   coco_problem_set_id(problem, problem_id_template, function, instance, dimension);
   coco_problem_set_name(problem, problem_name_template, function, instance, dimension);
   coco_problem_set_type(problem, "block-rotated_multi-modal");
-  
+
   coco_free_block_matrix(B1, dimension);
   coco_free_block_matrix(B2, dimension);
   coco_free_memory(P11);
-  coco_free_memory(P21);
   coco_free_memory(P12);
+  coco_free_memory(P21);
   coco_free_memory(P22);
   coco_free_memory(block_sizes1);
   coco_free_memory(block_sizes2);
