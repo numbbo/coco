afd3b2bea8066d9feeaaf2050998e09b78081c53
brockhof <dimo.brockhoff@inria.fr>
2016-05-11 00:21:51 +0200

    corrected wrong caption in many-algorithm template about Bonferroni correction (issue #980)

--- code-postprocessing/bbob_pproc/compall/pptables.py
+++ code-postprocessing/bbob_pproc/compall/pptables.py
@@ -63,7 +63,7 @@ def get_table_caption():
         Entries, succeeded by a star, are statistically significantly better (according to
         the rank-sum test) when compared to all other algorithms of the table, with
         $p = 0.05$ or $p = 10^{-k}$ when the number $k$ following the star is larger
-        than 1, with Bonferroni correction by the number of instances. """ +
+        than 1, with Bonferroni correction of #2. """ +
         (r"""A $\downarrow$ indicates the same tested against the best
         algorithm of BBOB-2009."""
         if not (testbedsettings.current_testbed.name == testbedsettings.testbed_name_bi)
