0c3a3da5659323105d8c6b30e6c3e8ff0470b2ca
brockhof <dimo.brockhoff@inria.fr>
2016-07-07 16:51:50 +0200

    added reference sets to plots, corrected bug with using arrxopt instead of xopt (not so clear why it was used before)

--- docs/bbob-biobj/functions/code/plots_alongDirections.py
+++ docs/bbob-biobj/functions/code/plots_alongDirections.py
@@ -18,10 +18,12 @@ import generate_plots
 ###########################################
 # parameters to play with:
 dims = (5,)
-functions = (1,2,3)
+functions = range(1,56)
+#functions = (1,2,3,10,20,30,54,55)
 instances = (1,)
 # Note: in single-objective bbobdocfunctions.pdf documentation, '0' seems to be the instance used
-inputfolderforParetoFronts = 'archives/before_workshop/archives/'
+#inputfolderforParetoFronts = 'archives/before_workshop/archives/'
+inputfolderforParetoFronts = 'F:/5D-archives-from-Tea/after_workshop/archives/'
 outputfolder = 'plots/before_workshop/'
 tofile = True # if True: files are written; if False: no files but screen output
 ###########################################
@@ -52,4 +54,6 @@ for problem_index, problem in enumerate(suite):
         print("processing %s..." % problem.id)
     
     generate_plots.generate_plots(f, d, i, f1_id, f2_id, i1, i2,
-                                  folder=outputfolder, tofile=tofile)
+                                  outputfolder=outputfolder, 
+                                  inputfolder=inputfolderforParetoFronts,
+                                  tofile=tofile)
