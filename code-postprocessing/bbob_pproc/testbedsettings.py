corrected bug with scaling plots, updated bestalg (now derived from all 15 official BBOB-2016 submissions)

code-postprocessing/bbob_pproc/testbedsettings.py
@@ -152,9 +152,11 @@ class GECCOBiObjBBOBTestbed(Testbed):
         self.ppfig2_ftarget = 1e-5                
         self.ppfigdim_target_values = targetValues((1e-1, 1e-2, 1e-3, 1e-4, 1e-5))  # possibly changed in config
         self.pprldistr_target_values = targetValues((1e-1, 1e-2, 1e-3, 1e-5))  # possibly changed in config
-        target_values = np.append(np.append(10 ** np.arange(0, -5.1, -0.1), [0]), -10 ** np.arange(-5, -3.9, 0.2))
+        #target_values = np.append(np.append(10 ** np.arange(0, -5.1, -0.1), [0]), -10 ** np.arange(-5, -3.9, 0.2))
+        target_values = 10 ** np.arange(0, -5.1, -0.1)
         self.pprldmany_target_values = targetValues(target_values)  # possibly changed in config
-        self.pprldmany_target_range_latex = '$\{-10^{-4}, -10^{-4.2}, $ $-10^{-4.4}, -10^{-4.6}, -10^{-4.8}, -10^{-5}, 0, 10^{-5}, 10^{-4.9}, 10^{-4.8}, \dots, 10^{-0.1}, 10^0\}$'
+        #self.pprldmany_target_range_latex = '$\{-10^{-4}, -10^{-4.2}, $ $-10^{-4.4}, -10^{-4.6}, -10^{-4.8}, -10^{-5}, 0, 10^{-5}, 10^{-4.9}, 10^{-4.8}, \dots, 10^{-0.1}, 10^0\}$'
+        self.pprldmany_target_range_latex = '$\{-10^{-4}, -10^{-4.2}, $ $-10^{-4.4}, -10^{-4.6}, -10^{-4.8}, -10^{-5}\}$'
         # ppscatter_target_values are copied from the single objective case. Define the correct values!
         self.ppscatter_target_values = targetValues(np.logspace(-8, 2, 46))  # that does not look right here!
         self.rldValsOfInterest = (1e-1, 1e-2, 1e-3, 1e-4, 1e-5)  # possibly changed in config
@@ -169,11 +171,12 @@ class GECCOBiObjBBOBTestbed(Testbed):
         #self.pptablemany_targetsOfInterest = self.pptable_targetsOfInterest
         self.pptablemany_targetsOfInterest = targetValues((1, 1e-1, 1e-2, 1e-3))  # used for pptables
         self.scenario = scenario_biobjfixed
-        self.best_algorithm_filename = ''
-        self.best_algorithm_displayname = '' # TODO: should be read in from data set in best_algorithm_filename
+        self.best_algorithm_filename = 'bestbiobjalgentries2016.pickle.gz'
+        self.best_algorithm_displayname = 'best 2016' # TODO: should be read in from data set in best_algorithm_filename
         self.short_names = get_short_names(self.shortinfo_filename)
         # expensive optimization settings:
         self.pptable_target_runlengths = [0.5, 1.2, 3, 10, 50]  # [0.5, 2, 10, 50]  # used in config for expensive setting
         self.pptable2_target_runlengths = [0.5, 1.2, 3, 10, 50]  # [0.5, 2, 10, 50]  # used in config for expensive setting
         self.pptables_target_runlengths = [2, 10, 50]  # used in config for expensive setting
-        self.instancesOfInterest = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}  # 2016 biobjective instances
+        self.instancesOfInterest = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1}  # 2016 biobjective instances
+        #self.instancesOfInterest = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1}