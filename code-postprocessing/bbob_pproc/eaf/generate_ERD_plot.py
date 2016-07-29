912b29e9dc5dbee905b203381d8a47783a79aa8a
brockhof <dimo.brockhoff@inria.fr>
2016-07-29 15:36:58 +0200

    added logscale option

--- code-postprocessing/bbob_pproc/eaf/generate_ERD_plot.py
+++ code-postprocessing/bbob_pproc/eaf/generate_ERD_plot.py
@@ -8,6 +8,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from matplotlib import patches
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np  # "pip install numpy" installs numpy
 import os
 import sys
@@ -20,6 +21,8 @@ import bbobbenchmarks as bm
 logscale = True
 downsample = True
 decimals=2 # precision for downsampling
+maxplot = 2 # maximal displayed value (assuming nadir in [1,1])
+precision = 5e-4 # smallest displayed value at maxplot*precision in logscale
 
 biobjinst = {1: [2, 4],
              2: [3, 5],
@@ -104,9 +107,15 @@ def generate_ERD_plot(f_id, dim, inst_id, f1_id, f2_id, f1_instance, f2_instance
                     splitline = line.split()
                     newline = np.array(splitline[:3], dtype=np.float)
                     # normalize objective vector:
-                    newline[1] = (newline[1]-ideals[instance][0])/(nadirs[instance][0]-ideals[instance][0])
-                    newline[2] = (newline[2]-ideals[instance][1])/(nadirs[instance][1]-ideals[instance][1])
+                    if logscale:
+                        newline[1] = np.log10((newline[1]-ideals[instance][0])/(nadirs[instance][0]-ideals[instance][0]))
+                        newline[2] = np.log10((newline[2]-ideals[instance][1])/(nadirs[instance][1]-ideals[instance][1]))
+                    else:
+                        newline[1] = (newline[1]-ideals[instance][0])/(nadirs[instance][0]-ideals[instance][0])
+                        newline[2] = (newline[2]-ideals[instance][1])/(nadirs[instance][1]-ideals[instance][1])
+
                     B.append(newline)
+                    
 
             # store data of final instance:
             if instance not in A and not instance == -1:
@@ -128,41 +137,86 @@ def generate_ERD_plot(f_id, dim, inst_id, f1_id, f2_id, f1_instance, f2_instance
 #            plt.plot(a[1], a[2], 'xk')
 
 
+#    for a in A[1]:
+#        plt.plot(a[1], a[2], 'xk')
+#    plt.show()
+
     
     # plot grid in normalized 2*[ideal, nadir]:
-    n = 100 # number of grid points per objective
-    maxgrid = 2 # maximal displayed value (assuming nadir in [1,1])
-    gridpoints = maxgrid * np.array(list(product(range(n),range(n))))/(n-1)
-    #gridpoints[:,0] = maxgrid * gridpoints[:,0]
-    #gridpoints[:,1] = maxgrid * gridpoints[:,1]
+    n = 50 # number of grid points per objective
+    if logscale:    
+        #gridpoints = 10**(np.log10(maxplot) * (np.array(list(product(range(n),range(n))))/(n-1)))
+        gridpoints = np.log10(maxplot * 10**(np.log10(precision)*np.array(list(product(range(n),range(n))))/(n-1)))
+    else:
+        gridpoints = maxplot * np.array(list(product(range(n),range(n))))/(n-1)
     
-    colors = 1-np.log10(compute_aRT(gridpoints, A))
+    colors = compute_aRT(gridpoints, A)
 
-    #plt.scatter(gridpoints[:,0], gridpoints[:,1], c=colors, cmap='Blues', lw=0)
-    plt.scatter(gridpoints[:,0], gridpoints[:,1], c=colors, cmap='autumn', lw=0)
+    # normalize colors:
+    print(colors)
+    colors = 255*(colors/max(colors))
+    print(colors)
+    
+    #plt.scatter(gridpoints[:,0], gridpoints[:,1], c=colors, cmap='autumn_r', lw=0, norm=matplotlib.colors.LogNorm())
+    plt.scatter(gridpoints[:,0], gridpoints[:,1], marker='s', c=colors, cmap='autumn_r', s=80, lw=0, norm=matplotlib.colors.LogNorm())
+    
+    
+
+#    autumn = plt.get_cmap('autumn_r') 
+#    scalarMap = matplotlib.cm.ScalarMappable(cmap=autumn)
+#    scalarMap.set_norm(matplotlib.colors.LogNorm())
+
+    
+#    for i in range(len(gridpoints)):
+#        if not np.isfinite(colors[i]):
+#            continue # no finite aRT
+#        if logscale:
+#            ax.add_patch(patches.Rectangle(
+#                ((gridpoints[i])[0], (gridpoints[i])[1]),
+#                 np.log10(maxplot)-(gridpoints[i])[0],
+#                 np.log10(maxplot)-(gridpoints[i])[1],
+#                 alpha=1.0,
+#                 color=matplotlib.cm.autumn_r(colors[i])),
+#                 cmap='autumn_r')
+#        else:
+#            ax.add_patch(patches.Rectangle(
+#                (gridpoints[:,0], gridpoints[:,1]), maxplot-gridpoints[:,0], maxplot-gridpoints[:,1],
+#                alpha=0.05,
+#                color='k'))
+#            
     
     plt.colorbar()
     
     # beautify:
-    ax.set_xlabel(r'$f_1 - f_1^\mathsf{opt}$ (normalized)', fontsize=16)
-    ax.set_ylabel(r'$f_2 - f_2^\mathsf{opt}$ (normalized)', fontsize=16)
     ax.set_title("aRT in objective space for bbob-biobj function $f_{%d}$ (%d-D, %d instances)" % (f_id, dim, len(A)))
     [line.set_zorder(3) for line in ax.lines]
     [line.set_zorder(3) for line in ax.lines]
     #fig.subplots_adjust(left=0.1) # more room for the y-axis label
-    
     if logscale:                
-        ax.set_xscale('log')
-        ax.set_yscale('log')
+        ax.set_xlabel(r'log10($f_1 - f_1^\mathsf{opt}$) (normalized)', fontsize=16)
+        ax.set_ylabel(r'log10($f_2 - f_2^\mathsf{opt}$) (normalized)', fontsize=16)    
         # we might want to zoom in a bit:
-        ax.set_xlim((1e-3, maxgrid))
-        ax.set_ylim((1e-3, maxgrid))
+        ax.set_xlim((np.log10(precision), np.log10(maxplot)))
+        ax.set_ylim((np.log10(precision), np.log10(maxplot)))
     else:
+        ax.set_xlabel(r'$f_1 - f_1^\mathsf{opt}$ (normalized)', fontsize=16)
+        ax.set_ylabel(r'$f_2 - f_2^\mathsf{opt}$ (normalized)', fontsize=16)    
         # we might want to zoom in a bit:
-        ax.set_xlim((0, maxgrid))
-        ax.set_ylim((0, maxgrid))        
+        ax.set_xlim((0, maxplot))
+        ax.set_ylim((0, maxplot))        
     
-    plt.show()
+    # printing
+    if tofile:
+        if not os.path.exists(outputfolder):
+            os.makedirs(outputfolder)
+        filename = outputfolder + "aRTobjspace-f%02d-d%02d" % (f_id, dim)
+        if logscale:
+            filename = filename + "-log"
+        saveFigure(filename, verbose=True)
+    else:        
+        plt.show(block=True)
+
+    plt.close()
     
     
 def compute_aRT(points, A):
@@ -176,20 +230,25 @@ def compute_aRT(points, A):
     
     for key in A:
         points_finished = [False] * len(points)
-        runtime_to_attain_points = [np.inf] * len(points)
+        runtime_to_attain_points = [np.nan] * len(points)
         max_runtimes = np.zeros(len(points))
         for a in A[key]:
+#            print('..........................')
+#            print(a)
             for i in range(len(points)):
                 if not points_finished[i]:
                     if dominates(np.array([a[1], a[2]]), points[i]):
+#                        print("(%e, %e) dominates (%e, %e)" % (a[1], a[2], points[i][0], points[i][1]))
+#                        print("recorded runtime here: %d" % a[0])
                         runtime_to_attain_points[i] = a[0]
                         points_finished[i] = True
                     else:
+#                        print("(%e, %e) does not dominate (%e, %e)" % (a[1], a[2], points[i][0], points[i][1]))
                         max_runtimes[i] = a[0]
             if min(points_finished): # all grid points dominated
                 break
         for i in range(len(points)):
-            if runtime_to_attain_points[i] == np.inf:
+            if runtime_to_attain_points[i] == np.nan:
                 sum_runtimes_unsuccessful[i] = sum_runtimes_unsuccessful[i] + max_runtimes[i]
             else:
                 sum_runtimes_successful[i] = sum_runtimes_successful[i] + runtime_to_attain_points[i]
@@ -200,7 +259,7 @@ def compute_aRT(points, A):
         if num_runtimes_successful[i] > 0:
             aRT[i] = (sum_runtimes_unsuccessful[i] + sum_runtimes_successful[i])/num_runtimes_successful[i]
         else:
-            aRT[i] = np.inf
+            aRT[i] = np.nan
     
     return aRT
     
