d2b9d9c47f56ed33edc071630bb0cdf7cdc392e5
NDManh <nguyenducmanh1982@yahoo.com>
2016-08-02 20:18:22 +0200

    Change f_lunacek_bi_rastrigin in large scale

--- code-experiments/src/transform_vars_x_hat_generic.c
+++ code-experiments/src/transform_vars_x_hat_generic.c
@@ -20,6 +20,16 @@ typedef struct {
 } transform_vars_x_hat_generic_data_t;
 
 /**
+ * @brief Data type for the versatile_data_t
+ */
+typedef struct {
+    coco_problem_t *sub_problem_mu0;
+    coco_problem_t *sub_problem_mu1;
+    double *x_hat;
+} f_lunacek_bi_rastrigin_versatile_data_t;
+
+
+/**
  * @brief Evaluates the transformation.
  */
 static void transform_vars_x_hat_generic_evaluate(coco_problem_t *problem, const double *x, double *y) {
@@ -31,6 +41,7 @@ static void transform_vars_x_hat_generic_evaluate(coco_problem_t *problem, const
 
   for (i = 0; i < problem->number_of_variables; ++i) {
     data->x[i] = 2.0 * data->sign_vector[i] * x[i];
+    ((f_lunacek_bi_rastrigin_versatile_data_t *) problem->versatile_data)->x_hat[i] = data->x[i];
   }
   coco_evaluate_function(inner_problem, data->x, y);
   assert(y[0] + 1e-13 >= problem->best_value[0]);
