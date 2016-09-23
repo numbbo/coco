8c1a4a7619c0348fff431c26c019c70b5c0e4145
Phillipe Rodrigues Sampaio <phillipece@gmail.com>
2016-09-23 17:35:29 +0200

    prepare_html() in do.py back to default values

--- code-postprocessing/bbob_pproc/preparehtml.py
+++ code-postprocessing/bbob_pproc/preparehtml.py
@@ -41,7 +41,7 @@ def prepare_html(texFile):
     
     print('pdflatex done')
     
-    tthFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tth//tth')
+    tthFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tth\\tth.exe')
     args = "%s %s" % (tthFile, texFile)
     subprocess.call(args.split(), stdout=FNULL, stderr=FNULL, shell=False)    
 
