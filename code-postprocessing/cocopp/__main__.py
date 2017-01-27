6c0c68b96463228f17aa30669548ad6e477bbcc8
brockhof <dimo.brockhoff@inria.fr>
2017-01-27 20:32:12 +0100

    update of test after renaming of remote data folders (see #1233)

--- code-postprocessing/cocopp/__main__.py
+++ code-postprocessing/cocopp/__main__.py
@@ -103,7 +103,7 @@ def prepare_data(run_all_tests):
     # retrieve_algorithm(dataPath, '2010', 'IPOP-ACTCMA-ES_ros_noiseless.tar.gz')
     # [outcommented and replaced by BIPOP until 2010 data is in new format]
     retrieve_algorithm(dataPath, '2009', 'BFGS_ros_noiseless.tgz')
-    retrieve_algorithm(dataPath, 'bbob-biobj-2016', 'RS-4.tgz')
+    retrieve_algorithm(dataPath, 'bbob-biobj/2016', 'RS-4.tgz')
 
     if run_all_tests:
         retrieve_algorithm(dataPath, '2009', 'BIPOP-CMA-ES_hansen_noiseless.tgz')
@@ -114,9 +114,9 @@ def prepare_data(run_all_tests):
         retrieve_algorithm(dataPath, '2013', 'lmm-CMA-ES_auger_noiseless.tgz')
         retrieve_algorithm(dataPath, '2009', 'DE-PSO_garcia-nieto_noiseless.tgz')
         retrieve_algorithm(dataPath, '2009', 'VNS_garcia-martinez_noiseless.tgz')
-        retrieve_algorithm(dataPath, 'bbob-biobj-2016', 'RS-4.tgz')
-        retrieve_algorithm(dataPath, 'bbob-biobj-2016', 'RS-100.tgz')
-        retrieve_algorithm(dataPath, 'biobj-test', 'N-II.tgz') # diff. location and name due to Jenkins settings with too long paths
+        retrieve_algorithm(dataPath, 'bbob-biobj/2016', 'RS-4.tgz')
+        retrieve_algorithm(dataPath, 'bbob-biobj/2016', 'RS-100.tgz')
+        retrieve_algorithm(dataPath, 'test', 'N-II.tgz') # diff. location and name due to Jenkins settings with too long paths
         retrieve_algorithm(dataPath, '2009', 'BFGS_ros_noisy.tgz')
         retrieve_algorithm(dataPath, '2009', 'MCS_huyer_noisy.tgz')        
 
