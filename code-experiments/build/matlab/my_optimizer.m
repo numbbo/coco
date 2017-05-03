0953ad33e2289982a318abd85b85929d27fcb348
asmaatamna <asma.atamna@inria.fr>
2017-05-03 15:44:17 +0200

    interfacing fmincon: 1st draft

--- code-experiments/build/matlab/my_optimizer.m
+++ code-experiments/build/matlab/my_optimizer.m
@@ -1,10 +1,19 @@
+%function my_optimizer (problem, lower_bounds, upper_bounds, budget)
+%n = length(lower_bounds);
+%delta = upper_bounds - lower_bounds;
+%for i= 1:budget
+%    x = lower_bounds + rand(1,n) .* delta;
+%    cocoEvaluateFunction(problem, x);
+%    if cocoProblemGetNumberOfConstraints(problem) > 0
+%        cocoEvaluateConstraint(problem, x)
+%    end
+%end
+
+
 function my_optimizer (problem, lower_bounds, upper_bounds, budget)
 n = length(lower_bounds);
 delta = upper_bounds - lower_bounds;
-for i= 1:budget
-    x = lower_bounds + rand(1,n) .* delta;
-    cocoEvaluateFunction(problem, x);
-    if cocoProblemGetNumberOfConstraints(problem) > 0
-        cocoEvaluateConstraint(problem, x)
-    end
+x0 = lower_bounds + rand(1,n) .* delta;
+options = optimoptions('fmincon', 'Display', 'iter', 'MaxFunctionEvaluations', budget);
+x = fmincon(@(x)cocoEvaluateFunction(problem, x), x0, [], [], [], [], lower_bound, upper_bound, @(x)cocoEvaluateConstraint(problem, x), options);
 end
