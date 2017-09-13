f6a5367c2eb7969323de8b1a33360b74f0492631
nikohansen <nikolaus.hansen@inria.fr>
2017-09-13 14:30:03 +0200

    final message gives full path now
    	modified:   code-postprocessing/cocopp/rungeneric1.py
    	modified:   code-postprocessing/cocopp/rungenericmany.py

--- code-postprocessing/cocopp/rungeneric1.py
+++ code-postprocessing/cocopp/rungeneric1.py
@@ -502,7 +502,8 @@ def main(argv=None):
         prepend_to_file(latex_commands_file,
                         ['\\providecommand{\\algname}{' + 
                          (str_to_latex(strip_pathname1(args[0])) if len(args) == 1 else str_to_latex(dsList[0].algId)) + '{}}'])
-        print("Output data written to folder %s" % algoutputdir)
+        print("Output data written to folder %s" %
+              os.path.join(os.getcwd(), algoutputdir))
 
         plt.rcdefaults()
 
