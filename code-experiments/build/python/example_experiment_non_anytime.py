0fd3df79fa7e3773a8d51ade8c43cd2dea957a47
Tea Tusar <tea.tusar@ijs.si>
2017-09-15 15:31:36 +0200

    Some improvements of the non-anytime experiment.

--- code-experiments/build/python/example_experiment_non_anytime.py
+++ code-experiments/build/python/example_experiment_non_anytime.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-"""An example experiment for benchmarking non-anytime optimization algorithms with restarts.
+"""An example experiment for benchmarking non-anytime optimization algorithms
+with restarts.
 
-Non-anytime algorithms are those whose parameters depend not only on the dimensionality of the problem, but also on the
-budget of evaluations. Benchmarking such algorithms therefore entails running experiments on a test suite using various
-(increasing) budgets. These budgets can/should be provided by the user. If the user does not specify a budget list, a
-default budget list is used.
+Non-anytime algorithms are those whose parameters depend not only on the
+dimensionality of the problem, but also on the budget of evaluations.
+Benchmarking such algorithms therefore entails running experiments on a test
+suite using various (increasing) budgets. These budgets can/should be provided
+by the user. If the user does not specify a budget list, a default budget list
+is used.
 
-This code builds upon the example experiment for beginners and provides only simplistic progress information.
+This code builds upon the example experiment for beginners and provides only
+simplistic progress information.
 
 To apply the code to a different solver, add an interface to function `fmin`.
 """
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
+from __future__ import unicode_literals
 import cocoex  # experimentation module
-from numpy.random import rand  # for randomised restarts
+import numpy as np
 
 # for postprocessing
 import cocopp
@@ -25,32 +30,42 @@ import cma
 
 
 def default_budget_list(max_budget=10, num=100):
-    """Produces a budget list with at most `num` different increasing budgets within [1, `max_budget`] that are equally
-    spaced in the logarithmic space.
+    """Produces a budget list with at most `num` different increasing budgets
+    within [1, `max_budget`] that are equally spaced in the logarithmic space.
     """
-    from math import log10
-    from numpy import unique, logspace
-    return unique(logspace(0, log10(max_budget), num=num).astype(int))
+    return np.unique(np.logspace(0, np.log10(max_budget), num=num).astype(int))
 
 
 def fmin(problem, x0, solver, budget):
     """Invokes `solver` on `problem` with initial solution `x0`.
 
-    `solver` should evaluate the final/returned solution. Add here the interface to your solver.
+    `solver` should evaluate the final/returned solution. Add here the
+    interface to your solver.
     """
-    if solver.__name__ == 'fmin' and solver.__globals__['__name__'] in ['cma', 'cma.evolution_strategy', 'cma.es']:
-        return solver(problem, x0, 2, {'verbose': -9})  # set 'verbose': 1 for more output
+    if solver.__name__ == 'fmin':
 
-    elif solver.__name__ == 'fmin' and solver.__globals__['__name__'] == 'scipy.optimize.optimize':
-        return solver(problem, x0, xtol=1e-9, ftol=1e-9, maxfun=budget, disp=False)  # set disp=True for more output
+        if solver.__globals__['__name__'] in ['cma', 'cma.evolution_strategy',
+                                              'cma.es']:
+            return solver(problem, x0, 2,
+                          {'verbose': -9})  # set 'verbose': 1 for more output
+
+        elif solver.__globals__['__name__'] == 'scipy.optimize.optimize':
+            return solver(problem, x0, xtol=1e-9, ftol=1e-9, maxfun=budget,
+                          disp=False)  # set disp=True for more output
+
+        else:
+            return solver(problem, x0)
 
     elif solver.__name__ == 'differential_evolution':
-        pop_size_multiplier = 5  # pop_size = pop_size_multiplier * problem.dimension
-        # a strange but correct way to set max_iter so that the budget is respected
-        max_iter = max(1, int(budget / pop_size_multiplier / problem.dimension) - 1)
+        pop_size_multiplier = 5  # pop_size=pop_size_multiplier*dimension
+        # a strange but correct way to set max_iter to respect the budget
+        max_iter = max(1, int(
+            budget / pop_size_multiplier / problem.dimension) - 1)
         bounds = [x for x in zip(problem.lower_bounds, problem.upper_bounds)]
-        return solver(problem, bounds, popsize=pop_size_multiplier, maxiter=max_iter, strategy='best1bin',
-                      tol=1e-9, polish=False, disp=False)  # set disp=True for more output
+        return solver(problem, bounds, popsize=pop_size_multiplier,
+                      maxiter=max_iter, strategy='best1bin',
+                      tol=1e-9, polish=False,
+                      disp=False)  # set disp=True for more output
 
     else:
         return solver(problem, x0)
@@ -60,43 +75,51 @@ def main():
     ### input
     suite_name = "bbob"
     solver = scipy.optimize.fmin  # the Nelder-Mead downhill simplex method
-    # solver = scipy.optimize.differential_evolution  # to use differential evolution instead
+    # solver = scipy.optimize.differential_evolution  # to use DE instead
     # solver = cma.fmin  # to use CMA-ES instead
     algorithm_name = "Nelder-Mead"  # no spaces allowed
     output_folder = algorithm_name  # no spaces allowed
 
     # a list of increasing budgets (to be additionally multiplied by dimension)
-    # gradually increase `max_budget` to 10, 1000, ... or replace with a user-defined list
+    # gradually increase `max_budget` to 10, 100, ...
+    # or replace with a user-defined list
     budget_multiplier_list = default_budget_list(max_budget=5)
 
-    ### prepare (see http://numbbo.github.io/coco-doc/C/ for suite and observer parameters)
+    ### prepare (see http://numbbo.github.io/coco-doc/C/ for parameters)
     suite = cocoex.Suite(suite_name, "", "")
-    observer = cocoex.Observer(suite_name, "result_folder: {} algorithm_name: {}".format(output_folder, algorithm_name))
+    observer = cocoex.Observer(suite_name,
+                               "result_folder: {} algorithm_name: {}".format(
+                                   output_folder, algorithm_name))
 
     ### go
-    for index, _ in enumerate(suite):
+    for index in range(len(suite)):
         with ProblemNonAnytime(suite, observer, index) as problem:
             problem.print_progress()
-            for budget in [problem.dimension * x for x in budget_multiplier_list]:  # iterate over budgets
+            for budget in [problem.dimension * x for x in
+                           budget_multiplier_list]:  # iterate over budgets
                 problem.reset()
                 x0 = problem.initial_solution  # initial solution
-                # apply restarts while neither the problem is solved nor the budget is exhausted
-                while problem.evaluations < budget and not problem.final_target_hit:
-                    fmin(problem, x0, solver, budget)
-                    x0 = problem.random_solution  # a random initial solution for restarted algorithms
-            problem.free()
+                # apply restarts while neither the problem is solved nor the
+                # budget is exhausted
+                while problem.evaluations < budget and \
+                        not problem.final_target_hit:
+                    fmin(problem, x0, solver, budget - problem.evaluations)
+                    # a random initial solution for restarted algorithms
+                    x0 = problem.random_solution
 
     ### post-process data
-    cocopp.main(observer.result_folder)  # re-run folders look like "...-001" etc
+    cocopp.main(observer.result_folder)  # re-run folders look like "...-001"
     webbrowser.open("file://" + os.getcwd() + "/ppdata/index.html")
 
 
 class ProblemNonAnytime(object):
     """The non-anytime problem class.
 
-    Contains two problems - one observed and the other unobserved. It switches from unobserved to observed when
-    p_unobserved.evaluations >= p_observed.evaluations.
+    Contains two problems - one observed and the other unobserved. It switches
+    from unobserved to observed when p_unobserved.evaluations >=
+    p_observed.evaluations.
     """
+
     def __init__(self, suite, observer, index):
         self.suite = suite
         self.p_unobserved = suite[index]
@@ -118,8 +141,9 @@ class ProblemNonAnytime(object):
 
     def __call__(self, *arg, **args):
         if self.evaluations > self.p_observed.evaluations:
-            raise ValueError("Evaluations {} are larger than observed evaluations {}".format(
-                self.evaluations, self.p_observed.evaluations))
+            raise ValueError(
+                "Evaluations {} are larger than observed evaluations {}"
+                "".format(self.evaluations, self.p_observed.evaluations))
         if self.evaluations >= self.p_observed.evaluations:
             self._p = self.p_observed
         self.evaluations += 1
@@ -136,8 +160,10 @@ class ProblemNonAnytime(object):
     def print_progress(self):
         if not self.index % (len(self.suite) / len(self.suite.dimensions)):
             print("\nd={}: ".format(self.dimension))
-        print("{}{} ".format(self.id[self.id.index("_f")+1:self.id.index("_i")],
-                             self.id[self.id.index("_i"):self.id.index("_d")]), end='', flush=True)
+        print("{}{} ".format(
+            self.id[self.id.index("_f") + 1:self.id.index("_i")],
+            self.id[self.id.index("_i"):self.id.index("_d")]), end='',
+              flush=True)
         if not (self.index + 1) % 10:
             print("")
 
@@ -157,8 +183,9 @@ class ProblemNonAnytime(object):
 
     @property
     def random_solution(self):
-        return self.lower_bounds + ((rand(self.dimension) + rand(self.dimension)) *
-                                    (self.upper_bounds - self.lower_bounds) / 2)
+        return self.lower_bounds + (
+            (np.rand(self.dimension) + np.rand(self.dimension)) * (
+                self.upper_bounds - self.lower_bounds) / 2)
 
 
 if __name__ == '__main__':
