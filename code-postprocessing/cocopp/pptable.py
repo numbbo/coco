be94c3b2627b7c933f686f1c7f8218a2573d0344
nikohansen <nikohansen@users.noreply.github.com>
2018-02-01 17:42:32 +0100

    fix non-int type problem calling writeFEvals2

--- code-postprocessing/cocopp/pptable.py
+++ code-postprocessing/cocopp/pptable.py
@@ -293,8 +293,8 @@ def main(dsList, dims_of_interest, outputdir, latex_commands_file):
                     tableentryHtml = writeFEvalsMaxPrec(tmp, 2)
 
                     if np.isinf(tmp) and i == len(data)-1:
-                        tableentry = (tableentry + r'\textit{%s}' % writeFEvals2(np.median(entry.maxevals), 2, 3))
-                        tableentryHtml = (tableentryHtml + ' <i>%s</i>' % writeFEvals2(np.median(entry.maxevals), 2, 3))
+                        tableentry = (tableentry + r'\textit{%s}' % writeFEvals2(np.median(entry.maxevals).astype(int), 2, 3))
+                        tableentryHtml = (tableentryHtml + ' <i>%s</i>' % writeFEvals2(np.median(entry.maxevals).astype(int), 2, 4))
                         if is_bold:
                             tableentry = r'\textbf{%s}' % tableentry
                             tableentryHtml = '<b>%s</b>' % tableentryHtml
