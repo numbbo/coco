862945cfc6d4c53fedb0198472d68d1bd3e2dae4
Tea Tusar <tea.tusar@ijs.si>
2018-05-17 15:38:01 +0200

    Working towards a mixed-integer BBOB suite

--- code-experiments/src/transform_vars_discretize.c
+++ code-experiments/src/transform_vars_discretize.c
@@ -28,10 +28,7 @@ static void transform_vars_discretize_evaluate_function(coco_problem_t *problem,
   transform_vars_discretize_data_t *data;
   coco_problem_t *inner_problem;
   double *discretized_x = NULL;
-  double *inner_l = NULL;
-  double *inner_u = NULL;
-  double *outer_l = problem->smallest_values_of_interest;
-  double *outer_u = problem->largest_values_of_interest;
+  double inner_l, inner_u, outer_l, outer_u;
   
   if (coco_vector_contains_nan(x, coco_problem_get_dimension(problem))) {
   	coco_vector_set_to_nan(y, coco_problem_get_number_of_objectives(problem));
@@ -40,14 +37,15 @@ static void transform_vars_discretize_evaluate_function(coco_problem_t *problem,
 
   data = (transform_vars_discretize_data_t *) coco_problem_transformed_get_data(problem);
   inner_problem = coco_problem_transformed_get_inner_problem(problem);
-  inner_l = inner_problem->smallest_values_of_interest;
-  inner_u = inner_problem->largest_values_of_interest;
 
   discretized_x = coco_duplicate_vector(x, problem->number_of_variables);
   coco_problem_round_solution(problem, discretized_x);
   for (i = 0; i < problem->number_of_variables; ++i) {
-    discretized_x[i] = inner_l[i] + (inner_u[i] - inner_l[i]) * (x[i] - outer_l[i]) /
-        (outer_u[i] - outer_l[i]) - data->offset[i];
+    inner_l = inner_problem->smallest_values_of_interest[i];
+    inner_u = inner_problem->largest_values_of_interest[i];
+    outer_l = problem->smallest_values_of_interest[i];
+    outer_u = problem->largest_values_of_interest[i];
+    discretized_x[i] = inner_l + (inner_u - inner_l) * (x[i] - outer_l) / (outer_u - outer_l) - data->offset[i];
   }
   
   coco_evaluate_function(inner_problem, discretized_x, y);
@@ -72,6 +70,7 @@ static coco_problem_t *transform_vars_discretize(coco_problem_t *inner_problem,
                                                  const int *are_variables_integer) {
   transform_vars_discretize_data_t *data;
   coco_problem_t *problem = NULL;
+  double inner_l, inner_u, outer_l, outer_u, xopt;
   size_t i;
 
   data = (transform_vars_discretize_data_t *) coco_allocate_memory(sizeof(*data));
@@ -90,13 +89,23 @@ static coco_problem_t *transform_vars_discretize(coco_problem_t *inner_problem,
     problem->are_variables_integer[i] = are_variables_integer[i];
     if (are_variables_integer[i] == 0)
       data->offset[i] = 0;
-    else
-      data->offset[i] = problem->best_parameter[i];
+    else {
+      /* Compute the offset in four steps: */
+      inner_l = inner_problem->smallest_values_of_interest[i];
+      inner_u = inner_problem->largest_values_of_interest[i];
+      outer_l = problem->smallest_values_of_interest[i];
+      outer_u = problem->largest_values_of_interest[i];
+      /* Step 1: Find location of the optimum in the outer problem */
+      xopt = outer_l + (outer_u - outer_l) * (inner_problem->best_parameter[i] - inner_l) / (inner_u - inner_l);
+      /* Step 2: Round to closest integer */
+      xopt = coco_double_round(xopt);
+      problem->best_parameter[i] = xopt;
+      /* Step 3: Find the corresponding discretized value in the inner problem */
+      xopt = inner_l + (inner_u - inner_l) * (xopt - outer_l) / (outer_u - outer_l);
+      /* Step 4: Compute the difference */
+      data->offset[i] = xopt - inner_problem->best_parameter[i];
+    }
   }
-
-  /* Update the best parameter */
-  for (i = 0; i < problem->number_of_variables; i++)
-    coco_problem_round_solution(problem, problem->best_parameter);
     
   if (inner_problem->number_of_objectives > 0)
     problem->evaluate_function = transform_vars_discretize_evaluate_function;
