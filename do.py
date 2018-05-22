d531b54edefc0790ef7ab6766fca96e0b9c3cb50
asmaatamna <asma.atamna@inria.fr>
2018-05-22 16:03:53 +0200

    undoing change in do.py

--- do.py
+++ do.py
@@ -632,7 +632,7 @@ def build_java():
     write_file(git_revision(), "code-experiments/build/java/REVISION")
     write_file(git_version(), "code-experiments/build/java/VERSION")
     run('code-experiments/build/java', ['javac', 'CocoJNI.java'], verbose=_verbosity)
-    run('code-experiments/build/java', ['javac', '-h', 'CocoJNI'], verbose=_verbosity)
+    run('code-experiments/build/java', ['javah', 'CocoJNI'], verbose=_verbosity)
 
     # Finds the path to the headers jni.h and jni_md.h (platform-dependent)
     # and compiles the CocoJNI library (compiler-dependent). So far, only
