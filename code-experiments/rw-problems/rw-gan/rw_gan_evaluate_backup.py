19cf60905cf1e00dcec29bc531c97206451433c8
Tea Tušar <tea.tusar@ijs.si>
2018-05-26 21:15:40 +0200

    Updated the rw_gan_evaluate_backup.py to work with the redefined problem

--- code-experiments/rw-problems/rw-gan/rw_gan_evaluate_backup.py
+++ code-experiments/rw-problems/rw-gan/rw_gan_evaluate_backup.py
@@ -1,19 +1,14 @@
 if __name__ == '__main__':
-    expected_num_variables = 32
     path = '../../rw-problems/rw-gan/'
 
     # Read the variables
     with open(path + 'variables.txt') as f:
         content = f.readlines()
         content = [float(line.rstrip('\n')) for line in content]
-        num_variables = content[0]
-        if num_variables != expected_num_variables:
-            raise ValueError("num_variables should be '{}', but is '{}'"
-                             "".format(expected_num_variables, num_variables))
         f.close()
 
     # Compute the result
-    variable_sum = sum(content[1:])
+    variable_sum = abs(sum(content[1:]))
 
     # Write the result
     with open(path + 'objectives.txt', 'w') as f:
