15d25c4547be24c9ecb18f283e2919a6d3e16eca
Tea Tusar <tea.tusar@ijs.si>
2018-10-10 15:46:18 +0200

    Corrected text and comment regarding 2019 instances

--- code-postprocessing/cocopp/genericsettings.py
+++ code-postprocessing/cocopp/genericsettings.py
@@ -79,7 +79,7 @@ instancesOfInterest2017 = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 61: 1, 62: 1, 63: 1, 64
 instancesOfInterest2018 = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 71: 1, 72: 1, 73: 1, 74: 1,
                            75: 1, 76: 1, 77: 1, 78: 1, 79: 1, 80: 1}  # 2018 instances
 instancesOfInterest2019 = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 81: 1, 82: 1, 83: 1, 84: 1,
-                           85: 1, 86: 1, 87: 1, 88: 1, 89: 1, 90: 1}  # 2018 instances
+                           85: 1, 86: 1, 87: 1, 88: 1, 89: 1, 90: 1}  # 2019 instances
 instancesOfInterestBiobj2016 = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1}  # bi-objective 2016 instances
 instancesOfInterestBiobj2017 = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1,
                                 10: 1, 11: 1, 12: 1, 13: 1, 14:1, 15:1}  # bi-objective 2017 instances
