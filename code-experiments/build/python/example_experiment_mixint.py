99bd61426338a768a25f8f403ddbe84472071075
Tea Tusar <tea.tusar@ijs.si>
2018-10-10 10:52:51 +0200

    Adjustments to the mixint experiment

--- code-experiments/build/python/example_experiment_mixint.py
+++ code-experiments/build/python/example_experiment_mixint.py
@@ -8,26 +8,39 @@ from numpy.random import rand
 import scipy.optimize as opt
 import cma
 
-budget_multiplier = 1e2
-de = opt.differential_evolution
+budget_multiplier = 1e5
 
 
 def solve(solver_name, problem, x0, budget, de_pop_mult):
-    bounds = [x for x in zip(problem.lower_bounds, problem.upper_bounds)]
     if solver_name == "de":
         # a strange but correct way to set max_iter to respect the budget
         max_iter = max(1, int(budget / de_pop_mult / problem.dimension) - 1)
-        return de(problem, bounds, popsize=de_pop_mult, maxiter=max_iter, strategy='best1bin',
-                  tol=1e-9, polish=False, seed=224, disp=False)  # set disp=True for more output
-    elif solver_name == "cma-int":
-        return cma.fmin(problem, x0, 2, {'verbose': -9,
+        de = opt.differential_evolution
+        return de(problem,
+                  [x for x in zip(problem.lower_bounds, problem.upper_bounds)],
+                  popsize=de_pop_mult,
+                  maxiter=max_iter,
+                  strategy='best1bin',
+                  tol=1e-9,
+                  polish=False,
+                  seed=224,
+                  disp=False)  # set disp=True for more output
+    elif solver_name == "cma-b-int":
+        # CMA with bounds and integer variables
+        return cma.fmin(problem, x0, 0.5, {'verbose': -9,
                                          'maxfevals': budget,
-                                         'bounds': bounds,
+                                         'bounds': [problem.lower_bounds, problem.upper_bounds],
                                          'integer_variables': np.arange(problem.number_of_integer_variables)})
-    elif solver_name == "cma":
-        return cma.fmin(problem, x0, 2, {'verbose': -9,
+    elif solver_name == "cma-b-cont":
+        # CMA with bounds and continuous variables
+        return cma.fmin(problem, x0, 0.5, {'verbose': -9,
                                          'maxfevals': budget,
-                                         'bounds': bounds})
+                                         'bounds': [problem.lower_bounds, problem.upper_bounds]})
+    elif solver_name == "cma-cont":
+        # CMA without bounds and with continuous variables
+        return cma.fmin(problem, x0, 2, {'verbose': -9,
+                                         'maxfevals': budget})
+
 
 def run_experiment(suite_name, observer_name, solver_name):
     suite = cocoex.Suite(suite_name, "", "")
@@ -39,7 +52,7 @@ def run_experiment(suite_name, observer_name, solver_name):
         x0 = problem.initial_solution
         # population size and population sizde multiplier for DE
         de_pop_size = int(3 * np.power(np.log10(problem.dimension * budget_multiplier), 2))
-        de_pop_mult = int(de_pop_size / problem.dimension)
+        de_pop_mult = max(1, int(de_pop_size / problem.dimension))
         # apply restarts while neither the problem is solved nor the budget is exhausted
         while (problem.evaluations < problem.dimension * budget_multiplier
                and not problem.final_target_hit):
@@ -53,8 +66,11 @@ def run_experiment(suite_name, observer_name, solver_name):
 def run_all():
     observer_name = "bbob"
     for suite_name in ["bbob-mixint-1", "bbob-mixint-2"]:
-        for solver_name in ["de", "cma", "cma-int"]:
-            run_experiment(suite_name, observer_name, solver_name)
+        for solver_name in ["cma-b-int", "cma-b-cont", "cma-cont", "de"]:
+            try:
+                run_experiment(suite_name, observer_name, solver_name)
+            except ValueError as e:
+                print(e)
 
 
 if __name__ == '__main__':
