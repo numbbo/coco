4b636228c163322ed87473a9f3aa886a1d6fa073
Tea Tusar <tea.tusar@ijs.si>
2018-10-11 12:12:00 +0200

    Removed years from the instantiation of biobj problems in tests

--- code-experiments/test/regression-test/create/create_suite_data.py
+++ code-experiments/test/regression-test/create/create_suite_data.py
@@ -41,7 +41,10 @@ def generate_test_data_for_suite(suite_name, filename, solution_array=solution_a
     """
     if os.path.exists(filename):
         raise ValueError("file '" + filename + "' exists already")
-    suite = cocoex.Suite(suite_name, "year: 0000", "")
+    if "biobj" in suite_name:
+        suite = cocoex.Suite(suite_name, "instances: 1-10", "")
+    else:
+        suite = cocoex.Suite(suite_name, "year: 0000", "")
     xfc_dict = {}
     for i, f in enumerate(suite):
         for x in solution_array(f.dimension):
