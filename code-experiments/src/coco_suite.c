c0bd4244992168e4c14d7278cc59fdb48cff8330
Tea Tusar <tea.tusar@ijs.si>
2018-10-11 10:28:26 +0200

    Reorganized the code handling bi-objective problems

--- code-experiments/src/coco_suite.c
+++ code-experiments/src/coco_suite.c
@@ -70,10 +70,6 @@ static const char *coco_suite_get_instances_by_year(const coco_suite_t *suite, c
 
   if (strcmp(suite->suite_name, "bbob") == 0) {
     year_string = suite_bbob_get_instances_by_year(year);
-  } else if (strcmp(suite->suite_name, "bbob-biobj") == 0) {
-    year_string = suite_biobj_get_instances_by_year(year);
-  } else if (strcmp(suite->suite_name, "bbob-biobj-ext") == 0) {
-    year_string = suite_biobj_ext_get_instances_by_year(year);
   } else if (strcmp(suite->suite_name, "bbob-constrained") == 0) {
     year_string = suite_cons_bbob_get_instances_by_year(year);
   } else if (strcmp(suite->suite_name, "bbob-mixint-1") == 0) {
