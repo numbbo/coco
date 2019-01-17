31c8443291c0679e1c235806c1c3b8144856f2ae
brockhof <dimo.brockhoff@inria.fr>
2019-01-17 15:23:58 +0100

    added year `0000` of instances to initiate the tests

--- code-experiments/src/suite_largescale.c
+++ code-experiments/src/suite_largescale.c
@@ -42,7 +42,7 @@ static coco_suite_t *suite_largescale_initialize(void) {
  * @brief Sets the instances associated with years for the bbob large-scale suite.
  */
 static const char *suite_largescale_get_instances_by_year(const int year) {
-   if (year == 2016) {
+   if (year == 2016 || year == 0) {
     return "1-15";
   }
   else {
