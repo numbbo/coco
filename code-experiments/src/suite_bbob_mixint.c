d86126a0b9ff73885604ee52fc299217afaf43eb
Tea Tušar <tea.tusar@ijs.si>
2019-01-28 00:23:37 +0100

    Adjusted scaling

--- code-experiments/src/suite_bbob_mixint.c
+++ code-experiments/src/suite_bbob_mixint.c
@@ -17,12 +17,21 @@ static coco_suite_t *coco_suite_allocate(const char *suite_name,
                                          const char *default_instances);
 
 static double suite_bbob_mixint_scaling_factors[] = {
-       1, 1e-3,    1,    1,    1,  /* f1 to f5 */
-    1e-2,    1, 1e-1, 1e-1, 1e-3,  /* f6 to f10 */
-    1e-2, 1e-4,    1,    1,    1,  /* f11 to f15 */
+       1, 1e-3, 1e-1, 1e-1,    1,  /* f1 to f5 */
+    1e-2,    1, 1e-1, 1e-2, 1e-3,  /* f6 to f10 */
+    1e-2, 1e-4, 1e-1,    1, 1e-1,  /* f11 to f15 */
        1,   10,    1,   10, 1e-1,  /* f16 to f20 */
-       1,    1,    10,   1         /* f21 to f24 */
+       1,    1,   10, 1e-1         /* f21 to f24 */
 };
+
+/*
+static double suite_bbob_mixint_scaling_factors[] = {
+       1, 1e-3,    1,    1,    1,  * f1 to f5 *
+    1e-2,    1, 1e-1, 1e-1, 1e-3,  * f6 to f10 *
+    1e-2, 1e-4,    1,    1,    1,  * f11 to f15 *
+       1,   10,    1,   10, 1e-1,  * f16 to f20 *
+       1,    1,    10,   1         * f21 to f24 *
+}; */
 
 /**
  * @brief Sets the dimensions and default instances for the bbob-mixint suite.
