06c83def2b1979473f5a979140da333138dd0e34
Tea Tusar <tea.tusar@ijs.si>
2019-02-14 18:52:12 +0100

    Corrected problem types

--- code-experiments/src/f_bent_cigar_generalized.c
+++ code-experiments/src/f_bent_cigar_generalized.c
@@ -135,7 +135,7 @@ static coco_problem_t *f_bent_cigar_generalized_permblockdiag_bbob_problem_alloc
 
   coco_problem_set_id(problem, problem_id_template, function, instance, dimension);
   coco_problem_set_name(problem, problem_name_template, function, instance, dimension);
-  coco_problem_set_type(problem, "block_rotated_ill-conditioned");
+  coco_problem_set_type(problem, "3-ill-conditioned");
   coco_free_block_matrix(B, dimension);
   coco_free_memory(P1);
   coco_free_memory(P2);
