b437bfa7f7128b8b76ea3272496b393a575cdcb1
nikohansen <nikolaus.hansen@inria.fr>
2019-03-05 14:44:08 +0100

    revised example_experiment2.py

--- code-experiments/build/python/example_experiment2.py
+++ code-experiments/build/python/example_experiment2.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """A short and simple example experiment with restarts.
 
 The script is fully functional but also emphasises on readability. It
 features restarts, timings and recording termination conditions.
 
 To benchmark a different solver, `fmin` must be re-assigned and another
-`elif` block added around line 70 to account for the solver-specific
+`elif` block added around line 136 to account for the solver-specific
 call.
 
-When calling the script, variables can be re-assigned via a
-``name=value`` argument white spaces, where ``value`` is interpreted as
-a single python literal. Additionally, ``batch`` is recognized as
-argument defining the `current_batch` number and the number of
-`batches`, like ``batch=2/8`` runs batch 2 of 8.
+When calling the script, previously assigned variables can be re-assigned
+via a ``name=value`` argument without white spaces, where ``value`` is
+interpreted as a single python literal. Additionally, ``batch`` is recognized
+as argument defining the `current_batch` number and the number of `batches`,
+like ``batch=2/8`` runs batch 2 of 8.
 
 Examples, preceeded by "python" in an OS shell and by "run" in an IPython
 shell::
 
     example_experiment2.py budget_multiplier=3  # times dimension
 
-    example_experiment2.py budget_multiplier=1e4 cocopp=None
+    example_experiment2.py budget_multiplier=1e4 cocopp=None  # omit post-processing
     
     example_experiment2.py budget_multiplier=1e4 suite_name=bbob-biobj
 
     example_experiment2.py budget_multiplier=1000 batch=1/16
+
+Post-processing with `cocopp` is only invoked in the single-batch case.
 
 Details: ``batch=9/8`` is equivalent to ``batch=1/8``. The first number
 is taken modulo to the second.
@@ -37,11 +40,18 @@ from collections import defaultdict
 import os, webbrowser  # to show post-processed results in the browser
 import numpy as np  # for np.median
 import cocoex  # experimentation module
-import cocopp  # post-processing module, comment out if post-processing is not desired
+try: import cocopp  # post-processing module
+except: pass
 
 ### solver imports (add other imports if necessary)
 import scipy.optimize  # to define the solver to be benchmarked
-# import cma
+try: import cma
+except: pass  # may not be installed
+
+def random_search(f, lbounds, ubounds, evals):
+    """Won't work (well or at all) for `evals` much larger than 1e5"""
+    [f(x) for x in np.asarray(lbounds) + (np.asarray(ubounds) - lbounds)
+                               * np.random.rand(int(evals), len(ubounds))]
 
 ### input (to be modified if necessary/desired)
 # fmin = scipy.optimize.fmin
@@ -52,23 +62,31 @@ fmin = scipy.optimize.fmin_slsqp
 
 suite_name = "bbob"  # see cocoex.known_suite_names
 budget_multiplier = 2  # times dimension, increase to 10, 100, ...
-omit_last_dimension = False
-
+suite_filter_options = (# "dimensions: 2,3,5,10,20 " +  # skip dimension 40
+                        # "instance_indices: 1-5 " +  # relative to suite instances
+                        "")  # without filtering a suite has instance_indices 1-15
 batches = 1  # number of batches, batch=3/32 works to set both, current_batch and batches
 current_batch = 1  # only current_batch modulo batches is relevant
+output_folder = ''
 
 ### possibly modify/overwrite above input parameters from input args
 if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] in ('-h', 'help', '-help', '--help'):
+        print(__doc__)
+        raise ValueError("printed help and aborted")
     input_params = cocoex.utilities.args_to_dict(
         sys.argv[1:], globals(), {'batch': 'current_batch/batches'}, print=print)
     globals().update(input_params)  # (re-)assign variables
 
-# overwrites folder input parameter, comment out if desired otherwise
-output_folder = '%s_of_%s_%dD_on_%s' % (fmin.__name__, fmin.__module__,
-                                        int(budget_multiplier), suite_name)
+# extend output folder input parameter, comment out if desired otherwise
+output_folder += '%s_of_%s_%dD_on_%s' % (
+        fmin.__name__, fmin.__module__, int(budget_multiplier), suite_name)
+
+if batches > 1:
+    output_folder += "_batch%03dof%d" % (current_batch, batches)
 
 ### prepare
-suite = cocoex.Suite(suite_name, "", "")
+suite = cocoex.Suite(suite_name, "", suite_filter_options)
 observer = cocoex.Observer(suite_name, "result_folder: " + output_folder)
 minimal_print = cocoex.utilities.MiniPrint()
 stoppings = defaultdict(list)  # dict of lists, key is the problem index
@@ -78,60 +96,59 @@ timings = defaultdict(list)  # key is the dimension
 print('*** benchmarking %s from %s on suite %s ***'
       % (fmin.__name__, fmin.__module__, suite_name))
 time0 = time.time()
-for problem in suite:  # this loop may take several minutes or hours or days...
-    if omit_last_dimension and problem.dimension == max(suite.dimensions):
-        break
-    if problem.index % batches != current_batch % batches:
+for batch_counter, problem in enumerate(suite):  # this loop may take hours or days...
+    if batch_counter % batches != current_batch % batches:
         continue
     if not len(timings[problem.dimension]) and len(timings) > 1:
-        print("\n    done in %.1e seconds/evaluations"
-              % np.median(timings[sorted(timings)[-2]]), end='')
+        print("\n   %s %d-D done in %.1e seconds/evaluations"
+              % (minimal_print.stime, sorted(timings)[-2],
+                 np.median(timings[sorted(timings)[-2]])), end='')
     problem.observe_with(observer)  # generate the data for cocopp post-processing
-    maxevals = problem.dimension * budget_multiplier  # just giving a short name
+    problem(np.zeros(problem.dimension))  # making algorithms more comparable
+    propose_x0 = problem.initial_solution_proposal  # callable, all zeros in first call
+    evalsleft = lambda: int(problem.dimension * budget_multiplier + 1 -
+                            max((problem.evaluations, problem.evaluations_constraints)))
     time1 = time.time()
-
     # apply restarts
-    while max((problem.evaluations, problem.evaluations_constraints)) < maxevals and not problem.final_target_hit:
-        x0 = problem.initial_solution_proposal()  # give different proposals, all zeros in first call
+    irestart = -1
+    while evalsleft() > 0 and not problem.final_target_hit:
+        irestart += 1
+
         # here we assume that `fmin` evaluates the final/returned solution:
         if fmin is scipy.optimize.fmin:
-            output = fmin(problem, x0, maxfun=maxevals, disp=False, full_output=True)
+            output = fmin(problem, propose_x0(), maxfun=evalsleft(), disp=False, full_output=True)
             stoppings[problem.index].append(output[4])
         elif fmin is scipy.optimize.fmin_slsqp:
-            output = fmin(problem, x0, iter=int(budget_multiplier+1),  # very approximate way to respect budget
+            output = fmin(problem, propose_x0(), iter=int(evalsleft() / problem.dimension + 1),  # very approximate way to respect budget
                           full_output=True, iprint = -1)
             # print(problem.dimension, problem.evaluations)
             stoppings[problem.index].append(output[3:])
-        elif fmin is cocoex.solvers.random_search:
-            fmin(problem, problem.dimension * [-5], problem.dimension * [5],
-                 maxevals)
+        elif fmin in (cocoex.solvers.random_search, random_search):
+            fmin(problem, problem.dimension * [-5], problem.dimension * [5], evalsleft())
         elif fmin.__name__ == 'fmin2' and 'cma' in fmin.__module__:  # cma.fmin2:
-            xopt, es = fmin(problem, problem.initial_solution_proposal(), 2,
-                            {'maxfevals':maxevals, 'verbose':-9}, restarts=7)
+            xopt, es = fmin(problem, propose_x0, 2,
+                            {'maxfevals':evalsleft(), 'verbose':-9}, restarts=9)
             stoppings[problem.index].append(es.stop())
         elif fmin is scipy.optimize.fmin_cobyla:
-            fmin(problem, x0, lambda x: -problem.constraint(x), maxfun=maxevals,
-                   disp=0, rhoend=1e-9)
-        # add another solver here
+            fmin(problem, propose_x0(), lambda x: -problem.constraint(x), maxfun=evalsleft(),
+                 disp=0, rhoend=1e-9)
+        else: # add another solver here
+            raise NotImplementedError
 
     timings[problem.dimension].append((time.time() - time1) / problem.evaluations
                                       if problem.evaluations else 0)
-    minimal_print(problem, restarted=any(x0 != problem.initial_solution),
-                  final=problem.index == len(suite) - 1)
-    with open(output_folder + '_stopping_conditions.out', 'wt') as file_:
+    minimal_print(problem, restarted=irestart, final=problem.index == len(suite) - 1)
+    with open(output_folder + '_stopping_conditions.pydict', 'wt') as file_:
         file_.write("# code to read in these data:\n"
                     "# import ast\n"
-                    "# with open('%s_stopping_conditions.out', 'rt') as file_:\n"
+                    "# with open('%s_stopping_conditions.pydict', 'rt') as file_:\n"
                     "#     stoppings = ast.literal_eval(file_.read())\n"
                     % output_folder)
-        file_.write(repr(stoppings))
+        file_.write(repr(dict(stoppings)))
 
 ### print timings and final message
-print("\n  dimension  median seconds/evaluations")
-print("  -------------------------------------")
-for dimension in sorted(timings):
-    print("    %3d       %.1e" % (dimension, np.median(timings[dimension])))
-print("  -------------------------------------")
+print("\n   %s %d-D done in %.1e seconds/evaluations"
+      % (minimal_print.stime, sorted(timings)[-1], np.median(timings[sorted(timings)[-1]])))
 if batches > 1:
     print("*** Batch %d of %d batches finished in %s."
           " Make sure to run *all* batches (via current_batch or batch=#/#) ***"
@@ -139,6 +156,13 @@ if batches > 1:
 else:
     print("*** Full experiment done in %s ***"
           % cocoex.utilities.ascetime(time.time() - time0))
+
+print("Timing summary:\n"
+      "  dimension  median seconds/evaluations\n"
+      "  -------------------------------------")
+for dimension in sorted(timings):
+    print("    %3d       %.1e" % (dimension, np.median(timings[dimension])))
+print("  -------------------------------------")
 
 ### post-process data
 if batches == 1 and 'cocopp' in globals() and cocopp not in (None, 'None'):
