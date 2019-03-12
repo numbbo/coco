3824a741c62c44d4f0a7e9e3eb7e33cacbd5befc
Tea Tusar <tea.tusar@ijs.si>
2019-03-12 12:38:28 +0100

    Comment update

--- code-experiments/build/matlab/my_optimizer.m
+++ code-experiments/build/matlab/my_optimizer.m
@@ -3,7 +3,7 @@ n = length(lower_bounds);
 delta = upper_bounds - lower_bounds;
 for i= 1:budget
     x = lower_bounds + rand(1,n) .* delta;
-    % Round the variable values that need to be integer
+    % Round the variable values that need to be integer (not really needed)
     if num_integer_vars > 0
         x(1:num_integer_vars) = round(x(1:num_integer_vars));
     end
