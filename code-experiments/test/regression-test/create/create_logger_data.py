0cfe127b11c90ac52756d228ddbdf3ddefb2b1aa
Tea Tusar <tea.tusar@ijs.si>
2019-08-12 17:26:33 +0200

    Adding tests (not stable)

--- code-experiments/test/regression-test/create/create_logger_data.py
+++ code-experiments/test/regression-test/create/create_logger_data.py
@@ -3,39 +3,58 @@
 with the same name.
 """
 from __future__ import division, print_function
-import os
 import numpy as np
 import cocoex
 
 
-def run_experiment(logger, folder):
+def run_experiment(suite_name, logger, folder, order=''):
     """Runs random search on a subset of problems from either the bbob or bbob-biobj suites.
 
+    If order='', the problems are called in the usual order.
+    If order='rand', the problems are called in a random order.
+    If order='inst', the problems are split wrt their instances (first half of instances goes first,
+    then the other half).
     Reuses much of the code from the example experiment for beginners
+    Returns the name of the actual output folder
     """
     ### input
-    suite_name = logger
     budget_multiplier = 5
 
     ### prepare
-    suite = cocoex.Suite(suite_name, 'instances: 5-6,5-6', 'dimensions: 2,3 function_indices: 9-20')
+    suite = cocoex.Suite(suite_name, 'instances: 5-6,1-6', 'dimensions: 2,3 function_indices: 9-20')
     observer = cocoex.Observer(logger, 'result_folder: {}'.format(folder))
     minimal_print = cocoex.utilities.MiniPrint()
     np.random.seed(12345)
 
     ### go
-    for problem in suite:  # this loop will take several minutes or longer
-        problem.observe_with(observer)  # generates the data for cocopp post-processing
-        # apply restarts while neither the problem is solved nor the budget is exhausted
+    problem_indices = np.arange(len(suite))
+    if order == 'rand':
+        np.random.shuffle(problem_indices)
+    elif order == 'inst':
+        num_dims = len(suite.dimensions)
+        funcs = [x[x.find('_f')+1:x.find('_i')] for x in suite.ids()[0:int(len(suite)/num_dims)]]
+        num_func = len(set(funcs))
+        half_num_inst = int(len(suite) / num_func / num_dims / 2)
+        other_half_num_inst = len(suite) - half_num_inst * num_func * num_dims
+        mask = np.tile(np.append(np.ones(half_num_inst, dtype=bool),
+                                 np.zeros(other_half_num_inst, dtype=bool)),
+                       num_dims * num_func)
+        problem_indices = np.append(problem_indices[mask], problem_indices[~mask])
+    for problem_index in problem_indices:
+        problem = suite[problem_index]
+        problem.observe_with(observer)  #
         while (problem.evaluations < problem.dimension * budget_multiplier
                and not problem.final_target_hit):
             x = np.random.uniform(problem.lower_bounds, problem.upper_bounds, problem.dimension)
             problem(x)
         minimal_print(problem, final=problem.index == len(suite) - 1)
+    return observer.result_folder
 
 
 if __name__ == "__main__":
     for logger in ['bbob', 'bbob-biobj']:
-        data_folder = os.path.join('..', '..', 'data', '{}_logger_data'.format(logger))
-        run_experiment(logger, data_folder)
+        data_folder = '{}_logger_data'.format(logger)
+        run_experiment(logger, logger, data_folder)
+        data_folder = '{}_logger_data_rand'.format(logger)
+        run_experiment(logger, logger, data_folder, random_order=True)
 
