97d386d050bb5d70bf99f248b40e25fa2cd5195c
Tea Tusar <tea.tusar@ijs.si>
2020-05-05 11:37:38 +0200

    Reverted changes to random_search
    (cherry picked from commit 9863df6d6ff5201603a3f4f4257d416e00269f30)

--- code-experiments/build/python/python/solvers.py
+++ code-experiments/build/python/python/solvers.py
@@ -17,7 +17,9 @@ def random_search(fun, lbounds, ubounds, budget):
         X = lbounds + (ubounds - lbounds) * np.random.rand(chunk, dim)
         if fun.number_of_constraints > 0:
             C = [fun.constraint(x) for x in X]  # call constraints
-        F = [fun(x) for x in X]
+            F = [fun(x) for i, x in enumerate(X) if np.all(C[i] <= 0)]
+        else:
+            F = [fun(x) for x in X]
         if fun.number_of_objectives == 1:
             index = np.argmin(F) if len(F) else None
             if index is not None and (f_min is None or F[index] < f_min):
