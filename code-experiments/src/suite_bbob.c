54e3718a788a596206f01328b9e20e85eda64f59
brockhof <dimo.brockhoff@inria.fr>
2021-01-21 15:21:11 +0100

    added 2021 instances for bbob suite in preparation of BBOB'2021 workshop, corrected error when trying to run example experiment on bbob-biobj-mixint suite

--- code-experiments/src/suite_bbob.c
+++ code-experiments/src/suite_bbob.c
@@ -43,7 +43,7 @@ static coco_suite_t *suite_bbob_initialize(void) {
   const size_t num_dimensions = sizeof(dimensions) / sizeof(dimensions[0]);
 
   /* IMPORTANT: Make sure to change the default instance for every new workshop! */
-  suite = coco_suite_allocate("bbob", 24, num_dimensions, dimensions, "year: 2018");
+  suite = coco_suite_allocate("bbob", 24, num_dimensions, dimensions, "year: 2021");
 
   return suite;
 }
@@ -76,6 +76,9 @@ static const char *suite_bbob_get_instances_by_year(const int year) {
   }
   else if (year == 2018) {
     return "1-5,71-80";
+  }
+  else if (year == 2021) {
+    return "1-5,91-100";
   }
 
   else {
