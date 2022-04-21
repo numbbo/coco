numbbo/coco

commit f5289e354c1b1c8fc6b51aa5f90ca0ecbc0d2c60

polish x-limit code and make the setting changeable

code-postprocessing/cocopp/compall/pprldmany.py
@@ -46,7 +46,6 @@ from .. import pptex  # numtotex
 PlotType = ppfig.enum('ALG', 'DIM', 'FUNC')
 
 displaybest = True
-x_limit = genericsettings.x_limit_pprldmany
 divide_by_dimension = True
 annotation_line_end_relative = 1.11  # lines between graph and annotation
 annotation_space_end_relative = 1.24  # figure space end relative to x_limit
@@ -197,6 +196,7 @@ def plotdata(data, maxval=None, maxevals=None, CrE=0., **kwargs):
     :param kwargs: optional arguments provided to plot function.
     
     """
+    x_limit = genericsettings.xlimit_pprldmany
 
     # Expect data to be a ndarray.
     x = data[np.isnan(data) == False]  # Take away the nans
@@ -566,8 +566,8 @@ def main(dictAlg, order=None, outputdir='.', info='default',
     :param str parentHtmlFileName: defines the parent html page 
 
     """
-    global x_limit  
     global divide_by_dimension  # not fully implemented/tested yet
+    x_limit = genericsettings.xlimit_pprldmany
 
     tmp = pp.dictAlgByDim(dictAlg)
     algorithms_with_data = [a for a in dictAlg.keys() if dictAlg[a] != []]

code-postprocessing/cocopp/genericsettings.py
@@ -44,7 +44,11 @@ target_runlengths_ppscatter = np.logspace(np.log10(0.5), np.log10(50), 8) # used
 target_runlength = 10  # used in ppfigs.main
 single_runlength_factors = [0.5, 1.2, 3, 10] + [10 ** i for i in range(2, 12)] # used in pprldistr
 
-xlimit_expensive = 1e3  # used in 
+xlimit_pprldmany = 1e7
+"""maximal run length used in pprldmany, noisy: rather 1e8
+   maybe better: 10 * genericsettings.evaluation_setting[1]"""
+xlimit_expensive = 1e3
+"""maximal run length in expensive setting, used in config for pprldmany and ppfigdim"""
 #tableconstant_target_function_values = (
 #1e1, 1e0, 1e-1, 1e-3, 1e-5, 1e-7)  # used as input for pptables.main in rungenericmany
 # tableconstant_target_function_values = (1e3, 1e2, 1e1, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-7) # for post-workshop landscape tables
@@ -323,5 +327,3 @@ isLogLoss = True  # only affects rungeneric1
 isPickled = False  # only affects rungeneric1
 ##    
 isScatter = True  # only affects rungenericmany
-
-x_limit_pprldmany = 1e7  # better: 10 * genericsettings.evaluation_setting[1], noisy: 1e8, otherwise: 1e7. maximal run length shown

commit 4a6cc23fbdc959cd7d2cfb2d9d7c93513b7bc4b7

fix #2097 xlimit bug introduced with commit f5289e3

code-postprocessing/cocopp/compall/pprldmany.py
@@ -46,6 +46,7 @@ from .. import pptex  # numtotex
 PlotType = ppfig.enum('ALG', 'DIM', 'FUNC')
 
 displaybest = True
+x_limit = genericsettings.xlimit_pprldmany
 divide_by_dimension = True
 annotation_line_end_relative = 1.065  # lines between graph and annotation, was 1.11, see also subplots_adjust
 annotation_space_end_relative = 1.21  # figure space end relative to x_limit, space is however determined rather by subplots_adjust(...) below!?
@@ -211,8 +212,6 @@ def plotdata(data, maxval=None, maxevals=None, CrE=0., **kwargs):
     :param kwargs: optional arguments provided to plot function.
     
     """
-    x_limit = genericsettings.xlimit_pprldmany
-
     # Expect data to be a ndarray.
     x = data[np.isnan(data) == False]  # Take away the nans
     nn = len(x)
@@ -597,7 +596,6 @@ def main(dictAlg, order=None, outputdir='.', info='default',
 
     """
     global divide_by_dimension  # not fully implemented/tested yet
-    x_limit = genericsettings.xlimit_pprldmany
 
     tmp = pp.dictAlgByDim(dictAlg)
     algorithms_with_data = [a for a in dictAlg.keys() if dictAlg[a] != []]

code-postprocessing/cocopp/config.py
@@ -145,8 +145,8 @@ def config(suite_name=None):
         ppscatter.markersize = 16
 
     else:
-        pass  # here the default values of the modules apply
-        # pprlmany.x_limit = ...should depend on noisy/noiseless
+        # here the default values of the modules apply
+        pprldmany.x_limit = settings.xlimit_pprldmany  # ...should depend on noisy/noiseless
     if 11 < 3:  # for testing purpose
         if tbs.current_testbed:
             # TODO: this case needs to be tested yet: the current problem is that no noisy data are in this folder

code-postprocessing/cocopp/genericsettings.py
@@ -45,8 +45,15 @@ target_runlength = 10  # used in ppfigs.main
 single_runlength_factors = [0.5, 1.2, 3, 10] + [10 ** i for i in range(2, 12)] # used in pprldistr
 
 xlimit_pprldmany = 1e7
-"""maximal run length multiplier used in `pprldmany`, noisy: rather 1e8
-   maybe better: ``10 * genericsettings.evaluation_setting[1]``?"""
+"""maximal run length multiplier used in `pprldmany`.
+
+   After a manual change like ``cocopp.genericsettings.xlimit_pprldmany =
+   1e5``, ``cocopp.config.config()`` needs to be called to "activate" the change.
+   Alternatively, we could also directly set
+   ``cocopp.compall.pprldmany.x_limit = 1e5``.
+
+   Noisy setting should be rather 1e8?
+   ?maybe better: ``10 * genericsettings.evaluation_setting[1]``?"""
 xlimit_expensive = 1e3
 """maximal run length multiplier in expensive setting, used in config for
    `pprldmany` and `ppfigdim`"""