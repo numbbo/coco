7b157cbb0d62e36482a83cb668897add23eb8111
Olaf Mersmann <olafm@p-value.net>
2022-12-31 00:42:23 +0100

    Fix warnings in function cast

--- code-experiments/test/unit-test/test_brentq.c
+++ code-experiments/test/unit-test/test_brentq.c
@@ -6,10 +6,14 @@
  * Tests the function coco_observer_targets_trigger.
  */
 
+double my_sqrt(double x, COCO_UNUSED void *other) {
+  return sqrt(x);
+}
+
 MU_TEST(test_brentq) {
     double y;
 
-    y = brentq((callback_type) &sqrt, 4, 1, 4, 1E-14, 1E-10, 200, NULL);
+    y = brentq((callback_type) &my_sqrt, 4, 1, 4, 1E-14, 1E-10, 200, NULL);
     mu_check(y - 2 < 1E-14);
     mu_check(2 - y < 1E-14);
 }
@@ -17,7 +21,7 @@ MU_TEST(test_brentq) {
 MU_TEST(test_brentinv) {
     double y;
 
-    y = brentinv((callback_type) &sqrt, 4, NULL);
+    y = brentinv((callback_type) &my_sqrt, 4, NULL);
     mu_check(y - 2 < 1E-14);
     mu_check(2 - y < 1E-14);
 }
