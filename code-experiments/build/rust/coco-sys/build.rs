da1a912fe3626a15ec108e1293d423793cd1f8cf
Olaf Mersmann <olafm@p-value.net>
2023-02-21 00:44:22 +0100

    Rework Rust build
    1. Have cargo run bindgen for us when building `coco-sys`.
    2. Simplify instructions in README.md
    3. Fix size_t -> u64 / usize confusion.

--- code-experiments/build/rust/coco-sys/build.rs
+++ code-experiments/build/rust/coco-sys/build.rs
@@ -1,47 +1,18 @@
-use std::{
-    env,
-    path::{Path, PathBuf},
-    process::Command,
-};
+extern crate bindgen;
+use std::env;
+use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
-    generate_source();
-    generate_bindings();
-    compile_and_link();
-}
-
-fn generate_source() {
-    let src_path = Path::new("vendor");
-
-    let required_files = ["coco.c", "coco.h", "coco_internal.h"];
-    let files_exist = || {
-        required_files
-            .iter()
-            .all(|file| src_path.join(file).exists())
-    };
-
-    if !files_exist() {
-        let build_script_path = Path::new("../../../../do.py");
-        assert!(build_script_path.exists(), "build script does not exist");
-
-        Command::new("python")
-            .arg(build_script_path)
-            .arg("prepare-build-rust")
-            .output()
-            .expect("failed to run prepare-build-rust command");
-    }
-
-    assert!(files_exist(), "failed to generate code files");
-}
-
-fn generate_bindings() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
+    let src_path = Path::new("vendor");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .size_t_is_usize(true)
         .blocklist_item("FP_NORMAL")
         .blocklist_item("FP_SUBNORMAL")
         .blocklist_item("FP_ZERO")
@@ -53,10 +24,6 @@ fn generate_bindings() {
     bindings
         .write_to_file(out_path.join("coco_sys.rs"))
         .expect("Couldn't write bindings to 'OUT_DIR/coco_sys.rs'!");
-}
-
-fn compile_and_link() {
-    let src_path = Path::new("vendor");
 
     cc::Build::new()
         .file(src_path.join("coco.c"))
