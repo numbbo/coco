317f02b69e0f866930b750bc8a78a214a947022d
Olaf Mersmann <olafm@p-value.net>
2023-10-24 16:08:09 +0200

    Fix java bindings

--- code-experiments/build/java/Problem.java
+++ code-experiments/build/java/Problem.java
@@ -65,6 +65,14 @@ public class Problem {
 		return CocoJNI.cocoEvaluateConstraint(this.pointer, x);
 	}
 
+	/**
+	 * Recommends solution x (the solution is logged, but its function values are not returned)
+	 * @param x
+	 */
+	public void recommendSolution(double[] x) {
+		CocoJNI.cocoRecommendSolution(this.pointer, x);
+	}
+
 	// Getters
 	public long getPointer() {
 		return this.pointer;
