109542255acf9f40e622b12091f7eba1ef7198ae
nikohansen <nikolaus.hansen@inria.fr>
2024-05-01 14:07:04 +0200

    [code-experiments/python] polish beginner example experiment

--- code-experiments/build/python/example/example_experiment_for_beginners.py
+++ code-experiments/build/python/example/example_experiment_for_beginners.py
@@ -18,13 +18,12 @@ from __future__ import division, print_function
 import cocoex, cocopp  # experimentation and post-processing modules
 import scipy.optimize  # to define the solver to be benchmarked
 from numpy.random import rand  # for randomised restarts
-import os, webbrowser  # to show post-processed results in the browser
 
 ### input
 suite_name = "bbob"
 output_folder = "scipy-optimize-fmin"
 fmin = scipy.optimize.fmin
-budget_multiplier = 1  # increase to 10, 100, ...
+budget_multiplier = 1.1  # increase to 10, 100, ...
 
 ### prepare
 suite = cocoex.Suite(suite_name, "", "")
@@ -32,19 +31,19 @@ observer = cocoex.Observer(suite_name, "result_folder: " + output_folder)
 minimal_print = cocoex.utilities.MiniPrint()
 
 ### go
-for problem in suite:  # this loop will take several minutes or longer
+for problem in suite:  # this loop will take 2-3 minutes or longer
     problem.observe_with(observer)  # generates the data for cocopp post-processing
     x0 = problem.initial_solution
     # apply restarts while neither the problem is solved nor the budget is exhausted
     while (problem.evaluations < problem.dimension * budget_multiplier
            and not problem.final_target_hit):
         observer.signal_restart(problem)
-        fmin(problem, x0, disp=False)  # here we assume that `fmin` evaluates the final/returned solution
+        xopt = fmin(problem, x0, disp=False)
+        problem(xopt)  # make sure the returned solution is evaluated
         x0 = problem.lower_bounds + ((rand(problem.dimension) + rand(problem.dimension)) *
                     (problem.upper_bounds - problem.lower_bounds) / 2)
     minimal_print(problem, final=problem.index == len(suite) - 1)
 
 ### post-process data
 cocopp.main(observer.result_folder)  # re-run folders look like "...-001" etc
-webbrowser.open("file://" + os.getcwd() + "/ppdata/index.html")
 
